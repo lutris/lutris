3c22eb0e0a82acdb984b4c25b4c0a0231b24cfb3
Mathieu Comandon <strycore@gmail.com>
2011-01-05 00:50:29 +0100

    Fixing DATA_PATH ... again

--- lutris/constants.py
+++ lutris/constants.py
@@ -1,19 +1,19 @@
 # -*- coding:Utf-8 -*-
 #
 #  Copyright (C) 2010 Mathieu Comandon <strider@strycore.com>
-# 
+#
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License version 3 as
 #  published by the Free Software Foundation.
-# 
+#
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-# 
+#
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-# 
+#
 
 import os
 import sys
@@ -46,11 +46,13 @@ license = """
 #Paths
 
 LAUNCH_PATH = os.path.abspath(sys.path[0])
-
+print "LP : ", LAUNCH_PATH
 if not LAUNCH_PATH.startswith('/usr'):
     DATA_PATH = os.path.join(sys.path[0], '..','data')
 else:
     DATA_PATH = '/usr/share/lutris'
+if not os.path.exists(DATA_PATH):
+    print "DATA_PATH can't be found at : %s" % DATA_PATH
 lutris_icon_file = "media/logo.svg"
 lutris_icon_path = os.path.join(DATA_PATH, lutris_icon_file)
 
