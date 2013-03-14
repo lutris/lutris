2e72acf91565b8f3a412514053ae3888e857e4c2
Mathieu Comandon <strycore@gmail.com>
2013-03-14 08:33:25 +0100

    Fix XDG Menu parser

--- lutris/game.py
+++ lutris/game.py
@@ -79,8 +79,7 @@ def get_list():
 def reset_pulse():
     """ Reset pulseaudio. """
     pulse_reset = "pulseaudio --kill && sleep 1 && pulseaudio --start"
-    subprocess.Popen(pulse_reset,
-                        shell=True)
+    subprocess.Popen(pulse_reset, shell=True)
     logger.debug("PulseAudio restarted")
 
 
--- lutris/xdgmenu.py
+++ lutris/xdgmenu.py
@@ -1,6 +1,7 @@
 """Get games available from the XDG menu"""
 import xdg.Menu
 import locale
+import subprocess
 
 IGNORED_ENTRIES = ["lutris", "mame", "dosbox", "steam"]
 
@@ -8,18 +9,32 @@ locale._strxfrm = locale.strxfrm
 
 
 def strxfrm(s):
-    """Monkey patch to address an encoding bug in pyxdg (fixed in current trunk)"""
+    """Monkey patch to address an encoding bug in pyxdg (fixed in current
+    trunk)"""
     return locale._strxfrm(s.encode('utf-8'))
 locale.strxfrm = strxfrm
 
 
+def get_main_menu():
+    return xdg.Menu.parse()
+
+
+def get_game_entries(menu):
+    menu_entries = [entry for entry in menu.getEntries()]
+    game_entries = []
+    for entry in menu_entries:
+        if hasattr(entry, 'Categories') and 'Game' in entry.Categories:
+            game_entries.append(entry)
+        elif hasattr(entry, 'getEntries') and entry.getName() == 'Games':
+            game_entries += get_game_entries(entry)
+    return game_entries
+
+
 def get_xdg_games():
     """Return list of games stored in the XDG menu."""
-    cache = xdg.Menu.parse()
-    game_menu = [entry
-                 for entry in cache.getEntries() if str(entry) == "Games"][0]
     xdg_games = []
-    for game in game_menu.getEntries():
+    for game in get_game_entries(get_main_menu()):
+
         if not isinstance(game, xdg.Menu.MenuEntry):
             continue
         entry_name = str(game)[:-(len(".desktop"))]
@@ -27,6 +42,18 @@ def get_xdg_games():
             continue
         desktop_entry = game.DesktopEntry
         game_name = unicode(desktop_entry)
-        exe = desktop_entry.getExec()
-        xdg_games.append((game_name, exe))
+        exe_and_args = desktop_entry.getExec().split(' ', 2)
+        if len(exe_and_args) == 1:
+            exe = exe_and_args[0]
+            args = ''
+        else:
+            exe, args = exe_and_args
+        if not exe.startswith('/'):
+            exe = subprocess.Popen("which '%s'" % exe, stdout=subprocess.PIPE,
+                                   shell=True).communicate()[0].strip('\n')
+        xdg_games.append((game_name, exe, args))
     return xdg_games
+
+if __name__ == '__main__':
+
+    print get_xdg_games()
