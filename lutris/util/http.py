4044a508690f0bbd69ec4d744801a9cbbd58b70f
Mathieu Comandon <strycore@gmail.com>
2013-04-21 03:12:23 +0200

    Add forgotten http module

---
+++ lutris/util/http.py
@@ -0,0 +1,27 @@
+import os
+import urllib
+
+from lutris.util.log import logger
+
+
+class RessourceOpener(urllib.FancyURLopener):
+    def http_error_default(self, url, fp, errcode, errmsg, headers):
+        if errcode == 404:
+            raise IOError(errmsg, errcode, url)
+
+
+def download_asset(url, dest, overwrite=False):
+    asset_opener = RessourceOpener()
+    if os.path.exists(dest):
+        if overwrite:
+            os.remove(dest)
+        else:
+            logger.info("Destination %s exists, not overwritting" % dest)
+            return
+    try:
+        asset_opener.retrieve(url, dest)
+    except IOError as ex:
+        if ex[1] == 404:
+            logger.warning("Asset %s not found" % url)
+        else:
+            logger.error("Error while fetching %s: %s" % (url, ex))
