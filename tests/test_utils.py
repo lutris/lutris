fe660706b5cadedf911257b7d76d6e6a0a68b5de
Mathieu Comandon <strycore@gmail.com>
2013-07-11 14:42:30 +0200

    Test for filename substitution

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -21,3 +21,10 @@ class TestFileUtils(TestCase):
 
         file_id = '${foo-bar bang}'
         self.assertEqual(files.python_identifier(file_id), '${foo-bar bang}')
+
+    def test_file_ids_are_substitued(self):
+        fileid = '${foo-bar}'
+        _files = {
+            'foo-bar': "/foo/bar"
+        }
+        self.assertEqual(files.substitute(fileid, _files), "/foo/bar")
