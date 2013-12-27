1095da73b5074a62b9fb30407fddd00f6774b63e
Mathieu Comandon <strycore@gmail.com>
2013-12-28 00:33:08 +0100

    Search for correct pcsxr executable

--- lutris/runners/pcsxr.py
+++ lutris/runners/pcsxr.py
@@ -1,10 +1,10 @@
 from lutris.runners.runner import Runner
+from lutris.util.system import find_executable
 
 
 class pcsxr(Runner):
     """PlayStation emulator"""
 
-    executable = "pcsxr"
     package = "pcsxr"
     is_installable = True
     platform = "Playstation"
@@ -15,8 +15,19 @@ class pcsxr(Runner):
     }]
     runner_options = []
 
+    def get_executable(self):
+        candidates = ('pcsx', 'pcsxr')
+        for candidate in candidates:
+            executable = find_executable(candidate)
+            if executable:
+                return executable
+
+    def is_installed(self):
+        return bool(self.get_executable())
+
     def play(self):
         """Run Playstation game"""
         iso = self.settings["game"].get("iso")
-        command = [self.executable, " -nogui -cdfile \"" + iso + "\" -runcd"]
+        executable = self.get_executable()
+        command = [executable, " -nogui -cdfile \"" + iso + "\" -runcd"]
         return {'command': command}
