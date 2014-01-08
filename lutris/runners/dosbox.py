f090fffbe793b22e88410a6166d4509328664ac3
Mathieu Comandon <strycore@gmail.com>
2014-01-09 00:42:00 +0100

    Add dosexec dosbox installer task

--- lutris/runners/dosbox.py
+++ lutris/runners/dosbox.py
@@ -1,29 +1,15 @@
 # -*- coding:Utf-8 -*-
-###############################################################################
-## Lutris
-##
-## Copyright (C) 2009 Mathieu Comandon strycore@gmail.com
-##
-## This program is free software; you can redistribute it and/or modify
-## it under the terms of the GNU General Public License as published by
-## the Free Software Foundation; either version 3 of the License, or
-## (at your option) any later version.
-##
-## This program is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
-##
-## You should have received a copy of the GNU General Public License
-## along with this program; if not, write to the Free Software
-## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-###############################################################################
-
 """ Runner for MS Dos games """
-
 import os
+import subprocess
 from lutris.util.log import logger
 from lutris.runners.runner import Runner
+
+
+def dosexec(config_file):
+    logger.debug("Running dosbox with config %s" % config_file)
+    subprocess.Popen("dosbox -conf %s" % config_file, shell=True,
+                     stdout=subprocess.PIPE).communicate()
 
 
 # pylint: disable=C0103
