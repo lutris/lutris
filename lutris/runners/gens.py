0548ab1529577a17331e83232831d0816684f379
Mathieu Comandon <strycore@gmail.com>
2014-04-22 22:52:00 +0200

    gens runner cleanup

--- lutris/runners/gens.py
+++ lutris/runners/gens.py
@@ -1,8 +1,5 @@
-""" Runner for Sega Genesis games """
-import subprocess
-
 import os
-from os.path import expanduser
+import subprocess
 
 from lutris.runners.runner import Runner
 from lutris.gui.dialogs import DownloadDialog
@@ -11,41 +8,29 @@ from lutris.settings import CACHE_DIR
 
 class gens(Runner):
     """Runner for Sega Genesis games"""
-    def __init__(self, settings=None):
-        """Constructor"""
-        super(gens, self).__init__()
-        self.package = 'gens-gs'
-        self.executable = 'gens'
-        self.platform = 'Sega Genesis'
-        self.description = 'Sega Genesis emulator.'
-        self.game_options = [{
-            'option': 'rom',
-            'type': 'file',
-            'label':  'Rom File'
-        }]
-        self.runner_options = [
-            {
-                'option': 'fullscreen',
-                'type': 'bool',
-                'label': 'Fullscreen'
-            },
-            {
-                'option': 'quickexit',
-                'type': 'bool',
-                'label': 'Exit emulator with Esc'
-            }
-        ]
-        if settings:
-            if 'fullscreen' in settings['gens']:
-                if settings['gens']['fullscreen']:
-                    self.arguments = self.arguments + ['--fs']
-                else:
-                    self.arguments = self.arguments + ['--window']
-            if 'quickexit' in settings['gens']:
-                if settings['gens']['quickexit']:
-                    self.arguments = self.arguments + ['--quickexit']
-            if 'rom' in settings['game']:
-                self.rom = settings['game']['rom']
+    package = 'gens-gs'
+    executable = 'gens'
+    platform = 'Sega Genesis'
+    description = 'Sega Genesis emulator.'
+    game_options = [{
+        'option': 'main_file',
+        'type': 'file',
+        'label':  'Rom File'
+    }]
+    runner_options = [
+        {
+            'option': 'fullscreen',
+            'type': 'bool',
+            'label': 'Fullscreen',
+            'default': True
+        },
+        {
+            'option': 'quickexit',
+            'type': 'bool',
+            'label': 'Exit emulator with Esc',
+            'default': True
+        }
+    ]
 
     def install(self):
         """Downloads deb package and installs it"""
@@ -59,14 +44,18 @@ class gens(Runner):
 
     def play(self):
         """ Run the game """
+        arguments = [self.executable]
+        if self.runner_config.get('fullscreen', True):
+            arguments.append('--fs')
+        else:
+            arguments.append('--window')
+        if self.runner_config.get('quickexit', True):
+            arguments.append('--quickexit')
+        rom = self.settings['game']['main_file']
         plugins_dir = os.path.join(os.path.expanduser('~'), '.gens/plugins')
         if not os.path.exists(plugins_dir):
             os.makedirs(plugins_dir)
-        if not self.is_installed():
-            return {'error': 'RUNNER_NOT_INSTALLED',
-                    'runner': self.__class__.__name__}
-        if not os.path.exists(self.rom):
-            return {'error': 'FILE_NOT_FOUND', 'file': self.rom}
-        arguments = ["--game \"%s\"" % self.rom]
-        command = [self.executable] + arguments
-        return {"command": command}
+        if not os.path.exists(rom):
+            return {'error': 'FILE_NOT_FOUND', 'file': rom}
+        arguments.append("--game \"%s\"" % rom)
+        return {"command": arguments}
