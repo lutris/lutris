4b6ac1b72fb82c94abd3e7fa01b887b9e46bc99d
Mathieu Comandon <strycore@gmail.com>
2014-04-23 00:44:58 +0200

    Update tests for multiple installers

--- tests/test_installer.py
+++ tests/test_installer.py
@@ -1,12 +1,10 @@
-from tempfile import NamedTemporaryFile
 from unittest import TestCase
 from lutris.installer import ScriptInterpreter, ScriptingError
 
 
 class MockInterpreter(ScriptInterpreter):
     """ a script interpreter mock """
-    def _fetch_script(self, name):
-        return {'runner': 'linux'}
+    script = {'runner': 'linux'}
 
     def is_valid(self):
         return True
@@ -14,37 +12,34 @@ class MockInterpreter(ScriptInterpreter):
 
 class TestScriptInterpreter(TestCase):
     def test_script_with_correct_values_is_valid(self):
-        script_data = """[{
-        runner: foo,
-        installer: bar,
-        name: baz}]
-        """
-        with NamedTemporaryFile(delete=False) as temp_script:
-            temp_script.write(script_data)
-        interpreter = ScriptInterpreter(temp_script.name, None)
+        script = {
+            'runner': 'foo',
+            'installer': 'bar',
+            'name': 'baz'
+        }
+        interpreter = ScriptInterpreter(script, None)
         self.assertFalse(interpreter.errors)
         self.assertTrue(interpreter.is_valid())
 
     def test_move_requires_src_and_dst(self):
-        with NamedTemporaryFile(delete=False) as temp_script:
-            temp_script.write("""
-                            foo: bar
-                            installer: {}
-                            name: missing_runner
-                            """)
+        script = {
+            'foo': 'bar',
+            'installer': {},
+            'name': 'missing_runner'
+        }
         with self.assertRaises(ScriptingError):
-            interpreter = ScriptInterpreter(temp_script.name, None)
+            interpreter = ScriptInterpreter(script, None)
             interpreter._get_move_paths({})
 
     def test_get_command_returns_a_method(self):
-        interpreter = MockInterpreter('test', None)
+        interpreter = MockInterpreter({}, None)
         command, params = interpreter._map_command({'move': 'whatever'})
         self.assertIn("bound method MockInterpreter.move", str(command))
         self.assertEqual(params, "whatever")
 
     def test_get_command_doesnt_return_private_methods(self):
         """ """
-        interpreter = MockInterpreter('test', None)
+        interpreter = MockInterpreter({}, None)
         with self.assertRaises(ScriptingError) as ex:
             command, params = interpreter._map_command(
                 {'_substitute': 'foo'}
