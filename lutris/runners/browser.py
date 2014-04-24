974ceca2e97ef6a495b0989b2df342857ea02e00
Mathieu Comandon <strycore@gmail.com>
2014-04-24 02:03:40 +0200

    Cleanup browser runner

--- lutris/runners/browser.py
+++ lutris/runners/browser.py
@@ -1,46 +1,31 @@
-# -*- coding:Utf-8 -*-
-""" Runner for browser games """
+# -*- coding: utf-8 -*-
 from lutris.runners.runner import Runner
 
 
-# pylint: disable=C0103
 class browser(Runner):
     """Runner for browser games"""
-
-    def __init__(self, settings=None):
-        """Constructor"""
-        super(browser, self).__init__()
-        self.package = None
-        self.executable = "xdg-open"
-        self.platform = "Web Browser"
-        self.description = "Run games in the browser"
-        self.game_options = [
-            {
-                "option": "main_file",
-                "type": "string",
-                "label": "URL"
-            }
-        ]
-        self.runner_options = [
-            {
-                'option': 'browser',
-                'type': "file",
-                'label': "Web Browser"
-            }
-        ]
-        if settings:
-            self.settings = settings
-            runner_settings = settings["browser"]
-            if runner_settings:
-                self.browser_exec = runner_settings.get('browser',
-                                                        self.executable)
-            else:
-                self.browser_exec = self.executable
+    executable = "xdg-open"
+    platform = "Web Browser"
+    description = "Run games in the browser"
+    game_options = [
+        {
+            "option": "main_file",
+            "type": "string",
+            "label": "URL"
+        }
+    ]
+    runner_options = [
+        {
+            'option': 'browser',
+            'type': "file",
+            'label': "Web Browser"
+        }
+    ]
 
     def is_installed(self):
         return True
 
     def play(self):
-        """Run a browser game"""
+        self.browser_exec = self.runner_config.get('browser', self.executable)
         url = self.settings["game"]["main_file"]
         return {'command': [self.browser_exec, "\"%s\"" % url]}
