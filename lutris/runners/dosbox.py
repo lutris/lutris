7daafd9cd2a5f77e681f3b20459960a7bb3b3a92
Mathieu Comandon <strycore@gmail.com>
2014-04-24 02:06:14 +0200

    Cleanup dosbox runner

--- lutris/runners/dosbox.py
+++ lutris/runners/dosbox.py
@@ -1,5 +1,4 @@
-# -*- coding:Utf-8 -*-
-""" Runner for MS Dos games """
+# -*- coding: utf-8 -*-
 import os
 import subprocess
 from lutris.util.log import logger
@@ -12,30 +11,24 @@ def dosexec(config_file):
                      stdout=subprocess.PIPE).communicate()
 
 
-# pylint: disable=C0103
 class dosbox(Runner):
     """Runner for MS Dos games"""
-    def __init__(self, settings=None):
-        """Constructor"""
-        super(dosbox, self).__init__()
-        self.package = "dosbox"
-        self.executable = "dosbox"
-        self.platform = "MS DOS"
-        self.description = "DOS Emulator"
-        self.game_options = [
-            {
-                "option": "main_file",
-                "type": "file",
-                "label": "EXE File"
-            },
-            {
-                "option": "config_file",
-                "type": "file",
-                "label": "Configuration file"
-            }
-        ]
-        self.runner_options = []
-        self.settings = settings
+    package = "dosbox"
+    executable = "dosbox"
+    platform = "MS DOS"
+    description = "DOS Emulator"
+    game_options = [
+        {
+            "option": "main_file",
+            "type": "file",
+            "label": "EXE File"
+        },
+        {
+            "option": "config_file",
+            "type": "file",
+            "label": "Configuration file"
+        }
+    ]
 
     @property
     def browse_dir(self):
@@ -45,8 +38,6 @@ class dosbox(Runner):
         return None
 
     def play(self):
-        """ Run the game """
-        logger.debug(self.settings)
         self.exe = self.settings["game"]["main_file"]
         self.game_path = os.path.dirname(self.exe)
         if not os.path.exists(self.exe):
