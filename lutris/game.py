09bc67cdd7b1f7a6c63496ff056633df5c452e0d
Xodetaetl <dev@ovocean.com>
2014-06-18 21:53:32 +0200

    Stopped returning game.directory first in get_browse_dir()
    This code should put in runner.get_game_path() instead I think. But I
    don't know how to access game.directory from the Runner class.

--- lutris/game.py
+++ lutris/game.py
@@ -64,11 +64,7 @@ class Game(object):
 
     def get_browse_dir(self):
         """Return the path to open with the Browse Files action."""
-        if os.path.exists(self.directory):
-            path = self.directory
-        else:
-            path = self.runner.browse_dir
-        return path
+        return self.runner.browse_dir
 
     def get_runner(self):
         """Return the runner's name."""
