207ae8d2e5bcedc6cddb64cb9af733461d4c6caf
Mathieu Comandon <strycore@gmail.com>
2014-08-06 21:51:12 +0200

    Add some checking for runner imports

--- lutris/runners/__init__.py
+++ lutris/runners/__init__.py
@@ -22,25 +22,28 @@ __all__ = (
     "openmsx", "frotz", "jzintv", "o2em",
 )
 
+
+def get_runner_module(runner_name):
+    if not runner_name:
+        raise ValueError("Missing runner name")
+    if runner_name not in __all__:
+        logger.error("Invalid runner name '%s'", runner_name)
+        return False
+    return __import__('lutris.runners.%s' % runner_name,
+                      globals(), locals(), [runner_name], -1)
+
+
 def import_runner(runner_name):
     """Dynamically import a runner class"""
-    try:
-        runner_module = __import__('lutris.runners.%s' % runner_name,
-                                   globals(), locals(), [runner_name], -1)
-        runner_cls = getattr(runner_module, runner_name)
-    except ImportError:
-        logger.error("Invalid runner %s" % runner_name)
-        raise
-    return runner_cls
+    runner_module = get_runner_module(runner_name)
+    if not runner_module:
+        return
+    return getattr(runner_module, runner_name)
 
 
 def import_task(runner, task):
     """Return a runner task"""
-    try:
-        runner_module = __import__('lutris.runners.%s' % runner,
-                                   globals(), locals(), [runner], -1)
-        runner_task = getattr(runner_module, task)
-    except ImportError:
-        logger.error("Invalid runner %s" % runner)
-        raise
-    return runner_task
+    runner_module = get_runner_module(runner)
+    if not runner_module:
+        return
+    return getattr(runner_module, task)
