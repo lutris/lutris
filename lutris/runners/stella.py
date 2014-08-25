88b4a462f882b0313a88d962e7374bbc261b247a
Mathieu Comandon <strycore@gmail.com>
2014-08-25 04:04:22 +0200

    Provide Stella runner

--- lutris/runners/stella.py
+++ lutris/runners/stella.py
@@ -1,11 +1,11 @@
 """Runner for stella Atari 2600 emulator"""
+import os
+from lutris import settings
 from lutris.runners.runner import Runner
 
 
 class stella(Runner):
     """Atari 2600 games emulator"""
-    package = "stella"
-    executable = "stella"
     platform = "Atari 2600"
     game_options = [{
         "option": "main_file",
@@ -14,6 +14,13 @@ class stella(Runner):
     }]
     runner_options = []
 
+    tarballs = {
+        "x64": "stella-4.0-x86_64.tar.gz",
+    }
+
+    def get_executable(self):
+        return os.path.join(settings.RUNNER_DIR, "stella/bin/stella")
+
     def play(self):
-        cart = self.settings["game"]["main_file"]
-        return {'command': ['stella', "\"%s\"" % cart]}
+        cart = self.settings["game"].get("main_file")
+        return {'command': [self.get_executable(), "\"%s\"" % cart]}
