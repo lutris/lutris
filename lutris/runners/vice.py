0527fb3b2e4bd870cf24c25d950721b532b9e4db
Mathieu Comandon <strycore@gmail.com>
2014-08-31 22:59:12 +0200

    Provide Vice runner

--- lutris/runners/vice.py
+++ lutris/runners/vice.py
@@ -1,10 +1,10 @@
+import os
+from lutris import settings
 from lutris.runners.runner import Runner
 
 
 class vice(Runner):
     """ Commodore Emulator """
-    package = "vice"
-    executable = "x64"
     platform = "Commodore 64"
 
     game_options = [{
@@ -28,11 +28,48 @@ class vice(Runner):
             "option": "double",
             "type": "bool",
             "label": "Double Size"
+        },
+        {
+            "option": "machine",
+            "type": "choice",
+            "label": "Machine",
+            "choices": (
+                ("c64", "C64"),
+                ("c128", "C128"),
+                ("vic20", "VIC20"),
+                ("pet", "PET"),
+                ("plus4", "Plus/4"),
+                ("cmbii", "CBM-II")
+            ),
+            "default": "c64"
         }
     ]
 
+    tarballs = {
+        "x64": "vice-2.4-x86_64.tar.gz"
+    }
+
+    def get_executable(self, machine=None):
+        if not machine:
+            machine = "c64"
+        executables = {
+            "c64": "x64",
+            "c128": "x128",
+            "vic20": "xvic",
+            "pet": "xpet",
+            "plus4": "xplus4",
+            "cmbii": "xcbm2"
+
+        }
+        try:
+            executable = executables[machine]
+        except KeyError:
+            raise ValueError("Invalid machine '%s'" % machine)
+        return os.path.join(settings.RUNNER_DIR, "vice/bin/%s" % executable)
+
     def play(self):
-        params = [self.executable]
+        machine = self.runner_config.get("machine")
+        params = [self.get_executable(machine)]
         if self.runner_config.get("fullscreen"):
             params.append("-fullscreen")
         if self.runner_config.get("double"):
