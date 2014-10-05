d483895903a90595283148a78fa86973aa60ca8f
Mathieu Comandon <strycore@gmail.com>
2014-10-05 21:01:51 +0200

    Edit mame description

--- lutris/runners/mame.py
+++ lutris/runners/mame.py
@@ -5,7 +5,7 @@ from lutris.runners.runner import Runner
 
 
 class mame(Runner):
-    """Runs arcade games with SDLMame"""
+    """Arcade game emulator"""
     platform = "Arcade"
     game_options = [
         {
