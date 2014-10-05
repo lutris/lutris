065a6bb261826ba05352c333fa29cbe04e2cb569
Xodetaetl <dev@ovocean.com>
2014-10-05 18:04:15 +0200

    Fix hatari bios path missing quotes

--- lutris/runners/hatari.py
+++ lutris/runners/hatari.py
@@ -131,7 +131,7 @@ class hatari(Runner):
             params.append("--joy1 " + self.runner_config['joy1'])
 
         if os.path.exists(self.runner_config.get('bios_file', '')):
-            params.append("--tos " + self.runner_config["bios_file"])
+            params.append('--tos "%s"' % self.runner_config["bios_file"])
         else:
             return {'error': 'NO_BIOS'}
         diska = game_settings.get('disk-a')
