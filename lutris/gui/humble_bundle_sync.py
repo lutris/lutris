71148272e1ca8bc3f712f5df7bdac0034d133c4d
Sasha Tapfumaneyi <Kehlyos@users.noreply.github.com>
2014-10-18 23:31:10 +0100

    Update humble_bundle_sync.py

--- lutris/gui/humble_bundle_sync.py
+++ lutris/gui/humble_bundle_sync.py
@@ -50,21 +50,7 @@ class ConnectWindow(Gtk.Window):
         gamekeys = client.get_gamekeys()
         print(gamekeys)
         global order_list
-<<<<<<< HEAD
-        order_list = client.order_list()
-        gamekeys = client.get_gamekeys()
-        self.get_order_products(client, gamekeys[0])
-        g = [product.human_name for product in self.get_order_products(client, gamekeys[0])]
-        print(g[0])
 
-
-#for gamekey in gamekeys:
-# order = client.get_order(gamekey)
- #           for subproduct in order.subproducts:
-  #              global gameName
-   #             subproduct.machine_name
-    #            print([product.human_name for product in products])
-=======
         global gameName
         order_list = client.order_list()
         gamekeys = client.get_gamekeys()
@@ -89,8 +75,6 @@ class ConnectWindow(Gtk.Window):
 #     global gameName
 #     subproduct.machine_name
 #     print([product.human_name for product in products])
->>>>>>> origin/master
-
 
 
     def on_editable_toggled(self, button):
@@ -110,15 +94,15 @@ class MainHBSyncWindow(Gtk.Window):
 
 
         self.liststore = Gtk.ListStore(str, bool)
-<<<<<<< HEAD
+
         self.liststore.append([order_list, False, True])
-=======
+
         self.liststore.append([gameName[0], False, True])
         
         # Used [0] for testing purpose, a for loop will come one day 
         # so one day we will just use [count] but for now it's [0]
         
->>>>>>> origin/master
+
         treeview = Gtk.TreeView(model=self.liststore)
 
         renderer_text = Gtk.CellRendererText()
