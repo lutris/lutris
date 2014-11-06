33d0099ea7902f8a7e550e7362986c852ec0f292
Mathieu Comandon <strycore@gmail.com>
2014-11-06 01:01:28 +0100

    Use GnomeKeyring to store HB credentials

--- lutris/gui/humble_bundle_sync.py
+++ lutris/gui/humble_bundle_sync.py
@@ -1,46 +1,36 @@
 #! /usr/bin/python
 from gi.repository import Gtk
+from lutris.gui.widgets import Dialog
 from lutris.util import humbleapi
 
 
-class HumbleBundleDialog(Gtk.Dialog):
+class HumbleBundleDialog(Dialog):
     def __init__(self):
-        Gtk.Dialog.__init__(self, title="Humble Bundle sync")
-
-        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
-        self.add(vbox)
-
+        super(HumbleBundleDialog, self).__init__(title="Humble Bundle")
+        self.connect('destroy', self.on_destroy)
         self.login_entry = Gtk.Entry()
         self.login_entry.set_text("login")
         self.login_entry.get_text()
-        vbox.pack_start(self.login_entry, True, True, 0)
+        self.vbox.pack_start(self.login_entry, True, True, 0)
 
         self.password_entry = Gtk.Entry()
         self.password_entry.set_text("password")
-        vbox.pack_start(self.password_entry, True, True, 0)
+        self.vbox.pack_start(self.password_entry, True, True, 0)
 
         connect_button = Gtk.Button("Connect")
         connect_button.connect("clicked", self.on_connect_clicked)
-        vbox.pack_start(connect_button, True, True, 0)
+        self.vbox.pack_start(connect_button, True, True, 0)
 
-    def get_order_products(client, gamekey):
-            order = client.get_order(gamekey)
-            return order.subproducts
+    def on_destroy(self, widget):
+        Gtk.main_quit()
 
     def on_connect_clicked(self, button):
-        self.login = self.entry.get_text()
-        self.passwd = self.entry2.get_text()
-
-        self.client = humbleapi.HumbleApi()
-        self.client.login(self.login, self.passwd)
-
-        order_list = self.client.order_list()
-        print order_list
-        # gamekeys = client.get_gamekeys()
-        # self.get_order_products(client, gamekeys[0])
-        # self.game_name = [product.human_name for product in self.get_order_products(client, gamekeys[0])]
-
+        login = self.login_entry.get_text()
+        password = self.password_entry.get_text()
+        self.client = humbleapi.Client(login, password)
+        self.client.store_credentials(login, password)
 
 if __name__ == "__main__":
-    HumbleBundleDialog()
+    dlg = HumbleBundleDialog()
+    dlg.show_all()
     Gtk.main()
--- lutris/util/humbleapi.py
+++ lutris/util/humbleapi.py
@@ -1,1 +1,36 @@
-from lutris.external.humblebundle import HumbleApi  # noqa
+from lutris.external.humblebundle import HumbleApi
+from gi.repository import GnomeKeyring
+
+
+class Client(object):
+    keyring_name = None
+
+    def __init__(self, login, password):
+        self.api = HumbleApi()
+        self.api.login(login, password)
+
+    def store_credentials(self, username, password):
+        # See https://bitbucket.org/kang/python-keyring-lib/src/8aadf61db38c70a5fe76fbe013df25fa62c03a8d/keyring/backends/Gnome.py?at=default
+        service = "HumbleBundle"
+        attrs = GnomeKeyring.Attribute.list_new()
+        GnomeKeyring.Attribute.list_append_string(attrs, 'username', username)
+        GnomeKeyring.Attribute.list_append_string(attrs, 'password', password)
+        GnomeKeyring.Attribute.list_append_string(attrs, 'application',
+                                                  'Lutris')
+        result = GnomeKeyring.item_create_sync(
+            self.keyring_name, GnomeKeyring.ItemType.NETWORK_PASSWORD,
+            "%s credentials for %s" % (service, username),
+            attrs, password, True
+        )[0]
+        if result == GnomeKeyring.Result.CANCELLED:
+            # XXX
+            return False
+        elif result != GnomeKeyring.Result.OK:
+            # XXX
+            return False
+
+    def _get_gamekeys(self):
+        return self.api.get_gamekeys()
+
+    def get_order(self, gamekey):
+        return self.api.get_order(gamekey)
