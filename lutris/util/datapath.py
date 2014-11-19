f305a9ef8e73993d19336ed223ba3925a3d13c9f
Mathieu Comandon <strycore@gmail.com>
2014-11-19 20:02:15 +0100

    Move data folder to a share folder

--- lutris/util/datapath.py
+++ lutris/util/datapath.py
@@ -9,13 +9,13 @@ def get():
         data_path = '/usr/local/share/lutris'
     elif launch_path.startswith("/usr"):
         data_path = '/usr/share/lutris'
-    elif os.path.exists(os.path.normpath(os.path.join(sys.path[0], 'data'))):
-        data_path = os.path.normpath(os.path.join(sys.path[0], 'data'))
+    elif os.path.exists(os.path.normpath(os.path.join(sys.path[0], 'share'))):
+        data_path = os.path.normpath(os.path.join(sys.path[0], 'share/lutris'))
     else:
         import lutris
         lutris_module = lutris.__file__
         data_path = os.path.join(
-            os.path.dirname(os.path.dirname(lutris_module)), 'data'
+            os.path.dirname(os.path.dirname(lutris_module)), 'share/lutris'
         )
     if not os.path.exists(data_path):
         raise IOError("data_path can't be found at : %s" % data_path)
