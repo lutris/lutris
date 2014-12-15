7257135cb93c1804f84b12e2b66eaf605dbae021
Travis Nickles <nickles.travis@gmail.com>
2014-12-14 23:15:26 -0600

    Use envvar for disabling Lutris runtime.

--- lutris/game.py
+++ lutris/game.py
@@ -145,12 +145,14 @@ class Game(object):
             launch_arguments.insert(0, 'LD_PRELOAD="{}"'.format(ld_preload))
 
         ld_library_path = []
-        runtime64_path = os.path.join(settings.RUNTIME_DIR, "lib64")
-        if os.path.exists(runtime64_path):
-            ld_library_path.append(runtime64_path)
-        runtime32_path = os.path.join(settings.RUNTIME_DIR, "lib32")
-        if os.path.exists(runtime32_path):
-            ld_library_path.append(runtime32_path)
+        if os.getenv("LUTRIS_RUNTIME", 1) == 1:
+            runtime64_path = os.path.join(settings.RUNTIME_DIR, "lib64")
+            if os.path.exists(runtime64_path):
+                ld_library_path.append(runtime64_path)
+            runtime32_path = os.path.join(settings.RUNTIME_DIR, "lib32")
+            if os.path.exists(runtime32_path):
+                ld_library_path.append(runtime32_path)
+
         game_ld_libary_path = gameplay_info.get('ld_library_path')
         if game_ld_libary_path:
             ld_library_path.append(game_ld_libary_path)
