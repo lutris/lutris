9d2c54468d5775e57fb400439a5f4954b7c684ea
Mathieu Comandon <strycore@gmail.com>
2015-03-22 02:15:04 +0100

    Standardize snes9x runner

--- lutris/runners/snes9x.py
+++ lutris/runners/snes9x.py
@@ -1,20 +1,12 @@
 # -*- coding:Utf-8 -*-
 """ Super Nintendo runner """
 import os
-import urllib
 import subprocess
 import xml.etree.ElementTree as etree
 
 from lutris.util.log import logger
 from lutris.runners.runner import Runner
 from lutris import settings
-from lutris.util.extract import extract_archive, decompress_gz
-
-SNES9X_VERSION = "snes9x-1.53"
-SNES9X_32 = settings.RUNNERS_URL + "%s-gtk-81-i386.tar.bz2" % SNES9X_VERSION
-SNES9X_64 = settings.RUNNERS_URL + "%s-gtk-81-x86_64.tar.bz2" % SNES9X_VERSION
-LIBPNG_32 = settings.LIB32_URL + "libpng14.so.14.12.0.gz"
-LIBPNG_64 = settings.LIB64_URL + "libpng14.so.14.12.0.gz"
 
 SNES9X_DIR = os.path.join(settings.DATA_DIR, "runners/snes9x")
 
@@ -23,8 +15,11 @@ class snes9x(Runner):
     """Runs Super Nintendo games with Snes9x"""
     human_name = "Snes9x"
     executable = "snes9x-gtk"
-    package = None
     platform = "Super Nintendo"
+    tarballs = {
+        'i386': 'snes9x-1.53-i686.tar.gz',
+        'x64': 'snes9x-1.53-x86_64.tar.gz',
+    }
 
     game_options = [
         {
@@ -64,23 +59,12 @@ class snes9x(Runner):
     ]
 
     def get_executable(self):
-        return os.path.join(SNES9X_DIR, self.executable)
-
-    def is_installed(self):
-        if os.path.exists(os.path.join(SNES9X_DIR, self.executable)):
-            return True
-        else:
-            return super(snes9x, self).is_installed()
+        return os.path.join(settings.RUNNER_DIR, "snes9x/bin/snes9x-gtk")
 
     def set_option(self, option, value):
         config_file = os.path.expanduser("~/.snes9x/snes9x.xml")
-        lib_path = os.path.join(SNES9X_DIR, "lib")
         if not os.path.exists(config_file):
-            subprocess.Popen(
-                "LD_LIBRARY_PATH=%s \"%s\" -help " % (
-                    lib_path, self.get_executable()
-                ), shell=True
-            )
+            subprocess.Popen([self.get_executable, '-help'])
         if not os.path.exists(config_file):
             logger.error("Snes9x config file creation failed")
             return
@@ -90,32 +74,6 @@ class snes9x(Runner):
             value = "1" if value else "0"
         node.attrib['value'] = value
         tree.write(config_file)
-
-    def install(self):
-        """ Install snes9x from lutris.net """
-        logger.debug("Installing snes9x")
-        tarball_url = SNES9X_64 if self.arch == 'x64' else SNES9X_32
-        tarball_file = os.path.basename(tarball_url)
-        dest = os.path.join(settings.TMP_PATH, tarball_file)
-        logger.debug("Downloading %s" % tarball_url)
-        urllib.urlretrieve(tarball_url, dest)
-
-        logger.debug("Extracting %s" % dest)
-        extract_archive(dest, SNES9X_DIR)
-
-        lib_dir = os.path.join(SNES9X_DIR, "lib")
-        os.mkdir(lib_dir)
-
-        libpng_url = LIBPNG_64 if self.arch == 'x64' else LIBPNG_32
-        libpng_file = os.path.basename(libpng_url)
-        lib_abspath = os.path.join(lib_dir, libpng_file)
-        logger.debug("Downloading %s" % libpng_url)
-        urllib.urlretrieve(libpng_url, lib_abspath)
-        logger.debug("Extracting %s" % lib_abspath)
-        decompress_gz(lib_abspath)
-        logger.debug("Creating lib symlinks")
-        os.link(lib_abspath[:-3], lib_abspath[:-5])
-        os.link(lib_abspath[:-3], lib_abspath[:-8])
 
     def play(self):
         options = self.options_as_dict('runner')
@@ -130,11 +88,4 @@ class snes9x(Runner):
         rom = self.game_config.get('main_file') or ''
         if not os.path.exists(rom):
             return {'error': 'FILE_NOT_FOUND', 'file': rom}
-
-        launch_info = {'command': [self.get_executable(), rom]}
-
-        lib_path = os.path.join(SNES9X_DIR, "lib")
-        if os.path.exists(lib_path):
-            launch_info['ld_library_path'] = lib_path
-
-        return launch_info
+        return {'command': [self.get_executable(), rom]}
