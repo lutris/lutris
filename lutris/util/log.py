21069847188543609acdef6dceca9f42b1f1479f
Mathieu Comandon <strycore@gmail.com>
2015-09-04 00:34:13 -0700

    Clean imports on util.log

--- lutris/util/log.py
+++ lutris/util/log.py
@@ -1,16 +1,17 @@
 """Utility module for creating an application wide logger."""
+import os
 import logging
 import logging.handlers
 import xdg.BaseDirectory
 
-from os import makedirs
-from os.path import join, isdir, realpath
 
-CACHE_DIR = realpath(join(xdg.BaseDirectory.xdg_cache_home, "lutris"))
-if not isdir(CACHE_DIR):
-    makedirs(CACHE_DIR)
+CACHE_DIR = os.path.realpath(
+    os.path.join(xdg.BaseDirectory.xdg_cache_home, "lutris")
+)
+if not os.path.isdir(CACHE_DIR):
+    os.makedirs(CACHE_DIR)
 
-LOG_FILENAME = join(CACHE_DIR, "lutris.log")
+LOG_FILENAME = os.path.join(CACHE_DIR, "lutris.log")
 loghandler = logging.handlers.RotatingFileHandler(LOG_FILENAME,
                                                   maxBytes=20971520,
                                                   backupCount=5)
