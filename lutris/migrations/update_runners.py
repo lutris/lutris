501a9b8338a24d2f4593b926f87ce4ed425b6117
Xodetaetl <dev@ovocean.com>
2015-12-22 18:23:08 +0100

    Add migration to update runners

---
+++ lutris/migrations/update_runners.py
@@ -0,0 +1,16 @@
+import os
+import shutil
+from lutris.settings import RUNNER_DIR
+
+
+def migrate():
+    for dirname in os.listdir(RUNNER_DIR):
+        path = os.path.join(RUNNER_DIR, dirname)
+        if not os.path.isdir(path):
+            return
+        if dirname in ['dgen', 'dolphin', 'dosbox', 'frotz', 'fs-uae',
+                       'hatari', 'jzintv', 'mame', 'mednafen', 'mess',
+                       'mupen64plus', 'nulldc', 'o2em', 'osmose', 'pcsxr',
+                       'reicast', 'ResidualVM', 'residualvm', 'scummvm',
+                       'snes9x', 'stella', 'vice', 'virtualjaguar']:
+            shutil.rmtree(path)
