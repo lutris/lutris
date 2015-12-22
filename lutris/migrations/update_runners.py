bd32e9553280d97685b3aac915d5b0387a06434a
Xodetaetl <dev@ovocean.com>
2015-12-22 19:04:51 +0100

    Add atari800 and gens to runners uninstalled by migration

--- lutris/migrations/update_runners.py
+++ lutris/migrations/update_runners.py
@@ -8,8 +8,8 @@ def migrate():
         path = os.path.join(RUNNER_DIR, dirname)
         if not os.path.isdir(path):
             return
-        if dirname in ['dgen', 'dolphin', 'dosbox', 'frotz', 'fs-uae',
-                       'hatari', 'jzintv', 'mame', 'mednafen', 'mess',
+        if dirname in ['atari800', 'dgen', 'dolphin', 'dosbox', 'frotz', 'fs-uae',
+                       'gens', 'hatari', 'jzintv', 'mame', 'mednafen', 'mess',
                        'mupen64plus', 'nulldc', 'o2em', 'osmose', 'pcsxr',
                        'reicast', 'ResidualVM', 'residualvm', 'scummvm',
                        'snes9x', 'stella', 'vice', 'virtualjaguar']:
