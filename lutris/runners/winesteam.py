a5359817366c9580d84aa69d2b5d03083539af63
Mathieu Comandon <strycore@gmail.com>
2016-02-26 23:39:59 -0800

    Update WineSteam install method

--- lutris/runners/winesteam.py
+++ lutris/runners/winesteam.py
@@ -31,18 +31,6 @@ def get_steam_installer_dest():
     return os.path.join(settings.TMP_PATH, "SteamInstall.msi")
 
 
-def download_steam(downloader=None, callback=None, callback_data=None):
-    """Downloads steam with `downloader` then calls `callback`"""
-    steam_installer_path = get_steam_installer_dest()
-    if not downloader:
-        dialog = DownloadDialog(STEAM_INSTALLER_URL, steam_installer_path)
-        dialog.run()
-    else:
-        downloader(STEAM_INSTALLER_URL,
-                   steam_installer_path, callback, callback_data)
-    return steam_installer_path
-
-
 def is_running():
     pid = system.get_pid('Steam.exe$')
     if pid:
@@ -64,6 +52,7 @@ class winesteam(wine.wine):
     human_name = "Wine Steam"
     platform = "Steam for Windows"
     runnable_alone = True
+    depends_on = wine.wine
     game_options = [
         {
             'option': 'appid',
@@ -224,17 +213,26 @@ class winesteam(wine.wine):
             if path:
                 return path
 
-    def install(self, installer_path=None, version=None):
-        if not self.is_wine_installed():
-            wine.wine().install(version=version)
-        prefix = self.get_or_create_default_prefix()
-        if not self.get_steam_path():
-            if not installer_path:
-                installer_path = get_steam_installer_dest()
-                download_steam()
+    def install(self, version=None, downloader=None, callback=None):
+        installer_path = get_steam_installer_dest()
+
+        def on_steam_downloaded():
+            prefix = self.get_or_create_default_prefix()
             self.msi_exec(installer_path, quiet=True, prefix=prefix,
                           wine_path=self.get_executable())
-        return True
+            if callback:
+                callback()
+
+        if not self.is_wine_installed():
+            # FIXME find another way to do that (already fixed from the install game
+            # dialog)
+            wine.wine().install(version=version, downloader=downloader)
+        if downloader:
+            downloader(STEAM_INSTALLER_URL, installer_path, on_steam_downloaded)
+        else:
+            dialog = DownloadDialog(STEAM_INSTALLER_URL, installer_path)
+            dialog.run()
+            on_steam_downloaded()
 
     def is_wine_installed(self):
         return super(winesteam, self).is_installed()
