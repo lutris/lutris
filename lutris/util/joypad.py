d86826e4499f01dfecc6afc5cdbbce8a560632fe
Mathieu Comandon <strycore@gmail.com>
2016-07-30 13:13:48 -0700

    Add readu_button reference function

--- lutris/util/joypad.py
+++ lutris/util/joypad.py
@@ -10,3 +10,20 @@ def get_joypads():
         return []
     device_names = evdev.list_devices()
     return [(dev, evdev.InputDevice(dev).name) for dev in device_names]
+
+
+def read_button(device):
+    """Reference function for reading controller buttons and axis values.
+    Not to be used as is.
+    """
+    for event in device.read_loop():
+        if event.type == evdev.ecodes.EV_KEY and event.value == 0:
+            print "button %s (%s): %s" % (event.code, hex(event.code), event.value)
+        if event.type == evdev.ecodes.EV_ABS:
+            sticks = (0, 1, 3, 4)
+            if event.code not in sticks or abs(event.value) > 5000:
+                print "axis %s (%s): %s" % (event.code, hex(event.code), event.value)
+
+    # Unreacheable return statement, to return the even, place a 'break' in the
+    # for loop
+    return event
