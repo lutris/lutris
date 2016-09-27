a00f661b5253bc1c3f55cc380330b22d9063c682
Mathieu Comandon <strycore@gmail.com>
2016-09-27 14:40:26 -0700

    Don't cast runner_slug to string if not provided

--- lutris/config.py
+++ lutris/config.py
@@ -127,7 +127,10 @@ class LutrisConfig(object):
     """
     def __init__(self, runner_slug=None, game_config_id=None, level=None):
         self.game_config_id = game_config_id
-        self.runner_slug = str(runner_slug)
+        if runner_slug:
+            self.runner_slug = str(runner_slug)
+        else:
+            self.runner_slug = runner_slug
 
         # Cascaded config sections (for reading)
         self.game_config = {}
