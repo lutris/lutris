bd02c4ec0996123b030c6f90d43fb3a982752995
Mathieu Comandon <strycore@gmail.com>
2016-09-27 18:35:48 -0700

    Rename variables in sync module

--- lutris/sync.py
+++ lutris/sync.py
@@ -14,16 +14,16 @@ def sync_missing_games(not_in_local, remote_library):
         return set()
 
     missing = []
-    for game in remote_library:
-        slug = game['slug']
+    for remote_game in remote_library:
+        slug = remote_game['slug']
         if slug in not_in_local:
             logger.debug("Adding to local library: %s", slug)
             missing.append({
-                'name': game['name'],
+                'name': remote_game['name'],
                 'slug': slug,
-                'year': game['year'],
-                'updated': game['updated'],
-                'steamid': game['steamid']
+                'year': remote_game['year'],
+                'updated': remote_game['updated'],
+                'steamid': remote_game['steamid']
             })
     missing_ids = pga.add_games_bulk(missing)
     logger.debug("%d games added", len(missing))
@@ -45,8 +45,8 @@ def sync_game_details(remote_library):
     # if not remote_games:
     #     return set()
 
-    for game in remote_library:
-        slug = game['slug']
+    for remote_game in remote_library:
+        slug = remote_game['slug']
         synced = False
         sync_icons = True
         local_game = pga.get_game_by_field(slug, 'slug')
@@ -54,15 +54,15 @@ def sync_game_details(remote_library):
             continue
 
         # Sync updated
-        if local_game['updated'] and game['updated'] > local_game['updated']:
+        if local_game['updated'] and remote_game['updated'] > local_game['updated']:
             synced = True
         # Sync new DB fields
         else:
             # XXX I have absolutely no idea what the code below does.
             for key, value in local_game.items():
-                if value or key not in game:
+                if value or key not in remote_game:
                     continue
-                if game[key]:
+                if remote_game[key]:
                     synced = True
                     sync_icons = False
         if not synced:
@@ -73,9 +73,9 @@ def sync_game_details(remote_library):
             name=local_game['name'],
             runner=local_game['runner'],
             slug=slug,
-            year=game['year'],
-            updated=game['updated'],
-            steamid=game['steamid']
+            year=remote_game['year'],
+            updated=remote_game['updated'],
+            steamid=remote_game['steamid']
         )
 
         # Sync icons (TODO: Only update if icon actually updated)
@@ -95,21 +95,21 @@ def sync_from_remote():
     :rtype: tuple of sets, added games and updated games
     """
     logger.debug("Syncing game library")
-    library = pga.get_games()
+
     # Get local library
-    local_slugs = set([game['slug'] for game in library])
-    logger.debug("%d games in local library", len(local_slugs))
+    local_library = pga.get_games()
+    local_slugs = set([game['slug'] for game in local_library])
+
     # Get remote library
     try:
         remote_library = api.get_library()
-    except Exception as e:
-        logger.debug("Error while downloading the remote library: %s" % e)
+    except Exception as ex:
+        logger.debug("Error while downloading the remote library: %s" % ex)
         remote_library = {}
     remote_slugs = set([game['slug'] for game in remote_library])
-    logger.debug("%d games in remote library (inc. unpublished)", len(remote_slugs))
 
-    not_in_local = remote_slugs.difference(local_slugs)
+    missing_slugs = remote_slugs.difference(local_slugs)
 
-    added = sync_missing_games(not_in_local, remote_library)
+    added = sync_missing_games(missing_slugs, remote_library)
     updated = sync_game_details(remote_library)
     return (added, updated)
