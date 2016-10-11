358238908a644a86731e8779d269a641fce79876
Mathieu Comandon <strycore@gmail.com>
2016-10-10 20:51:36 -0700

    Get rid of download_asset

--- lutris/util/resources.py
+++ lutris/util/resources.py
@@ -31,26 +31,6 @@ def has_icon(game, icon_type):
         return os.path.exists(icon_path)
 
 
-def download_asset(url, dest, overwrite=False, stop_request=None):
-    if os.path.exists(dest):
-        if overwrite:
-            os.remove(dest)
-        else:
-            logger.info("Destination %s exists, not overwriting" % dest)
-            return
-    # TODO: Downloading icons and banners makes a bunch of useless http
-    # requests + it's really slow
-
-    request = Request(url, stop_request=stop_request).get()
-    content = request.content
-    if content:
-        with open(dest, 'wb') as dest_file:
-            dest_file.write(content)
-        return True
-    else:
-        return False
-
-
 def fetch_icons(game_slugs, callback=None, stop_request=None):
     no_banners = []
     no_icons = []
@@ -62,22 +42,29 @@ def fetch_icons(game_slugs, callback=None, stop_request=None):
     for game in no_banners:
         if stop_request and stop_request.is_set():
             break
-        download_icon(slug, BANNER, callback=callback,
-                      stop_request=stop_request)
+        download_icon(slug, BANNER, callback=callback, stop_request=stop_request)
     for game in no_icons:
         if stop_request and stop_request.is_set():
             break
-        download_icon(slug, ICON, callback=callback,
-                      stop_request=stop_request)
+        download_icon(slug, ICON, callback=callback, stop_request=stop_request)
 
 
 def download_icon(game_slug, icon_type, overwrite=False, callback=None,
                   stop_request=None):
-    icon_url = get_icon_url(game_slug, icon_type)
-    icon_path = get_icon_path(game_slug, icon_type)
-    # XXX remove call to download_asset
-    icon_downloaded = http.download_asset(icon_url, icon_path, overwrite,
-                                          stop_request=stop_request)
-    if icon_downloaded and callback:
-        logger.debug("Downloaded %s for %s" % (icon_type, game_slug))
-        callback(game_slug)
+    url = get_icon_url(game_slug, icon_type)
+    dest = get_icon_path(game_slug, icon_type)
+
+    if os.path.exists(dest):
+        if overwrite:
+            os.remove(dest)
+        else:
+            return
+
+    request = Request(url, stop_request=stop_request).get()
+    content = request.content
+    if content:
+        with open(dest, 'wb') as dest_file:
+            dest_file.write(content)
+        if callback:
+            logger.debug("Downloaded %s for %s" % (icon_type, game_slug))
+            callback(game_slug)
