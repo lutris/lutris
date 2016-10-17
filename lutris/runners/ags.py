3c18f16abbb980312977fa41334f447a497e5294
Mathieu Comandon <strycore@gmail.com>
2016-10-17 11:09:25 -0700

    Add filter option to AGS

--- lutris/runners/ags.py
+++ lutris/runners/ags.py
@@ -11,7 +11,7 @@ class ags(Runner):
     game_options = [{
         'option': 'main_file',
         'type': 'file',
-        'label':  'blopblup '
+        'label':  'Game executable or directory'
     }]
     runner_options = [
         {
@@ -19,16 +19,34 @@ class ags(Runner):
             'type': 'bool',
             'label': 'Fullscreen',
             'default': True
+        },
+        {
+            'option': 'filter',
+            'type': 'choice',
+            'choices': [
+                ('None', 'none'),
+                ('Standard scaling', 'stdscale'),
+                ('HQ2x', 'hq2x'),
+                ('HQ3x', 'hq3x'),
+            ]
         }
     ]
 
     def play(self):
         """Run the game."""
-        arguments = [self.get_executable()]
-        if self.runner_config.get('fullscreen', True):
-            arguments.append('-f')
+
         main_file = self.game_config.get('main_file') or ''
         if not os.path.exists(main_file):
             return {'error': 'FILE_NOT_FOUND', 'file': main_file}
+
+        arguments = [self.get_executable()]
+        if self.runner_config.get('fullscreen', True):
+            arguments.append('--fullscreen')
+        else:
+            arguments.append('--windowed')
+        if self.runner_config.get('filter'):
+            arguments.append('--gfxfilter')
+            arguments.append(self.runner_config['filter'])
+
         arguments.append(main_file)
         return {"command": arguments}
