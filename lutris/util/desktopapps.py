c61ad7020666a1e1da2638ba32bd62560892f7a1
telanus <betelanus@gmail.com>
2017-03-03 18:04:18 +0200

    Update desktopapps.py

--- lutris/util/desktopapps.py
+++ lutris/util/desktopapps.py
@@ -14,7 +14,8 @@ from lutris.config import make_game_config_id, LutrisConfig
 
 IGNORED_GAMES = (
     "lutris", "mame", "dosbox", "playonlinux", "org.gnome.Games", "retroarch",
-    "steam", "steam-runtime", "steam-valve", "steam-native"
+    "steam", "steam-runtime", "steam-valve", "steam-native", "PlayOnLinux",
+    "fs-uae-arcade", "PCSX2", "ppsspp", "qchdman", "qmc2-sdlmame", "qmc2-arcade"
 )
 IGNORED_EXECUTABLES = (
     "lutris", "steam"
