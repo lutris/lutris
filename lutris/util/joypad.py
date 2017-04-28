4b899639c5e3a20ed52e1fa86344ad532cdacb9b
Mathieu Comandon <strycore@gmail.com>
2017-04-27 21:59:14 -0700

    Implement get_devices for evdev

--- lutris/util/joypad.py
+++ lutris/util/joypad.py
@@ -6,12 +6,15 @@ except ImportError:
     evdev = None
 
 
-def get_joypads():
-    """Return a list of tuples with the device and the joypad name"""
+def get_devices():
     if not evdev:
         return []
-    device_names = evdev.list_devices()
-    return [(dev, evdev.InputDevice(dev).name) for dev in device_names]
+    return [evdev.InputDevice(dev) for dev in evdev.list_devices()]
+
+
+def get_joypads():
+    """Return a list of tuples with the device and the joypad name"""
+    return [(dev.fn, dev.name) for dev in get_devices()]
 
 
 def read_button(device):
