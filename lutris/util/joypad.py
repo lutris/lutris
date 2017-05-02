fbdc8a4188668812bef6b7f29f316e0dd8f28393
Mathieu Comandon <strycore@gmail.com>
2017-05-02 05:44:58 -0700

    Connect gamecontrollerdb to x360ce configs

--- lutris/util/joypad.py
+++ lutris/util/joypad.py
@@ -5,9 +5,13 @@ try:
 except ImportError:
     evdev = None
 
+from lutris.util.log import logger
+from lutris.util.gamecontrollerdb import GameControllerDB
+
 
 def get_devices():
     if not evdev:
+        logger.warning("python3-evdev not installed, controller support not available")
         return []
     return [evdev.InputDevice(dev) for dev in evdev.list_devices()]
 
@@ -41,3 +45,17 @@ def get_sdl_identifier(device_info):
                                     device_info.product,
                                     device_info.version)
     return binascii.hexlify(device_identifier).decode()
+
+
+def get_controller_mappings():
+    devices = get_devices()
+    controller_db = GameControllerDB()
+
+    controllers = []
+
+    for device in devices:
+        guid = get_sdl_identifier(device.info)
+        if guid in controller_db.controllers:
+            controllers.append((device, controller_db[guid]))
+
+    return controllers
