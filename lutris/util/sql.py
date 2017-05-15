d8c0e38bd1ad5015d1137d407762f0757f0afad2
Mathieu Comandon <strycore@gmail.com>
2017-05-14 17:59:49 -0700

    Give more explicit name 'where' to argument in db_update

--- lutris/util/sql.py
+++ lutris/util/sql.py
@@ -61,14 +61,16 @@ def db_insert(db_path, table, fields):
     return inserted_id
 
 
-def db_update(db_path, table, updated_fields, row):
+def db_update(db_path, table, updated_fields, where):
     """Update `table` with the values given in the dict `values` on the
        condition given with the `row` tuple.
     """
     columns = "=?, ".join(list(updated_fields.keys())) + "=?"
     field_values = tuple(updated_fields.values())
-    condition_field = "{0}=?".format(row[0])
-    condition_value = (row[1], )
+
+    condition_field = "{0}=?".format(where[0])
+    condition_value = (where[1], )
+
     with db_cursor(db_path) as cursor:
         query = "UPDATE {0} SET {1} WHERE {2}".format(table, columns,
                                                       condition_field)
