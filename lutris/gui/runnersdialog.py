37f699ccc5d8c71662353d83537ed5f7f75dbf23
oleerik <github.eloelo@spamgourmet.com>
2017-05-19 16:28:07 +0200

    Add close button to dialog (571)

--- lutris/gui/runnersdialog.py
+++ lutris/gui/runnersdialog.py
@@ -59,6 +59,11 @@ class RunnersDialog(Gtk.Window):
         refresh_button.show()
         refresh_button.connect('clicked', self.on_refresh_clicked)
         buttons_box.pack_start(refresh_button, False, False, 10)
+        
+        close_button = Gtk.Button("Close")
+        close_button.show()
+        close_button.connect('clicked', self.on_close_clicked)
+        buttons_box.pack_start(close_button, False, False, 0)
 
         # Signals
         self.connect('destroy', self.on_destroy)
@@ -180,6 +185,9 @@ class RunnersDialog(Gtk.Window):
         for child in self.runner_listbox:
             child.destroy()
         self.populate_runners()
+        
+    def on_close_clicked(self, widget):
+        self.destroy()
 
     def set_install_state(self, widget, runner, runner_label):
         if runner.is_installed():
