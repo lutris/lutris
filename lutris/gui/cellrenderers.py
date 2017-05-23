5c5d960b42dfb0dab820ac81d56b67ac3d18be1a
Patrick Griffis <tingping@tingping.se>
2017-05-23 12:50:59 -0400

    Simplify GridViewCellRendererText

--- lutris/gui/cellrenderers.py
+++ lutris/gui/cellrenderers.py
@@ -3,11 +3,13 @@ from gi.repository import Gtk, Pango
 
 class GridViewCellRendererText(Gtk.CellRendererText):
     """CellRendererText adjusted for grid view display, removes extra padding"""
-    def __init__(self, width, *args, **kwargs):
-        super(GridViewCellRendererText, self).__init__(*args, **kwargs)
-        self.props.alignment = Pango.Alignment.CENTER
-        self.props.wrap_mode = Pango.WrapMode.WORD
-        self.props.xalign = 0.5
-        self.props.yalign = 0
-        self.props.width = width
-        self.props.wrap_width = width
+    def __init__(self, width, **kwargs):
+        super().__init__(
+            alignment=Pango.Alignment.CENTER,
+            wrap_mode=Pango.WrapMode.WORD,
+            xalign=0.5,
+            yalign=0,
+            width=width,
+            wrap_width=width,
+            **kwargs
+        )
