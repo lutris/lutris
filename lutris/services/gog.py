34aa393449d604c5f07ad1aa3e46e0ed6f4def68
Mathieu Comandon <strycore@gmail.com>
2017-05-22 22:31:18 -0700

    Move keyring example in own module

--- lutris/services/gog.py
+++ lutris/services/gog.py
@@ -1,10 +1,7 @@
 import os
-import gi
 import time
 import json
 from urllib.parse import urlencode, urlparse, parse_qsl
-gi.require_version('GnomeKeyring', '1.0')
-from gi.repository import GnomeKeyring
 from lutris import settings
 from lutris.util.http import Request
 from lutris.util.log import logger
@@ -126,25 +123,6 @@ class GogService:
 
     def get_download_info(self, downlink):
         return self.make_api_request(downlink)
-
-    def store_credentials(self, username, password):
-        # See
-        # https://bitbucket.org/kang/python-keyring-lib/src/8aadf61db38c70a5fe76fbe013df25fa62c03a8d/keyring/backends/Gnome.py?at=default # noqa
-        attrs = GnomeKeyring.Attribute.list_new()
-        GnomeKeyring.Attribute.list_append_string(attrs, 'username', username)
-        GnomeKeyring.Attribute.list_append_string(attrs, 'password', password)
-        GnomeKeyring.Attribute.list_append_string(attrs, 'application', 'Lutris')
-        result = GnomeKeyring.item_create_sync(
-            self.keyring_name, GnomeKeyring.ItemType.NETWORK_PASSWORD,
-            "%s credentials for %s" % (self.name, username),
-            attrs, password, True
-        )[0]
-        if result == GnomeKeyring.Result.CANCELLED:
-            # XXX
-            return False
-        elif result != GnomeKeyring.Result.OK:
-            # XXX
-            return False
 
 
 def is_connected():
