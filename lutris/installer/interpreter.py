e055d937d1337947cf6f42efea1076b9c2d32527
soredake <fdsfgs@krutt.org>
2017-12-03 10:30:58 +0200

    fix if order to allow extracting wine/winesteam data to fix #694

--- lutris/installer/interpreter.py
+++ lutris/installer/interpreter.py
@@ -257,14 +257,14 @@ class ScriptInterpreter(CommandsMixin):
         else:
             file_uri = game_file[file_id]
             filename = os.path.basename(file_uri)
-        if not filename:
-            raise ScriptingError("No filename provided, please provide 'url' and 'filename' parameters in the script")
         if file_uri.startswith("/"):
             file_uri = "file://" + file_uri
         elif file_uri.startswith(("$WINESTEAM", "$STEAM")):
             # Download Steam data
             self._download_steam_data(file_uri, file_id)
             return
+        if not filename:
+            raise ScriptingError("No filename provided, please provide 'url' and 'filename' parameters in the script")
 
         # Check for file availability in PGA
         pga_uri = pga.check_for_file(self.game_slug, file_id)
