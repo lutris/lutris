53606ce28eb8f0050ec47fbd7ed519d0e756020a
999gary <15350912+999gary@users.noreply.github.com>
2018-02-12 19:42:41 -0600

    Add rpcs3 runner
    Clean Code
    
    Forgot to add init

---
+++ lutris/runners/rpcs3.py
@@ -0,0 +1,20 @@
+from lutris.runners.runner import Runner
+
+
+class rpcs3(Runner):
+    human_name = "rpcs3"
+    description = "PlayStation 3 emulator"
+    platforms = ['Sony PlayStation 3']
+    runner_executable = 'rpcs3/bin/rpcs3'
+    game_options = [
+        {
+             	"option": "main_file",
+		"type": "file",
+		"default_path": "game_path",
+		"label": "EBOOT.BIN"
+        }
+    ]
+
+    def play(self):
+       
+        return {'command': [self.get_executable(), self.game_config.get('main_file')] }
