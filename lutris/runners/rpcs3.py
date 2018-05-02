a293198c2d528e5dbdd2ee1f023c377463cddd7e
Mathieu Comandon <strycore@gmail.com>
2018-05-02 13:54:19 -0700

    Fix path to rpcs3 executable

--- lutris/runners/rpcs3.py
+++ lutris/runners/rpcs3.py
@@ -5,16 +5,17 @@ class rpcs3(Runner):
     human_name = "rpcs3"
     description = "PlayStation 3 emulator"
     platforms = ['Sony PlayStation 3']
-    runner_executable = 'rpcs3/bin/rpcs3'
+    runner_executable = 'rpcs3/rpcs3'
     game_options = [
         {
-             	"option": "main_file",
-		"type": "file",
-		"default_path": "game_path",
-		"label": "EBOOT.BIN"
+            "option": "main_file",
+            "type": "file",
+            "default_path": "game_path",
+            "label": "EBOOT.BIN"
         }
     ]
 
     def play(self):
-       
-        return {'command': [self.get_executable(), self.game_config.get('main_file')] }
+        return {
+            'command': [self.get_executable(), self.game_config.get('main_file')]
+        }
