f06b4d634ac00114821996b83c1883bb82d34c63
Mathieu Comandon <strycore@gmail.com>
2018-05-16 15:54:40 -0700

    Handle xgamma not being installed on a system

--- lutris/util/display.py
+++ lutris/util/display.py
@@ -2,10 +2,12 @@ import re
 import time
 import subprocess
 
+from lutris.util.system import find_executable
 from lutris.util.log import logger
 
 XRANDR_CACHE = None
 XRANDR_CACHE_SET_AT = None
+XGAMMA_FOUND = False
 
 
 def cached(function):
@@ -144,7 +146,13 @@ def change_resolution(resolution):
 
 def restore_gamma():
     """Restores gamma to a normal level."""
-    subprocess.Popen(["xgamma", "-gamma", "1.0"])
+    global XGAMMA_FOUND
+    if XGAMMA_FOUND is None:
+        XGAMMA_FOUND = find_executable('xgamma')
+    if XGAMMA_FOUND is True:
+        subprocess.Popen(["xgamma", "-gamma", "1.0"])
+    else:
+        logger.warning('xgamma is not available on your system')
 
 
 def get_xrandr_version():
