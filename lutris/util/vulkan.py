3e910d940c818d3a4497fadd9dfa8b16d416e63f
Tom Todd <thomas.m.a.todd@gmail.com>
2018-10-21 16:02:30 +0100

    Added regex to check to allow for libvulkan.so.1 and similar as raised as an issue on mint and other distributions.

--- lutris/util/vulkan.py
+++ lutris/util/vulkan.py
@@ -1,5 +1,6 @@
 """Vulkan helper module"""
 import os
+import re
 from enum import Enum
 
 class vulkan_available(Enum):
@@ -8,11 +9,20 @@ class vulkan_available(Enum):
     SIXTY_FOUR = 2
     ALL = 3
 
+def search_for_file(directory):
+    if os.path.isdir(directory):
+        pattern = re.compile(r'^libvulkan\.so')
+        files = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
+        files = [os.path.join(directory, f) for f in files if pattern.search(f)]
+        if files:
+            return True
+    return False
+
 def vulkan_check():
-    vulkan_lib = os.path.isfile("/usr/lib/libvulkan.so")
-    vulkan_lib32 = os.path.isfile("/usr/lib32/libvulkan.so")
-    vulkan_lib_multi = os.path.isfile("/usr/lib/x86_64-linux-gnu/libvulkan.so")
-    vulkan_lib32_multi = os.path.isfile("/usr/lib32/i386-linux-gnu/libvulkan.so")
+    vulkan_lib = search_for_file("/usr/lib")
+    vulkan_lib32 = search_for_file("/usr/lib32")
+    vulkan_lib_multi = search_for_file("/usr/lib/x86_64-linux-gnu")
+    vulkan_lib32_multi =  search_for_file("/usr/lib32/i386-linux-gnu")
     has_32_bit = vulkan_lib32 or vulkan_lib32_multi
     has_64_bit = vulkan_lib or vulkan_lib_multi
 
