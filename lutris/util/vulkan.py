0892d95550d0247ff19e5b34982bd4044fdd5b8c
Tom Todd <thomas.m.a.todd@gmail.com>
2018-10-22 20:34:14 +0100

    Add call to ldconfig to check for libvulkan, keep file search as backup. Fix typo for 32 bit multilib check on file search.

--- lutris/util/vulkan.py
+++ lutris/util/vulkan.py
@@ -1,6 +1,8 @@
 """Vulkan helper module"""
 import os
 import re
+import subprocess
+import io
 from enum import Enum
 
 class vulkan_available(Enum):
@@ -19,12 +21,24 @@ def search_for_file(directory):
     return False
 
 def vulkan_check():
-    vulkan_lib = search_for_file("/usr/lib")
-    vulkan_lib32 = search_for_file("/usr/lib32")
-    vulkan_lib_multi = search_for_file("/usr/lib/x86_64-linux-gnu")
-    vulkan_lib32_multi =  search_for_file("/usr/lib32/i386-linux-gnu")
-    has_32_bit = vulkan_lib32 or vulkan_lib32_multi
-    has_64_bit = vulkan_lib or vulkan_lib_multi
+    has_32_bit = False
+    has_64_bit = False
+    pattern = re.compile(r'^libvulkan\.so')
+    proc = subprocess.Popen("ldconfig -p", shell=True, stdout=subprocess.PIPE)
+    for line in io.TextIOWrapper(proc.stdout, encoding="utf-8"):
+        if line.find('libvulkan') != -1:
+            if line.find('x86-64') != -1:
+                has_32_bit = True
+            else:
+                has_64_bit = True
+
+    if not (has_32_bit or has_64_bit):
+        vulkan_lib = search_for_file("/usr/lib")
+        vulkan_lib32 = search_for_file("/usr/lib32")
+        vulkan_lib_multi = search_for_file("/usr/lib/x86_64-linux-gnu")
+        vulkan_lib32_multi =  search_for_file("/usr/lib/i386-linux-gnu")
+        has_32_bit = vulkan_lib32 or vulkan_lib32_multi
+        has_64_bit = vulkan_lib or vulkan_lib_multi
 
     if not (has_64_bit or has_32_bit):
         return vulkan_available.NONE
