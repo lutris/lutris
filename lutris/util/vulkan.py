704949629fe352d1c6a37c076126e49ce02bd534
Tom Todd <thomas.m.a.todd@gmail.com>
2018-10-22 22:28:52 +0100

    Address issues raised

--- lutris/util/vulkan.py
+++ lutris/util/vulkan.py
@@ -11,34 +11,16 @@ class vulkan_available(Enum):
     SIXTY_FOUR = 2
     ALL = 3
 
-def search_for_file(directory):
-    if os.path.isdir(directory):
-        pattern = re.compile(r'^libvulkan\.so')
-        files = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
-        files = [os.path.join(directory, f) for f in files if pattern.search(f)]
-        if files:
-            return True
-    return False
-
 def vulkan_check():
-    has_32_bit = False
     has_64_bit = False
-    pattern = re.compile(r'^libvulkan\.so')
-    proc = subprocess.Popen("ldconfig -p", shell=True, stdout=subprocess.PIPE)
-    for line in io.TextIOWrapper(proc.stdout, encoding="utf-8"):
-        if line.find('libvulkan') != -1:
-            if line.find('x86-64') != -1:
+    has_32_bit = False
+    for line in subprocess.check_output(["ldconfig", "-p"]).splitlines():
+        line = str(line)
+        if 'libvulkan' in line:
+            if 'x86-64' in line:
                 has_32_bit = True
             else:
                 has_64_bit = True
-
-    if not (has_32_bit or has_64_bit):
-        vulkan_lib = search_for_file("/usr/lib")
-        vulkan_lib32 = search_for_file("/usr/lib32")
-        vulkan_lib_multi = search_for_file("/usr/lib/x86_64-linux-gnu")
-        vulkan_lib32_multi =  search_for_file("/usr/lib/i386-linux-gnu")
-        has_32_bit = vulkan_lib32 or vulkan_lib32_multi
-        has_64_bit = vulkan_lib or vulkan_lib_multi
 
     if not (has_64_bit or has_32_bit):
         return vulkan_available.NONE
