7df3ab4811edbe080ccbd9df6b9f165c26485b49
Nbiba Bedis <bedisnbiba@gmail.com>
2018-10-23 11:26:24 +0100

    add playtime

---
+++ lutris/util/timer.py
@@ -0,0 +1,21 @@
+import datetime
+
+
+class Timer:
+
+    def start_t(self):
+        self.start = datetime.datetime.now()
+
+    def end_t(self):
+        self.end = datetime.datetime.now()
+
+    def duration(self):
+        return (self.end - self.start).seconds / 3600
+
+    def increment(self, saved_dur):
+        if saved_dur == '':
+            return "%.1f hrs" % self.duration()
+
+        saved_dur = float(saved_dur.split()[0])
+        new_dur = saved_dur + self.duration()
+        return "%.1f hrs" % new_dur
