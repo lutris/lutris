ca156a3fef7418e78fb0ca4ca387e9f95d0eb4a8
telanus <betelanus@gmail.com>
2018-10-27 16:05:43 +0200

    Add files via upload

---
+++ lutris/runners/melonds.py
@@ -0,0 +1,23 @@
+import os
+from lutris.runners.runner import Runner
+
+
+class melonds(Runner):
+    human_name = "melonDS"
+    description = "Nintendo DS Emulator"
+    platforms = ['melonDS']
+    runner_executable = 'melonDS/melonDS'
+    game_options = [
+    	{
+            'option': 'main_file',
+            'type': 'file',
+            'label': 'ROM file',
+            'default_path': 'game_path'
+        }
+    ]
+
+    def play(self):
+        rom = self.game_config.get('main_file') or ''
+        if not os.path.exists(rom):
+            return {'error': 'FILE_NOT_FOUND', 'file': rom}
+        return {'command': [self.get_executable(), rom]}
