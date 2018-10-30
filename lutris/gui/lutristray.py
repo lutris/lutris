0d6f87de23b24e3375af221880a7772e1fa09da0
Mathieu Comandon <strycore@gmail.com>
2018-10-29 20:24:28 -0700

    Cleanup lutristray

--- lutris/gui/lutristray.py
+++ lutris/gui/lutristray.py
@@ -8,7 +8,7 @@ from lutris.gui.widgets.utils import get_runner_icon
 
 class LutrisTray(Gtk.StatusIcon):
     """Lutris tray icon"""
-    def __init__(self, application, **kwargs):
+    def __init__(self, application, **_kwargs):
         super().__init__()
         self.set_tooltip_text('Lutris')
         self.set_visible(True)
@@ -22,7 +22,7 @@ class LutrisTray(Gtk.StatusIcon):
         self.load_menu()
 
         self.connect('activate', self.on_activate)
-        self.connect('popup-menu', self.on_right_click)
+        self.connect('popup-menu', self.on_menu_popup)
 
     def load_menu(self):
         """Instanciates the menu attached to the tray icon"""
@@ -34,30 +34,35 @@ class LutrisTray(Gtk.StatusIcon):
 
         quit_menu = Gtk.MenuItem()
         quit_menu.set_label("Quit")
-        quit_menu.connect("activate", self.quit_application)
+        quit_menu.connect("activate", self.on_quit_application)
         self.menu.append(quit_menu)
         self.menu.show_all()
 
-    def on_activate(self, _widget, _event=None):
+    def on_activate(self, _status_icon, _event=None):
         """Callback to show or hide the window"""
         self.application.window.present()
 
-    def on_right_click(self, status, button, time):
+    def on_menu_popup(self, _status_icon, button, time):
+        """Callback to show the contextual menu"""
         self.menu.popup(None, None, None, None, button, time)
 
-    def quit_application(self, _widget):
+    def on_quit_application(self, _widget):
+        """Callback to quit the program"""
         self.application.do_shutdown()
 
     def add_runners(self):
+        """?"""
         self.installed_runners = runners.get_installed()
         for runner in self.installed_runners:
             menu_item = Gtk.ImageMenuItem()
             menu_item.set_label(runner.human_name)
-            menu_item.set_image(Gtk.Image.new_from_pixbuf(get_runner_icon(runner.name, format='pixbuf', size=(16, 16))))
+            runner_icon = get_runner_icon(runner.name, format='pixbuf', size=(16, 16))
+            menu_item.set_image(Gtk.Image.new_from_pixbuf(runner_icon))
             menu_item.connect('activate', self.on_runner_selected, runner.name)
             self.menu.append(menu_item)
 
     def add_platforms(self):
+        """?"""
         self.active_platforms = pga.get_used_platforms()
         for platform in self.active_platforms:
             menu_item = Gtk.MenuItem()
@@ -65,12 +70,14 @@ class LutrisTray(Gtk.StatusIcon):
             menu_item.connect('activate', self.on_platform_selected, platform)
             self.menu.append(menu_item)
 
-    def on_runner_selected(self, widget, *data):
+    def on_runner_selected(self, _widget, *data):
+        """?"""
         selected_runner = data[0]
         self.application.window.set_selected_filter(selected_runner, None)
         self.application.window.present()
 
-    def on_platform_selected(self, widget, *data):
+    def on_platform_selected(self, _widget, *data):
+        """?"""
         selected_platform = data[0]
         self.application.window.set_selected_filter(None, selected_platform)
         self.application.window.present()
