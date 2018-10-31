lutris/lutris

commit c8f253093b1a76074264c6d02876cb745d74a16d

Cast 'true' and 'false' to booleans (Fixes #1245)

lutris/installer/interpreter.py
@@ -698,6 +698,11 @@ class ScriptInterpreter(CommandsMixin):
 
     def _substitute(self, template_string):
         """Replace path aliases with real paths."""
+        if template_string.lower() == 'true':
+            return True
+        if template_string.lower() == 'false':
+            return False
+
         replacements = {
             "GAMEDIR": self.target_path,
             "CACHE": self.cache_path,

commit 9a40b716ba7c578c4ab02e854cf3736a69ff302c

Fix exception after steam game install
This fixes a regression introduced by c8f25309

lutris/installer/interpreter.py
@@ -666,6 +666,8 @@ class ScriptInterpreter(CommandsMixin):
                 config[key] = {k: self._substitute(v) for (k, v) in value.items()}
             elif isinstance(value, bool):
                 config[key] = value
+            elif isinstance(value, int):
+                config[key] = value
             else:
                 config[key] = self._substitute(value)
         return config