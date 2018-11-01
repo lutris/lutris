35960a43d4da11bc0d5eefba8742802dec98ca96
Mathieu Comandon <strycore@gmail.com>
2018-11-01 11:21:49 -0700

    Remove runners and platforms from the tray menu

--- lutris/gui/lutristray.py
+++ lutris/gui/lutristray.py
@@ -15,8 +15,6 @@ class LutrisTray(Gtk.StatusIcon):
         self.application = application
         self.set_from_icon_name('lutris')
 
-        self.installed_runners = None
-        self.active_platforms = None
         self.menu = None
 
         self.load_menu()
@@ -28,10 +26,6 @@ class LutrisTray(Gtk.StatusIcon):
         """Instanciates the menu attached to the tray icon"""
         self.menu = Gtk.Menu()
         self.add_games()
-        self.menu.append(Gtk.SeparatorMenuItem())
-        self.add_runners()
-        self.menu.append(Gtk.SeparatorMenuItem())
-        self.add_platforms()
         self.menu.append(Gtk.SeparatorMenuItem())
 
         quit_menu = Gtk.MenuItem()
@@ -70,37 +64,5 @@ class LutrisTray(Gtk.StatusIcon):
         for game in installed_games[:number_of_games_in_menu]:
             self.menu.append(self._make_menu_item_for_game(game))
 
-    def add_runners(self):
-        """Why the hell"""
-        self.installed_runners = runners.get_installed()
-        for runner in self.installed_runners:
-            menu_item = Gtk.ImageMenuItem()
-            menu_item.set_label(runner.human_name)
-            runner_icon = get_runner_icon(runner.name, format='pixbuf', size=(16, 16))
-            menu_item.set_image(Gtk.Image.new_from_pixbuf(runner_icon))
-            menu_item.connect('activate', self.on_runner_selected, runner.name)
-            self.menu.append(menu_item)
-
-    def add_platforms(self):
-        """No"""
-        self.active_platforms = pga.get_used_platforms()
-        for platform in self.active_platforms:
-            menu_item = Gtk.MenuItem()
-            menu_item.set_label(platform)
-            menu_item.connect('activate', self.on_platform_selected, platform)
-            self.menu.append(menu_item)
-
     def on_game_selected(self, _widget, *data):
         self.application.window.on_game_run(game_id=data[0])
-
-    def on_runner_selected(self, _widget, *data):
-        """WTF"""
-        selected_runner = data[0]
-        self.application.window.set_selected_filter(selected_runner, None)
-        self.application.window.present()
-
-    def on_platform_selected(self, _widget, *data):
-        """WTH"""
-        selected_platform = data[0]
-        self.application.window.set_selected_filter(None, selected_platform)
-        self.application.window.present()
