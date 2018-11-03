a833eafcff1c5d765f83694468b27c72b8c3990e
Mathieu Comandon <strycore@gmail.com>
2018-11-03 00:03:06 -0700

    Update version to 0.4.22

--- lutris/settings.py
+++ lutris/settings.py
@@ -5,7 +5,7 @@ from gi.repository import GLib
 from lutris.util.settings import SettingsIO
 
 PROJECT = "Lutris"
-VERSION = "0.4.21.1"
+VERSION = "0.4.22"
 COPYRIGHT = "(c) 2010-2018 Lutris Gaming Platform"
 AUTHORS = ["Mathieu Comandon <strycore@gmail.com>",
            "Pascal Reinhard (Xodetaetl) <dev@xod.me"]
