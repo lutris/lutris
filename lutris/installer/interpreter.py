96a78ffafa6dc79a9d5fccbca0e73580d2030923
Mathieu Comandon <strycore@gmail.com>
2018-11-18 17:53:10 -0800

    Don't make GOG field mandatory

--- lutris/installer/interpreter.py
+++ lutris/installer/interpreter.py
@@ -166,7 +166,7 @@ class ScriptInterpreter(CommandsMixin):
         if self.runner in ('steam', 'winesteam'):
             # Steam games installs in their steamapps directory
             return False
-        if self.files or self.script['game']['gog']:
+        if self.files or self.script.get('game', {}).get('gog'):
             return True
         command_names = [list(c.keys())[0] for c in self.script.get('installer', [])]
         if 'insert-disc' in command_names:
