5f179bb2914009e428db4e4977d7b94c76dda177
Mathieu Comandon <strycore@gmail.com>
2018-12-04 20:04:12 -0800

    Add note about revision URL

--- lutris/settings.py
+++ lutris/settings.py
@@ -31,7 +31,7 @@ PGA_DB = sio.read_setting("pga_path") or os.path.join(DATA_DIR, "pga.db")
 SITE_URL = sio.read_setting("website") or "https://lutris.net"
 
 INSTALLER_URL = SITE_URL + "/api/installers/%s"
-INSTALLER_REVISION_URL = SITE_URL + "/api/installers/games/%s/revisions/%s"
+INSTALLER_REVISION_URL = SITE_URL + "/api/installers/games/%s/revisions/%s"  # XXX change this, should query on the installer, not the game.
 GAME_URL = SITE_URL + "/games/%s/"
 ICON_URL = SITE_URL + "/games/icon/%s.png"
 BANNER_URL = SITE_URL + "/games/banner/%s.jpg"
