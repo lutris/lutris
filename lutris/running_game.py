b2613ec6b7aa7ab495b911e631a8560214f672fc
Mathieu Comandon <strycore@gmail.com>
2018-12-14 20:06:10 -0800

    Re-implement stop and show logs

--- lutris/running_game.py
+++ lutris/running_game.py
@@ -2,7 +2,18 @@ from gi.repository import Gtk
 from lutris.util.log import logger
 from lutris.game import Game
 from lutris.gui.installerwindow import InstallerWindow
+from lutris.gui.logdialog import LogDialog
 
+class RunningGameBox(Gtk.Box):
+    def __init__(self):
+        super().__init__(self, spacing=6, homogeneous=False)
+        label = Gtk.Label("It's a-me! the lutris game!")
+        self.pack_start(label, True, True, 0)
+        self.stop_button = Gtk.Button.new_from_icon_name("media-playback-stop-symbolic", Gtk.IconSize.MENU)
+        self.pack_start(self.stop_button, False, False, 0)
+        self.log_button = Gtk.Button.new_from_icon_name("accessories-text-editor-symbolic", Gtk.IconSize.MENU)
+        self.pack_start(self.log_button, False, False, 0)
+        self.show_all()
 
 class RunningGame:
     """Class for manipulating running games"""
@@ -21,16 +32,22 @@ class RunningGame:
             InstallerWindow(
                 game_slug=self.game.slug, parent=self.window, application=self.application
             )
-        label = Gtk.Label("It's a-me! the lutris game!")
-        label.show()
-        self.window.infobar_box.pack_start(label, True, True, 0)
+        running_game_box = RunningGameBox()
+        running_game_box.stop_button.connect("clicked", self.on_stop)
+        running_game_box.log_button.connect("clicked", self.on_show_logs)
+        self.window.infobar_box.pack_start(running_game_box, True, True, 0)
         self.window.infobar_revealer.set_reveal_child(True)
         self.game.play()
 
-    def stop(self):
+    def on_stop(self, _widget):
         """Stops the game"""
-        raise NotImplementedError
+        self.game.stop()
 
-    def show_logs(self):
+    def on_show_logs(self, _widget):
         """Display game log in a LogWindow"""
-        raise NotImplementedError
+        log_title = u"Log for {}".format(self.game)
+        log_window = LogDialog(
+            title=log_title, buffer=self.game.log_buffer, parent=self.window
+        )
+        log_window.run()
+        log_window.destroy()
