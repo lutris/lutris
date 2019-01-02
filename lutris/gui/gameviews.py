6085f0f29fd89fdcfcb38877c5d26054bb517ced
Mathieu Comandon <strycore@gmail.com>
2019-01-02 14:50:48 -0800

    Handle bad playtime values (doesn't fix anything)

--- lutris/gui/gameviews.py
+++ lutris/gui/gameviews.py
@@ -323,11 +323,14 @@ class GameView:
         if row:
             row[COL_YEAR] = str(game_year)
             if game_playtime:
-                try:
-                    game_playtime = "%.1f hrs" % game_playtime
-                except TypeError:
-                    logger.exception("Failed to parse playtime %s", game_playtime)
-                    game_playtime = "-"
+                if isinstance(game_playtime, str) and game_playtime.endswith("hrs"):
+                    logger.debug("This is messed up and needs to be sorted out")
+                else:
+                    try:
+                        game_playtime = "%.1f hrs" % game_playtime
+                    except TypeError:
+                        logger.exception("Failed to parse playtime %s", game_playtime)
+                        game_playtime = "-"
             else:
                 game_playtime = "-"
             row[COL_PLAYTIME_TEXT] = game_playtime
