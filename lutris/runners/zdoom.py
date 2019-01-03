64c16be9e80a450cdd0c0cc80951890b3e5942ab
Mathieu Comandon <strycore@gmail.com>
2019-01-03 00:22:10 -0800

    Remove COMMAND_CACHE left in zdoom

--- lutris/runners/zdoom.py
+++ lutris/runners/zdoom.py
@@ -97,7 +97,7 @@ class zdoom(Runner):
         return executable
 
     def prelaunch(self):
-        if not system.COMMAND_CACHE.get_soundfonts():
+        if not system.LINUX_SYSTEM.get_soundfonts():
             logger.warning("FluidSynth is not installed, you might not have any music")
         return True
 
