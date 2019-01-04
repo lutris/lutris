1ef6c400cb13fdbf67dffb570d3fd051eeb854cc
Mathieu Comandon <strycore@gmail.com>
2019-01-04 05:30:34 -0800

    Add notification module

---
+++ lutris/gui/notifications.py
@@ -0,0 +1,21 @@
+import gi
+NOTIFY_SUPPORT = True
+try:
+    gi.require_version('Notify', '0.7')
+
+    from gi.repository import GObject
+    from gi.repository import Notify
+except:
+    NOTIFY_SUPPORT = False
+from lutris.util.log import logger
+
+if NOTIFY_SUPPORT:
+    Notify.init("lutris")
+
+def send_notification(title, text, file_path_to_icon=""):
+    if NOTIFY_SUPPORT:
+        notification = Notify.Notification.new(title, text, file_path_to_icon)
+        notification.show()
+    else:
+        logger.info(title)
+        logger.info(text)
