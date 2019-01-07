acdfd0060852f2232a2ad0e16e6c2688511a9cfa
telanus <betelanus@gmail.com>
2019-01-07 05:17:20 +0200

    Added more command-line switches

--- lutris/runners/fsuae.py
+++ lutris/runners/fsuae.py
@@ -28,6 +28,50 @@ class fsuae(Runner):
         ("Amiga CD32", "CD32"),
         ("Commodore CDTV", "CDTV"),
     ]
+    memory_choices = [
+        ("0", "0"),
+        ("1 MB", "1024"),
+        ("2 MB", "2048"),
+        ("4 MB", "4096"),
+        ("8 MB", "8192"),
+    ]
+    flsound_choices = [
+        ("0", "0"),
+        ("25", "25"),
+        ("50", "50"),
+        ("75", "75"),
+        ("100", "100"),
+    ]
+    gpucard_choices = [
+        ("None", "None"),
+        ("UAEGFX", "uaegfx"),
+        ("UAEGFX Zorro II", "uaegfx-z2"),
+        ("UAEGFX Zorro III", "uaegfx-z3"),
+        ("Picasso II Zorro II", "picasso-ii"),
+        ("Picasso II+ Zorro II", "picasso-ii+"),
+        ("Picasso IV", "picasso-iv"),
+        ("Picasso IV Zorro II", "picasso-iv-z2"),
+        ("Picasso IV Zorro III", "picasso-iv-z3"),
+    ]
+    gpumem_choices = [
+        ("0", "0"),
+        ("1 MB", "1024"),
+        ("2 MB", "2048"),
+        ("4 MB", "4096"),
+        ("8 MB", "8192"),
+        ("16 MB", "16384"),
+        ("32 MB", "32768"),
+        ("64 MB", "65536"),
+        ("128 MB", "131072"),
+        ("256 MB", "262144"),
+    ]
+    flspeed_choices = [
+        ("Turbo", "0"),
+        ("100%", "100"),
+        ("200%", "200"),
+        ("400%", "400"),
+        ("800%", "800"),
+    ]
     runner_executable = "fs-uae/fs-uae"
     game_options = [
         {
@@ -80,8 +124,60 @@ class fsuae(Runner):
             "help": "Location of extended Kickstart used for CD32",
         },
         {
+            "option": "fmemory",
+            "label": "Fast Memory",
+            "type": "choice",
+            "choices": memory_choices,
+            "default": "0",
+            "help": "Specify how much Fast Memory the Amiga model should have.",
+        },
+        {
+            "option": "fdvolume",
+            "label": "Floppy Drive Volume",
+            "type": "choice",
+            "choices": flsound_choices,
+            "default": "0",
+            "help": "Set volume to 0 to disable floppy drive clicks when the drive is empty. Max volume is 100.",
+        },
+        {
+            "option": "fdspeed",
+            "label": "Floppy Drive Speed",
+            "type": "choice",
+            "choices": flspeed_choices,
+            "default": "100",
+            "help": "Set the speed of the emulated floppy drives, in percent. For example, you can specify 800 to get an 8x increase in speed. Use 0 to specify turbo mode. Turbo mode means that all floppy operations complete immediately. The default is 100 for most models.",
+        },
+        {
+            "option": "grafixcard",
+            "label": "Graphics Card",
+            "type": "choice",
+            "choices": gpucard_choices,
+            "default": "0",
+            "help": "Use this option to enable a graphics card. This option is none by default, in which case only chipset graphics (OCS/ECS/AGA) support is available.",
+        },
+        {
+            "option": "grafixmemory",
+            "label": "Graphics Card RAM",
+            "type": "choice",
+            "choices": gpumem_choices,
+            "default": "0",
+            "help": "Override the amount of graphics memory on the graphics card. The 0 MB option is not really valid, but exists for user interface reasons.",
+        },
+        {
             "option": "gfx_fullscreen_amiga",
             "label": "Fullscreen (F12 + s to switch)",
+            "type": "bool",
+            "default": False,
+        },
+        {
+            "option": "jitcompiler",
+            "label": "JIT Compiler",
+            "type": "bool",
+            "default": False,
+        },
+        {
+            "option": "bsdsocket",
+            "label": "UAE bsdsocket.library",
             "type": "bool",
             "default": False,
         },
@@ -103,6 +199,46 @@ class fsuae(Runner):
             for index, machine in enumerate(self.model_choices):
                 if machine[1] == model:
                     return self.platforms[index]
+        return ""
+
+    def get_memory(self):
+        fmemory = self.runner_config.get("fmemory")
+        if fmemory:
+            for index, fmemory1 in enumerate(self.memory_choices):
+                if fmemory1[1] == fmemory:
+                    return self.mem0ry[index]
+        return ""
+
+    def get_flsound(self):
+        fdvolume = self.runner_config.get("fdvolume")
+        if fdvolume:
+            for index, fdvolume1 in enumerate(self.flsound_choices):
+                if fdvolume1[1] == fdvolume:
+                    return self.flsound[index]
+        return ""
+
+    def get_flspeed(self):
+        fdspeed = self.runner_config.get("fdspeed")
+        if fdspeed:
+            for index, fdspeed1 in enumerate(self.flspeed_choices):
+                if fdspeed1[1] == fdspeed:
+                    return self.flspeed[index]
+        return ""
+
+    def get_gpucard(self):
+        grafixcard = self.runner_config.get("grafixcard")
+        if grafixcard:
+            for index, grafixcard1 in enumerate(self.gpucard_choices):
+                if grafixcard1[1] == grafixcard:
+                    return self.gpucard[index]
+        return ""
+
+    def get_gpumem(self):
+        grafixmemory = self.runner_config.get("grafixmemory")
+        if grafixmemory:
+            for index, grafixmemory1 in enumerate(self.gpumem_choices):
+                if grafixmemory1[1] == grafixmemory:
+                    return self.gpumem[index]
         return ""
 
     def insert_floppies(self):
@@ -140,6 +276,11 @@ class fsuae(Runner):
     def get_params(self):
         params = []
         model = self.runner_config.get("model")
+        fmemory = self.runner_config.get("fmemory")
+        fdvolume = self.runner_config.get("fdvolume")
+        fdspeed = self.runner_config.get("fdspeed")
+        grafixcard = self.runner_config.get("grafixcard")
+        grafixmemory = self.runner_config.get("grafixmemory")
         kickstart_file = self.runner_config.get("kickstart_file")
         if kickstart_file:
             params.append("--kickstart_file=%s" % kickstart_file)
@@ -148,12 +289,26 @@ class fsuae(Runner):
             params.append("--kickstart_ext_file=%s" % kickstart_ext_file)
         if model:
             params.append("--amiga_model=%s" % model)
+        if fmemory:
+            params.append("--fast_memory=%s" % fmemory)
+        if fdvolume:
+            params.append("--floppy_drive_volume_empty=%s" % fdvolume)
+        if fdspeed:
+            params.append("--floppy_drive_volume_empty=%s" % fdspeed)
+        if grafixcard:
+            params.append("--graphics_card=%s" % grafixcard)
+        if grafixmemory:
+            params.append("--graphics_card=%s" % grafixmemory)
         if self.runner_config.get("gfx_fullscreen_amiga"):
             width = int(get_current_resolution().split("x")[0])
             params.append("--fullscreen")
             # params.append("--fullscreen_mode=fullscreen-window")
             params.append("--fullscreen_mode=fullscreen")
             params.append("--fullscreen_width=%d" % width)
+        if self.runner_config.get("jitcompiler"):
+            params.append("--jit_compiler=1")
+        if self.runner_config.get("jitcompiler"):
+            params.append("--bsdsocket_library=1")
         if self.runner_config.get("scanlines"):
             params.append("--scanlines=1")
         return params
