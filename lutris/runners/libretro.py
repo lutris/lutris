7a1a807c526edba86eb1f9aab2e5e3bb51f8c180
telanus <betelanus@gmail.com>
2019-04-09 20:28:27 +0200

    BlastEm got libretro support
    https://www.retrodev.com/repos/blastem/rev/8500bd987dda

--- lutris/runners/libretro.py
+++ lutris/runners/libretro.py
@@ -12,6 +12,7 @@ from lutris import settings
 LIBRETRO_CORES = [
     ("4do (3DO)", "4do", "3DO"),
     ("atari800 (Atari 800/5200)", "atari800", "Atari 800/5200"),
+    ("BlastEm (Sega Genesis)", "blastem", "Sega Genesis"),
     ("blueMSX (MSX/MSX2/MSX2+)", "bluemsx", "MSX/MSX2/MSX2+"),
     ("Caprice32 (Amstrad CPC)", "cap32", "Amstrad CPC"),
     ("ChaiLove", "chailove", "ChaiLove"),
