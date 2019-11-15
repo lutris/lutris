7e9d00d6709d9db61beed149735818b30e154b2a
Mathieu Comandon <strycore@gmail.com>
2019-11-14 19:53:24 -0800

    Use different normalization method to remove TM symbols from slugs (Closes #2380)

--- lutris/util/strings.py
+++ lutris/util/strings.py
@@ -14,7 +14,12 @@ def slugify(value):
     and converts spaces to hyphens.
     """
     value = str(value)
-    value = unicodedata.normalize("NFKD", value).encode("ascii", "ignore")
+    # This differs from the Lutris website implementation which uses the Django
+    # version of `slugify` and uses the "NFKD" normalization method instead of
+    # "NFD". This creates some inconsistencies in titles containing a trademark
+    # symbols or some other special characters. The website version of slugify
+    # will likely get updated to use the same normalization method.
+    value = unicodedata.normalize("NFD", value).encode("ascii", "ignore")
     value = value.decode("utf-8")
     value = str(re.sub(r"[^\w\s-]", "", value)).strip().lower()
     return re.sub(r"[-\s]+", "-", value)
