a935e53e427e4899c985b470820ff7857f790cc9
Stephan Lachnit <stephanlachnit@protonmail.com>
2019-11-27 21:50:28 +0100

    Add option to use system winetricks
    Signed-off-by: Stephan Lachnit <stephanlachnit@protonmail.com>

--- lutris/runners/commands/wine.py
+++ lutris/runners/commands/wine.py
@@ -337,12 +337,17 @@ def winetricks(
         disable_runtime=False,
 ):
     """Execute winetricks."""
-    winetricks_path = os.path.join(settings.RUNTIME_DIR, "winetricks/winetricks")
-    if not system.path_exists(winetricks_path):
-        logger.warning(
-            "Could not find local winetricks install, falling back to bundled version"
-        )
-        winetricks_path = os.path.join(datapath.get(), "bin/winetricks")
+    wine_config = config or LutrisConfig(runner_slug="wine")
+    system_winetricks = wine_config.get("system_winetricks")
+    if system_winetricks:
+        winetricks_path = "usr/bin/winetricks"
+    else:
+        winetricks_path = os.path.join(settings.RUNTIME_DIR, "winetricks/winetricks")
+        if not system.path_exists(winetricks_path):
+            logger.warning(
+                "Could not find local winetricks install, falling back to bundled version"
+            )
+            winetricks_path = os.path.join(datapath.get(), "bin/winetricks")
     if wine_path:
         winetricks_wine = wine_path
     else:
