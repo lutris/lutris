c703140b9c14b39bbe596ad39b101d767620826d
Mathieu Comandon <strycore@gmail.com>
2020-01-01 17:55:53 -0800

    Remove hardcoded ignored process list

--- lutris/util/monitor.py
+++ lutris/util/monitor.py
@@ -4,45 +4,10 @@ import shlex
 
 from lutris.util.process import Process
 
-
-# List of process names that are not considered game processes. A game
-# is not "running" until a process that isn't one of the following
-# (or a system process; see below) belongs in its process tree.
-# Processes in this list will be sent SIGTERM when the game has exited
-
-# FIXME don't ignore any process, makes launching Steam possible
-EXCLUDED_PROCESSES = []
-_EXCLUDED_PROCESSES = [
-    "lutris",
-    "python",
-    "python3",
-    "tee",
-    "tr",
-    "zenity",
-    "xkbcomp",
-    "xboxdrv",
-    "steam",
-    "Steam.exe",
-    "steamer",
-    "steamerrorrepor",
-    "gameoverlayui",
-    "SteamService.ex",
-    "steamwebhelper",
-    "steamwebhelper.",
-    "PnkBstrA.exe",
-    "control",
-    "explorer.exe",
-    "winecfg.exe",
-    "wdfmgr.exe",
-    "wineconsole",
-    "winedbg",
-]
-
 # Processes that are considered sufficiently self-managing by the
-# monitoring system. These are not considered game processes for the
-# purpose of determining if a game is still running (just like the
-# EXCLUDED_PROCESSES above) and Lutris will never attempt to send
-# signals to these processes.
+# monitoring system. These are not considered game processes for
+# the purpose of determining if a game is still running and Lutris
+# will never attempt to send signals to these processes.
 # This is mostly a minor UX improvement where wine games will exit
 # faster if we let the wine processes tear themselves down.
 SYSTEM_PROCESSES = {
@@ -59,7 +24,7 @@ class ProcessMonitor:
     def __init__(self, include_processes, exclude_processes):
         """Creates a process monitor
 
-        All arguments accept process names like the ones in EXCLUDED_PROCESSES
+        All arguments accept a list of process names
 
         Args:
             exclude_processes (str or list): list of processes that shouldn't be monitored
@@ -69,19 +34,19 @@ class ProcessMonitor:
         include_processes = {
             x[0:15] for x in self.parse_process_list(include_processes)
         }
-        self.exclude_processes = {
-            x[0:15] for x in EXCLUDED_PROCESSES + self.parse_process_list(exclude_processes)
+        exclude_processes = {
+            x[0:15] for x in self.parse_process_list(exclude_processes)
         }
-        self.nongame_processes = (self.exclude_processes | SYSTEM_PROCESSES) - include_processes
+        self.nongame_processes = (exclude_processes | SYSTEM_PROCESSES) - include_processes
 
     @staticmethod
     def parse_process_list(process_list):
         """Parse a process list that may be given as a string"""
         if not process_list:
-            return []
+            return set()
         if isinstance(process_list, str):
-            return shlex.split(process_list)
-        return process_list
+            process_list = shlex.split(process_list)
+        return {p[0:15] for p in process_list}
 
     def iterate_game_processes(self):
         for child in self.iterate_all_processes():
