8f31fb015034199fe8654b995559e93883bc6c3e
Mathieu Comandon <strycore@gmail.com>
2020-03-17 22:14:26 -0700

    Disable winemenubuilder globally for the wine runner (Closes #2466)

--- lutris/runners/wine.py
+++ lutris/runners/wine.py
@@ -126,7 +126,9 @@ class wine(Runner):
 
     def __init__(self, config=None):
         super(wine, self).__init__(config)
-        self.dll_overrides = {}
+        self.dll_overrides = {
+            "winemenubuilder.exe": "d"
+        }
 
         def get_wine_version_choices():
             version_choices = [("Custom (select executable below)", "custom")]
@@ -806,13 +808,11 @@ class wine(Runner):
             overrides = self.runner_config["overrides"]
         except KeyError:
             overrides = {}
-        else:
-            if not isinstance(overrides, dict):
-                logger.warning("DLL overrides is not a mapping: %s", overrides)
-                overrides = {}
-            else:
-                overrides = overrides.copy()
+        if not isinstance(overrides, dict):
+            logger.warning("DLL overrides is not a mapping: %s", overrides)
+            overrides = {}
 
+        overrides = overrides.copy()
         overrides.update(self.dll_overrides)
         return overrides
 
