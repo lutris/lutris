0581612215c5aa4ccb136617c7e3f47c21a4d955
Mathieu Comandon <strycore@gmail.com>
2020-04-21 15:19:08 -0700

    Deprecate MESS runner

--- lutris/migrations/mess_to_mame.py
+++ lutris/migrations/mess_to_mame.py
@@ -1,8 +1,10 @@
+"""Migrate MESS games to MAME"""
 from lutris.pga import get_games
 from lutris.game import Game
 
 
 def migrate():
+    """Run migration"""
     for pga_game in get_games():
         game = Game(pga_game["id"])
         if game.runner_name != "mess":
