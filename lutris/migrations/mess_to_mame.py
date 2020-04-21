731df87307a5cc5f2856e4f008c956a1d64c1716
Mathieu Comandon <strycore@gmail.com>
2020-04-21 15:01:56 -0700

    Migrate MESS games to MAME

---
+++ lutris/migrations/mess_to_mame.py
@@ -0,0 +1,13 @@
+from lutris.pga import get_games
+from lutris.game import Game
+
+
+def migrate():
+    for pga_game in get_games():
+        game = Game(pga_game["id"])
+        if game.runner_name != "mess":
+            continue
+        if "mess" in game.config.game_level:
+            game.config.game_level["mame"] = game.config.game_level.pop("mess")
+        game.runner_name = "mame"
+        game.save()
