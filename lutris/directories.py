fc342797a500f827d6c1d09d264151a82d619e57
Mathieu Comandon <strycore@gmail.com>
2020-04-24 16:08:16 -0700

    Introduce RunnerDirectory class

---
+++ lutris/directories.py
@@ -0,0 +1,13 @@
+"""Directory management for runners"""
+
+
+class RunnerDirectory:
+    """Class to reference and manipulate directories used by folders"""
+    def __init__(self, path):
+        self.path = path
+
+    def __str__(self):
+        return self.path
+
+# That's it for now. There's literally no code at all. Still figuring things
+# out here.
