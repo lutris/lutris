9f67e6e2812722908dfb9fc3304ba11a8da81403
SwimmingTiger <hu60.cn@gmail.com>
2020-06-17 11:19:47 +0800

    Fix Travis failure by sorting imports (same as #2943)
    I accidentally modified this file in my previous submission, sorry.

--- lutris/util/linux.py
+++ lutris/util/linux.py
@@ -10,7 +10,6 @@ import subprocess
 import sys
 from collections import Counter, defaultdict
 
-from lutris.util import system
 # Lutris Modules
 from lutris.util import system
 from lutris.util.disks import get_drive_for_path
