84346e869762fe12f36cc5fc7dcda2f0f9207180
Mathieu Comandon <strycore@gmail.com>
2020-08-21 20:10:34 -0700

    Cleanup Steam game matching

--- lutris/services/steam.py
+++ lutris/services/steam.py
@@ -7,6 +7,7 @@ from lutris.config import LutrisConfig, make_game_config_id
 from lutris.database.games import get_games_where
 from lutris.services.base import BaseService
 from lutris.services.service_game import ServiceGame
+from lutris.util.log import logger
 from lutris.util.steam.appmanifest import AppManifest, get_appmanifests
 from lutris.util.steam.config import get_steamapps_paths
 
@@ -65,21 +66,20 @@ class SteamService(BaseService):
     def __init__(self):
         super().__init__()
         self._lutris_games = None
-        self._lutris_steamids = None
 
     @property
     def lutris_games(self):
-        """Return all Steam games present in the Lutris library"""
+        """Get all Lutris games with a Steam ID"""
         if not self._lutris_games:
             self._lutris_games = get_games_where(steamid__isnull=False, steamid__not="")
         return self._lutris_games
 
-    @property
-    def lutris_steamids(self):
-        """Return the Steam IDs of the games installed in Lutris"""
-        if not self._lutris_steamids:
-            self._lutris_steamids = {str(game["steamid"]) for game in self.lutris_games}
-        return self._lutris_steamids
+    def get_lutris_games_by_appid(self):
+        """Return Lutris games keyed by Steam AppID"""
+        lutris_steamid_map = {}
+        for lutris_game in self.lutris_games:
+            lutris_steamid_map[lutris_game["steamid"]] = lutris_game
+        return lutris_steamid_map
 
     def load(self):
         """Return importable Steam games"""
@@ -90,16 +90,13 @@ class SteamService(BaseService):
                 for appmanifest_file in get_appmanifests(steamapps_path):
                     app_manifest = AppManifest(os.path.join(steamapps_path, appmanifest_file))
                     if SteamGame.is_importable(app_manifest):
+                        logger.debug("Found Steam game %s", app_manifest)
                         games.append(SteamGame.new_from_steam_game(app_manifest))
+
+        steam_map = self.get_lutris_games_by_appid()
         for game in games:
+            if game.appid in steam_map:
+                game.lutris_slug = steam_map[game.appid]["slug"]
+                logger.debug("Attached Lutris slug %s", game.lutris_slug)
             game.save()
         self.emit("service-games-loaded", self.id)
-
-    def get_pga_game(self, game):
-        """Return a PGA game if one is found"""
-        for pga_game in self.lutris_games:
-            if (
-                str(pga_game["steamid"]) == game.appid
-                and (pga_game["runner"] == game.runner or not pga_game["runner"]) and not pga_game["installed"]
-            ):
-                return pga_game
