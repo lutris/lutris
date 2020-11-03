9a5abbe5a0625594b0581a7dc46d33316e47c01c
Mathieu Comandon <strycore@gmail.com>
2020-11-03 01:54:56 -0800

    Return empty set

--- lutris/gui/dialogs/runner_install.py
+++ lutris/gui/dialogs/runner_install.py
@@ -153,7 +153,7 @@ class RunnerInstallDialog(Dialog):
         """List versions available locally"""
         runner_path = os.path.join(settings.RUNNER_DIR, self.runner)
         if not os.path.exists(runner_path):
-            return {}
+            return set()
         return {
             tuple(p.rsplit("-", 1))
             for p in os.listdir(runner_path)
