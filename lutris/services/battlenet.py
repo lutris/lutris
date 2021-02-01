791830c31e3ba565209338bc43b44761096bfd1f
Mathieu Comandon <strycore@gmail.com>
2021-02-01 11:22:27 -0800

    Add some URLs for Battle.net

--- lutris/services/battlenet.py
+++ lutris/services/battlenet.py
@@ -13,3 +13,26 @@ class BattleNetService(OnlineService):
     name = _("Battle.net")
     icon = "battlenet"
     medias = {}
+    region = "na"
+
+    @property
+    def oauth_url(self):
+        """Return the URL used for OAuth sign in"""
+        if self.region == 'cn':
+            return "https://www.battlenet.com.cn/oauth"
+        return f"https://{self.region}.battle.net/oauth"
+
+    @property
+    def api_url(self):
+        """Main API endpoint"""
+        if self.region == 'cn':
+            return "https://gateway.battlenet.com.cn"
+        return f"https://{self.region}.api.blizzard.com"
+
+    @property
+    def login_url(self):
+        """Battle.net login URL"""
+        if self.region == 'cn':
+            return 'https://www.battlenet.com.cn/login/zh'
+        else:
+            return f'https://{self.region}.battle.net/login/en'
