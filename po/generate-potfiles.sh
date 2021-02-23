ad3875eb425e6a13d57e7cd21a608809c18710c9
Stephan Lachnit <stephanlachnit@protonmail.com>
2021-02-23 12:05:12 +0100

    i18n: update readme and POTFILES readability
    Signed-off-by: Stephan Lachnit <stephanlachnit@protonmail.com>

--- po/generate-potfiles.sh
+++ po/generate-potfiles.sh
@@ -2,14 +2,14 @@
 
 cd "$(dirname "$(realpath "$0")")/.."
 
-echo "# generated on $(date -u)" > ./po/POTFILES
+echo "# generated on $(date -u -Iseconds)" > po/POTFILES
+
+echo "" >> po/POTFILES
+echo "share/applications/net.lutris.Lutris.desktop" >> po/POTFILES
+echo "share/metainfo/net.lutris.Lutris.metainfo.xml" >> po/POTFILES
 
 echo "" >> ./po/POTFILES
-echo "./share/applications/net.lutris.Lutris.desktop" >> ./po/POTFILES
-echo "./share/metainfo/net.lutris.Lutris.metainfo.xml" >> ./po/POTFILES
+find share/lutris/ui -name '*.ui' | sort >> po/POTFILES
 
 echo "" >> ./po/POTFILES
-find ./share/lutris/ui -name '*.ui' | sort >> ./po/POTFILES
-
-echo "" >> ./po/POTFILES
-find ./lutris -name '*.py' | sort >> ./po/POTFILES
+find lutris -name '*.py' | sort >> po/POTFILES
