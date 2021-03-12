c2afbf3d1b71b212dbc5b29ff4cdecefa0a741d7
Mathieu Comandon <strycore@gmail.com>
2021-03-12 13:37:33 -0800

    Add environment variable to re-enable XDG service

--- lutris/services/__init__.py
+++ lutris/services/__init__.py
@@ -6,6 +6,7 @@ from lutris.services.gog import GOGService
 from lutris.services.humblebundle import HumbleBundleService
 from lutris.services.lutris import LutrisService
 from lutris.services.steam import SteamService
+from lutris.services.xdg import XDGService
 
 
 def get_services():
@@ -18,4 +19,6 @@ def get_services():
     }
     if os.environ.get("LUTRIS_ENABLE_DOLPHIN"):
         enabled_services["dolphin"] = DolphinService
+    if os.environ.get("LUTRIS_ENABLE_XDG"):
+        enabled_services["xdg"] = XDGService
     return enabled_services
