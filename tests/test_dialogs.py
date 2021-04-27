ff33ab73c2ab27d1d73b0827b3719a3fb76489c0
Mathieu Comandon <strycore@gmail.com>
2021-04-26 22:42:38 -0700

    Fix tests

--- tests/test_dialogs.py
+++ tests/test_dialogs.py
@@ -20,7 +20,7 @@ setup_test_environment()
 
 class TestGameDialogCommon(TestCase):
     def test_get_runner_liststore(self):
-        dlg = GameDialogCommon()
+        dlg = GameDialogCommon("test")
         list_store = dlg._get_runner_liststore()
         self.assertTrue(
             list_store[1][0].startswith(runners.get_installed()[0].human_name)
