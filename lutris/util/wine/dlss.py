b8cec47162bf0eb71b9804e52241e01cdb9c6dae
Mathieu Comandon <strycore@gmail.com>
2021-07-12 12:05:25 -0700

    Get real DLL path in DLSSManager

--- lutris/util/wine/dlss.py
+++ lutris/util/wine/dlss.py
@@ -1,9 +1,9 @@
 from lutris.util.wine.dll_manager import DLLManager
-
+from lutris.util.nvidia import get_nvidia_dll_path
 
 class DLSSManager(DLLManager):
     component = "DLSS"
-    base_dir = "/usr/lib/nvidia/wine"
+    base_dir = get_nvidia_dll_path()
     managed_dlls = ("nvngx", )
     versions_path = None
     releases_url = None
