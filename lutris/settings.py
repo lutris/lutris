ad176b9cecdb68482d37548b136ad0be296b0800
ProjectInfinity <a.sjur91@gmail.com>
2021-09-22 20:37:39 +0200

    Use CACHE_DIR for banners Closes #3625

--- lutris/settings.py
+++ lutris/settings.py
@@ -23,7 +23,7 @@ CACHE_DIR = os.path.join(GLib.get_user_cache_dir(), "lutris")
 GAME_CONFIG_DIR = os.path.join(CONFIG_DIR, "games")
 
 TMP_PATH = os.path.join(CACHE_DIR, "tmp")
-BANNER_PATH = os.path.join(DATA_DIR, "banners")
+BANNER_PATH = os.path.join(CACHE_DIR, "banners")
 COVERART_PATH = os.path.join(DATA_DIR, "coverart")
 ICON_PATH = os.path.join(GLib.get_user_data_dir(), "icons", "hicolor", "128x128", "apps")
 
