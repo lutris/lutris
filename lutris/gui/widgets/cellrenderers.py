234f98203def40d4b0b351fe2c4b5ea6fa354846
Daniel Johnson <danieljohnson2@fastmail.com>
2021-11-21 09:43:41 -0500

    An attempt to trigger download on rendering
    This leaves much to be desired!

--- lutris/gui/widgets/cellrenderers.py
+++ lutris/gui/widgets/cellrenderers.py
@@ -1,5 +1,6 @@
-from gi.repository import Gtk, Pango
-
+from gi.repository import GObject, Gtk, Pango
+from gi.repository.GdkPixbuf import Pixbuf
+from lutris.gui.views.media_loader import download_icons
 
 class GridViewCellRendererText(Gtk.CellRendererText):
     """CellRendererText adjusted for grid view display, removes extra padding"""
@@ -11,3 +12,27 @@ class GridViewCellRendererText(Gtk.CellRendererText):
         self.props.xalign = 0.5
         self.props.yalign = 0
         self.props.wrap_width = width
+
+class GridViewCellRendererBanner(Gtk.CellRendererPixbuf):
+    def __init__(self, service_media, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.service_media = service_media
+
+    @GObject.Property(type=str)
+    def slug(self):
+        'Read-write integer property.'
+        return self._slug
+
+    @slug.setter
+    def slug(self, value):
+        self._slug = value
+
+    def do_render(self, cr, widget, background_area, cell_area, flags):
+        slug = self._slug
+
+        media_urls = self.service_media.get_media_urls()
+        url = media_urls[slug]
+        download_icons({slug: url}, self.service_media)
+        
+        Gtk.CellRendererPixbuf.do_render(self, cr, widget, background_area, cell_area, flags)
+  
