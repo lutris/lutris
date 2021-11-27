d4de73e5631b70eac883774feb758667751f4103
Daniel Johnson <danieljohnson2@fastmail.com>
2021-11-27 16:06:32 -0500

    Commenting!

--- lutris/gui/widgets/cellrenderers.py
+++ lutris/gui/widgets/cellrenderers.py
@@ -18,10 +18,19 @@ class GridViewCellRendererText(Gtk.CellRendererText):
 
 
 class GridViewCellRendererBanner(Gtk.CellRendererPixbuf):
+    """A renderer that causes banners and icons to download if they are to be drawn
+    but do not exist (according to the service-media; we always get a pixbuf,
+    which may be a placeholder, but if we get the slug, we'll double check it."""
+
+    # We accumulate slugs to download for half a second so we can download in parallel
     pending_download_slugs = set()
+    # We know what downloads are already coming, and don't ask for them again
     ongoing_download_slugs = set()
+    # We know what downloads failed, and don't ask for them again.
     failed_slugs = set()
+    # We avoid starting the timer twice, through it shuts down as soon as it fires.
     download_timer_running = False
+    # Backing store for the slug property
     _slug = ""
 
     def __init__(self, game_store, service_media, *args, **kwargs):
@@ -31,6 +40,7 @@ class GridViewCellRendererBanner(Gtk.CellRendererPixbuf):
 
     @GObject.Property(type=str)
     def slug(self):
+        """This property contains the slug to download, if required at render time."""
         return self._slug
 
     @slug.setter
@@ -38,6 +48,10 @@ class GridViewCellRendererBanner(Gtk.CellRendererPixbuf):
         self._slug = value
 
     def do_render(self, cr, widget, background_area, cell_area, flags):
+        # Before we render check the slug- if it needs to download, we'll
+        # queue it up and start the timer. We do not download now- we]
+        # render the pixbuf we were given. Downloads take too long to delay
+        # rendering for.
         service_media = self.service_media
         slug = self._slug
 
@@ -50,7 +64,14 @@ class GridViewCellRendererBanner(Gtk.CellRendererPixbuf):
 
         Gtk.CellRendererPixbuf.do_render(self, cr, widget, background_area, cell_area, flags)
 
+    def on_widget_timeout(self):
+        self.download_timer_running = False
+        self.download_icons()
+        return False
+
     def download_icons(self):
+        # We need to work out which slugs to download; we'll recheck
+        # that they are still needed, and are not downloading or failed.
         service_media = self.service_media
         slugs = [
             slug for slug
@@ -65,6 +86,7 @@ class GridViewCellRendererBanner(Gtk.CellRendererPixbuf):
         if len(slugs) == 0:
             return
 
+        # Work out the URLs for the slugs
         urls_needed = service_media.get_media_urls_for(slugs)
 
         if len(urls_needed) == 0:
@@ -73,6 +95,8 @@ class GridViewCellRendererBanner(Gtk.CellRendererPixbuf):
         self.ongoing_download_slugs.update(slugs)
 
         def icons_download_cb(result, error):
+            """This is called when the download completes and records the 'icons';
+            that will cause the UI to update and the cells, ultimately, to rerender."""
             self.ongoing_download_slugs.difference_update(slugs)
 
             if error:
@@ -94,8 +118,3 @@ class GridViewCellRendererBanner(Gtk.CellRendererPixbuf):
             urls_needed,
             service_media
         )
-
-    def on_widget_timeout(self):
-        self.download_timer_running = False
-        self.download_icons()
-        return False
