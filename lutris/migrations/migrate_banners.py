f193d67ac1b5fc6b47276867115eccd7bb4b9fe7
Daniel Johnson <danieljohnson2@fastmail.com>
2021-12-21 04:59:21 -0500

    Also remove the old banners dir if empty
    And add logging if errors occur during migration, too.

--- lutris/migrations/migrate_banners.py
+++ lutris/migrations/migrate_banners.py
@@ -2,20 +2,27 @@
 import os
 
 from lutris import settings
+from lutris.util.log import logger
 
 
 def migrate():
     dest_dir = settings.BANNER_PATH
     src_dir = os.path.join(settings.DATA_DIR, "banners")
 
-    # init_lutris() creates the new banners directrory
-    if os.path.isdir(src_dir) and os.path.isdir(dest_dir):
-        for filename in os.listdir(src_dir):
-            try:
-                src_file = os.path.join(src_dir, filename)
-                dest_file = os.path.join(dest_dir, filename)
+    try:
+        # init_lutris() creates the new banners directrory
+        if os.path.isdir(src_dir) and os.path.isdir(dest_dir):
+            for filename in os.listdir(src_dir):
+                try:
+                    src_file = os.path.join(src_dir, filename)
+                    dest_file = os.path.join(dest_dir, filename)
 
-                if not os.path.exists(dest_file):
-                    os.rename(src_file, dest_file)
-            except OSError:
-                pass  # Skip what we can't migrate
+                    if not os.path.exists(dest_file):
+                        os.rename(src_file, dest_file)
+                except OSError as ex:  # Skip what we can't migrate
+                    logger.exception("Failed to migrate banner %s: %s", filename, ex)
+
+            if not os.listdir(src_dir):
+                os.rmdir(src_dir)
+    except OSError as ex:
+        logger.exception("Failed to migrate banners: %s", ex)
