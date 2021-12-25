2b93dfc681d4aa961a35b3ee3810f8003fe05319
telanus <betelanus@gmail.com>
2021-12-25 10:35:29 +0200

    Use CACHE_DIR for coverart

--- lutris/settings.py
+++ lutris/settings.py
@@ -24,7 +24,7 @@ GAME_CONFIG_DIR = os.path.join(CONFIG_DIR, "games")
 
 TMP_PATH = os.path.join(CACHE_DIR, "tmp")
 BANNER_PATH = os.path.join(CACHE_DIR, "banners")
-COVERART_PATH = os.path.join(DATA_DIR, "coverart")
+COVERART_PATH = os.path.join(CACHE_DIR, "coverart")
 ICON_PATH = os.path.join(GLib.get_user_data_dir(), "icons", "hicolor", "128x128", "apps")
 
 sio = SettingsIO(CONFIG_FILE)
