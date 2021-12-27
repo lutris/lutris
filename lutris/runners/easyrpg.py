31865ea1f7562f7624eb29cb13ae13a628678fc3
ObserverOfTime <chronobserver@disroot.org>
2021-12-27 15:15:37 +0200

    Bump EasyRPG player version

--- lutris/runners/easyrpg.py
+++ lutris/runners/easyrpg.py
@@ -13,7 +13,7 @@ class easyrpg(Runner):
     runnable_alone = True
     entry_point_option = "project_path"
     runner_executable = "easyrpg-player"
-    download_url = "https://easyrpg.org/downloads/player/0.6.2.3/easyrpg-player-0.6.2.3-linux.tar.gz"
+    download_url = "https://easyrpg.org/downloads/player/0.7.0/easyrpg-player-0.7.0-linux.tar.gz"
 
     game_options = [
         {
