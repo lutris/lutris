c057b4ee8a4cd04434c2e4f227f476ad6a02c873
Daniel Johnson <danieljohnson2@fastmail.com>
2021-12-30 06:01:19 -0500

    Pre-create the runner when installing
    We always need it; if we have it soon enough, we can use it in all
    methods. We'll need it!

--- lutris/installer/interpreter.py
+++ lutris/installer/interpreter.py
@@ -48,6 +48,7 @@ class ScriptInterpreter(GObject.Object, CommandsMixin):
         self.current_command = 0  # Current installer command when iterating through them
         self.runners_to_install = []
         self.installer = LutrisInstaller(installer, self, service=self.service, appid=self.appid)
+        self.runner = self.get_runner_class(self.installer.runner)()
         if not self.installer.script:
             raise ScriptingError(_("This installer doesn't have a 'script' section"))
         script_errors = self.installer.get_errors()
@@ -185,9 +186,7 @@ class ScriptInterpreter(GObject.Object, CommandsMixin):
         dependencies or runners used for installer tasks.
         """
         runners_to_install = []
-        required_runners = []
-        runner = self.get_runner_class(self.installer.runner)
-        required_runners.append(runner())
+        required_runners = [self.runner]
 
         for command in self.installer.script.get("installer", []):
             command_name, command_params = self._get_command_name_and_params(command)
