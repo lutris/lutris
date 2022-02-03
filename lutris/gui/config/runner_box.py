323c7440ec44bb3adcdb40dc18afb6f068827d68
Mathieu Comandon <strycore@gmail.com>
2022-02-03 11:22:27 -0800

    Always show runner configure button

--- lutris/gui/config/runner_box.py
+++ lutris/gui/config/runner_box.py
@@ -59,8 +59,7 @@ class RunnerBox(Gtk.Box):
         self.pack_start(self.configure_button, False, False, 0)
         if not self.runner.is_installed():
             self.runner_label_box.set_sensitive(False)
-        else:
-            self.configure_button.show()
+        self.configure_button.show()
         self.action_alignment = Gtk.Alignment.new(0.5, 0.5, 0, 0)
         self.action_alignment.show()
         self.action_alignment.add(self.get_action_button())
