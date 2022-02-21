c25e5b04d8dfef1985ab0acff8c7f921c36f2f41
Layerex <layerex@dismail.de>
2022-02-21 12:24:35 +0300

    Fix incorrect EasyRPG runner executable path

--- lutris/runners/easyrpg.py
+++ lutris/runners/easyrpg.py
@@ -12,7 +12,7 @@ class easyrpg(Runner):
     platforms = [_("Linux")]
     runnable_alone = True
     entry_point_option = "project_path"
-    runner_executable = "easyrpg-player"
+    runner_executable = "easyrpg/easyrpg-player"
     download_url = "https://easyrpg.org/downloads/player/0.7.0/easyrpg-player-0.7.0-linux.tar.gz"
 
     game_options = [
