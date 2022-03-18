a368505b50387aa18e00a92aa6521dc28360975f
liberavia <andre.gregor.herrmann@gmail.com>
2022-03-18 23:00:54 +0100

    Remove unused function

--- lutris/util/steam/shortcut.py
+++ lutris/util/steam/shortcut.py
@@ -8,30 +8,6 @@ from lutris.util.steam.config import search_in_steam_dirs
 steam_tag = "Lutris"
 
 
-def update_shortcuts(steamUserId):
-    shortcut_path = os.path.expanduser(f'~/.local/share/Steam/userdata/{steamUserId}/config/shortcuts.vdf')
-
-    lutris_shortcuts = []
-
-    # Read existing shortcuts, that have no lutris tag
-    with open(shortcut_path, "rb") as shortcut_file:
-        shortcuts = vdf.binary_loads(shortcut_file.read())['shortcuts'].values()
-    other_shortcuts = [
-        s for s in shortcuts
-        if steam_tag not in s['tags'].values()
-    ]
-
-    new_shortcuts = {
-        'shortcuts': {
-            str(index): elem for index, elem in enumerate(other_shortcuts + lutris_shortcuts)
-        }
-    }
-
-    # Write shortcuts back to file
-    with open(shortcut_path, "wb") as shortcut_file:
-        shortcut_file.write(vdf.binary_dumps(new_shortcuts))
-
-
 def get_shortcuts_vdf_path():
     # @todo: steam_userid is needed => path_suffix = "userdata/{}/config/shortcuts.vdf".format(get_steam__userid())
     path_suffix = "userdata/114179503/config/shortcuts.vdf"
