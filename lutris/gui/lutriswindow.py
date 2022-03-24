6c9471b4ce07c0053adb057fc8a7fc04dee016ad
Mathieu Comandon <strycore@gmail.com>
2022-03-24 13:11:00 -0700

    Disable splash screen

--- lutris/gui/lutriswindow.py
+++ lutris/gui/lutriswindow.py
@@ -418,12 +418,13 @@ class LutrisWindow(Gtk.ApplicationWindow):  # pylint: disable=too-many-public-me
                 self.show_label(_("Add games to your favorites to see them here."))
             elif self.filters.get("installed"):
                 self.show_label(_("No installed games found. Press Ctrl+H so show all games."))
-            elif (
-                not self.filters.get("runner")
-                and not self.filters.get("service")
-                and not self.filters.get("platform")
-            ):
-                self.show_splash()
+            # Splash screen disabled because unfinished.
+            # elif (
+            #     not self.filters.get("runner")
+            #     and not self.filters.get("service")
+            #     and not self.filters.get("platform")
+            # ):
+            #     self.show_splash()
             else:
                 self.show_label(_("No games found"))
 
