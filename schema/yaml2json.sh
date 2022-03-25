97ae6ec58ecb6fe607f6384676bf490675389608
Martynas Petuška <martynas@petuska.dev>
2022-03-25 01:18:28 +0000

    install script json schema

---
+++ schema/yaml2json.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+SELF_DIR="$(dirname "$0")"
+
+yaml2json() {
+    if [[ -z "$1" ]]; then
+        echo "ERROR: No YAML file passed in!"
+        exit 1
+    fi
+    base="$(basename "$1")"
+    target="${base%.*}.json"
+    echo "INFO: $1 > $target"
+    curl -d "$(cat "$SELF_DIR/$1")" \
+        -H "Content-Type: text/plain" \
+        -H 'Accept: application/json' \
+        -o "$SELF_DIR/$target" \
+        https://www.anyjson.in/api/v2/data/yamltojson &>/dev/null
+}
+
+for var in "$@"; do
+    yaml2json "$var"
+done
