3d8324ab1e70237bb02d6408d2a084ca2d05472c
liberavia <andre.gregor.herrmann@gmail.com>
2022-03-26 15:25:50 +0100

    Fix comments

--- lutris/util/resources.py
+++ lutris/util/resources.py
@@ -10,10 +10,10 @@ def get_icon_path(game_slug):
 
 
 def get_banner_path(game_slug):
-    """Return the absolute path for a game_slug icon"""
+    """Return the absolute path for a game_slug banner"""
     return os.path.join(settings.BANNER_PATH, "%s.png" % game_slug)
 
 
 def get_cover_path(game_slug):
-    """Return the absolute path for a game_slug icon"""
+    """Return the absolute path for a game_slug coverart"""
     return os.path.join(settings.COVERART_PATH, "%s.png" % game_slug)
