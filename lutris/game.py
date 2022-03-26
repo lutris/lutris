b6ddffdc27ade8cc0f88e192c57ca420c29005d7
Daniel Johnson <danieljohnson2@fastmail.com>
2022-03-26 11:35:16 -0400

    This error message should be localized
    Shouldn't it?

--- lutris/game.py
+++ lutris/game.py
@@ -343,7 +343,7 @@ class Game(GObject.Object):
     def start_xephyr(self, display=":2"):
         """Start a monitored Xephyr instance"""
         if not system.find_executable("Xephyr"):
-            raise GameConfigError("Unable to find Xephyr, install it or disable the Xephyr option")
+            raise GameConfigError(_("Unable to find Xephyr, install it or disable the Xephyr option"))
         xephyr_command = get_xephyr_command(display, self.runner.system_config)
         xephyr_thread = MonitoredCommand(xephyr_command)
         xephyr_thread.start()
