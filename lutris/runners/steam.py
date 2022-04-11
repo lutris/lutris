28433c6de7f45c20fe943cf407679fd7efce1300
Mathieu Comandon <strycore@gmail.com>
2022-04-11 16:42:23 -0700

    Pull steam executable from home folder in Flatpak

--- lutris/runners/steam.py
+++ lutris/runners/steam.py
@@ -154,7 +154,13 @@ class steam(Runner):
 
     def get_executable(self):
         if linux.LINUX_SYSTEM.is_flatpak:
-            # Use xdg-open for Steam URIs in Flatpak
+            # Using this binary to launch Steam stuff may require Steam to be running in the background
+            # This may not even work at all but is worth experimenting with.
+            steam_path = os.path.expanduser("~/.steam/steam/ubuntu12_32/steam")
+            if os.path.exists(steam_path):
+                return steam_path
+
+            # Fallback to xgd-open for Steam URIs in Flatpak
             return system.find_executable("xdg-open")
         if self.runner_config.get("lsi_steam") and system.find_executable("lsi-steam"):
             return system.find_executable("lsi-steam")
