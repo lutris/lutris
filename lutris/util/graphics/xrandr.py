5e45a0252c7770cfdaa2c56538c13b021984e7f2
Daniel Johnson <danieljohnson2@fastmail.com>
2022-04-16 18:54:47 -0400

    Sort and dedup the resolutions from LegacyDisplayManager
    Also remove get_unique_resolutions(), which is not called

--- lutris/util/graphics/xrandr.py
+++ lutris/util/graphics/xrandr.py
@@ -89,12 +89,7 @@ def get_resolutions():
             resolution_match = re.match(r".*?(\d+x\d+).*", line)
             if resolution_match:
                 resolution_list.append(resolution_match.groups()[0])
-    return resolution_list
-
-
-def get_unique_resolutions():
-    """Return available resolutions, without duplicates and ordered with highest resolution first"""
-    return sorted(set(get_resolutions()), key=lambda x: int(x.split("x")[0]), reverse=True)
+    return sorted(set(resolution_list), key=lambda x: int(x.split("x")[0]), reverse=True)
 
 
 def change_resolution(resolution):
