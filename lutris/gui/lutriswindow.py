8f86bfeb607625686650d2e77fe07ad49a44ba20
Daniel Johnson <danieljohnson2@fastmail.com>
2022-07-02 07:15:41 -0400

    Placate flake8 is the best rhyme!
    Also, fix a grammar error.

--- lutris/gui/lutriswindow.py
+++ lutris/gui/lutriswindow.py
@@ -416,12 +416,13 @@ class LutrisWindow(Gtk.ApplicationWindow):  # pylint: disable=too-many-public-me
         filter_text = self.filters.get("text")
         if filter_text:
             if self.filters.get("category") == "favorite":
-                self.show_label(_("Add a game matching '%s' to your favorites to see them here.") % filter_text)
+                self.show_label(_("Add a game matching '%s' to your favorites to see it here.") % filter_text)
             elif self.filters.get("installed"):
                 self.show_label(
                     _("No installed games matching '%s' found. Press Ctrl+I to show uninstalled games.") % filter_text)
-            elif self.filters.get("hidden") == False:  # but not if missing!
-                self.show_label(_("No visible games matching '%s' found. Press Ctrl+H to show hidden games.") % filter_text)
+            elif self.filters.get("hidden") is False:  # but not if missing!
+                self.show_label(_("No visible games matching '%s' found. Press Ctrl+H to show hidden games.") %
+                                filter_text)
             else:
                 self.show_label(_("No games matching '%s' found ") % filter_text)
         else:
@@ -429,7 +430,7 @@ class LutrisWindow(Gtk.ApplicationWindow):  # pylint: disable=too-many-public-me
                 self.show_label(_("Add games to your favorites to see them here."))
             elif self.filters.get("installed"):
                 self.show_label(_("No installed games found. Press Ctrl+I to show uninstalled games."))
-            elif self.filters.get("hidden") == False:  # but not if missing!
+            elif self.filters.get("hidden") is False:  # but not if missing!
                 self.show_label(_("No visible games found. Press Ctrl+H to show hidden games."))
             # Splash screen disabled because unfinished.
             # elif (
