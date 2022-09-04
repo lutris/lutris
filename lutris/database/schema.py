00c3c207dd62f1e49353c08c715d695783a8bf0c
gascarcella <scarcella.ga@gmail.com>
2022-09-04 02:02:40 -0300

    Adding Discord ID to Game Schema

--- lutris/database/schema.py
+++ lutris/database/schema.py
@@ -88,7 +88,11 @@ DATABASE = {
         {
             "name": "service_id",
             "type": "TEXT"
-        }
+        },
+        {
+            "name": "discord_id",
+            "type": "TEXT",
+        },
     ],
     "service_games": [
         {
