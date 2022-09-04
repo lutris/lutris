428ec9dd81c49d5f4ab792a83166ee766cb71e00
gascarcella <scarcella.ga@gmail.com>
2022-09-04 03:48:08 -0300

    Adding missing discord package

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(
         'lutris.services',
         'lutris.util',
         'lutris.util.amazon',
+        'lutris.util.discord',
         'lutris.util.dolphin',
         'lutris.util.egs',
         'lutris.util.graphics',
