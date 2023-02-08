2367ea3e0e2c6c3dac8176f9639229c8a1c8afbf
Daniel Johnson <danieljohnson2@fastmail.com>
2023-02-07 19:00:49 -0500

    Fix signal name type-o

--- lutris/gui/lutriswindow.py
+++ lutris/gui/lutriswindow.py
@@ -134,7 +134,7 @@ class LutrisWindow(Gtk.ApplicationWindow,
         GObject.add_emission_hook(Game, "game-updated", self.on_game_updated)
         GObject.add_emission_hook(Game, "game-stopped", self.on_game_stopped)
         GObject.add_emission_hook(Game, "game-removed", self.on_game_removed)
-        GObject.add_emission_hook(Game, "game-unhandled_error", self.on_game_unhandled_error)
+        GObject.add_emission_hook(Game, "game-unhandled-error", self.on_game_unhandled_error)
 
     def _init_actions(self):
         Action = namedtuple("Action", ("callback", "type", "enabled", "default", "accel"))
