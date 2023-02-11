acdbd633dc8f9fbf48311a6167d5f78646e15176
Mathieu Comandon <strycore@gmail.com>
2023-02-11 11:23:04 -0800

    Accept both Webkit2 4.1 and 4.0

--- lutris/gui/dialogs/webconnect_dialog.py
+++ lutris/gui/dialogs/webconnect_dialog.py
@@ -3,7 +3,10 @@ import os
 from gettext import gettext as _
 
 import gi
-gi.require_version("WebKit2", "4.0")
+try:
+    gi.require_version("WebKit2", "4.1")
+except ValueError:
+    gi.require_version("WebKit2", "4.0")
 from gi.repository import WebKit2
 
 from lutris.gui.dialogs import ModalDialog
