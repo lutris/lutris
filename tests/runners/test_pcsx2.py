31f327b50e9f3b652bf9b365824594a86a22343f
Daniel Johnson <danieljohnson2@fastmail.com>
2023-05-14 06:32:29 -0400

    Update PCSX2 unit test
    The switches have all changed, and config-file support is gone entirely.

--- tests/runners/test_pcsx2.py
+++ tests/runners/test_pcsx2.py
@@ -28,7 +28,7 @@ class TestPCSX2Runner(unittest.TestCase):
         mock_config.game_config = {'main_file': main_file}
         mock_config.runner_config = {'fullscreen': True}
         self.runner.config = mock_config
-        expected = {'command': [self.runner.get_executable(), '--fullscreen', main_file]}
+        expected = {'command': [self.runner.get_executable(), '-fullscreen', main_file]}
         self.assertEqual(self.runner.play(), expected)
 
     @patch('lutris.util.system.path_exists')
@@ -39,7 +39,7 @@ class TestPCSX2Runner(unittest.TestCase):
         mock_config.game_config = {'main_file': main_file}
         mock_config.runner_config = {'full_boot': True}
         self.runner.config = mock_config
-        expected = {'command': [self.runner.get_executable(), '--fullboot', main_file]}
+        expected = {'command': [self.runner.get_executable(), '-slowboot', main_file]}
         self.assertEqual(self.runner.play(), expected)
 
     @patch('lutris.util.system.path_exists')
@@ -50,47 +50,19 @@ class TestPCSX2Runner(unittest.TestCase):
         mock_config.game_config = {'main_file': main_file}
         mock_config.runner_config = {'nogui': True}
         self.runner.config = mock_config
-        expected = {'command': [self.runner.get_executable(), '--nogui', main_file]}
-        self.assertEqual(self.runner.play(), expected)
-
-    @patch('lutris.util.system.path_exists')
-    def test_play_cfg_set(self, mock_path_exists):
-        main_file = '/valid/path/to/iso'
-        config_file = '/valid/path/to/cfg'
-        cfg_arg = '--cfg=' + config_file
-        mock_path_exists.return_value = True
-        mock_config = MagicMock()
-        mock_config.game_config = {'main_file': main_file}
-        mock_config.runner_config = {'config_file': config_file}
-        self.runner.config = mock_config
-        expected = {'command': [self.runner.get_executable(), cfg_arg, main_file]}
-        self.assertEqual(self.runner.play(), expected)
-
-    @patch('lutris.util.system.path_exists')
-    def test_play_cfgpath_set(self, mock_path_exists):
-        main_file = '/valid/path/to/iso'
-        config_path = '/valid/path/to/cfgpath'
-        cfgpath_arg = '--cfgpath=' + config_path
-        mock_path_exists.return_value = True
-        mock_config = MagicMock()
-        mock_config.game_config = {'main_file': main_file}
-        mock_config.runner_config = {'config_path': config_path}
-        self.runner.config = mock_config
-        expected = {'command': [self.runner.get_executable(), cfgpath_arg, main_file]}
+        expected = {'command': [self.runner.get_executable(), '-nogui', main_file]}
         self.assertEqual(self.runner.play(), expected)
 
     @patch('lutris.util.system.path_exists')
     def test_play(self, mock_path_exists):
         main_file = '/valid/path/to/iso'
-        config_path = '/valid/path/to/cfgpath'
-        cfgpath_arg = '--cfgpath=' + config_path
         mock_path_exists.return_value = True
         mock_config = MagicMock()
         mock_config.game_config = {'main_file': main_file}
         mock_config.runner_config = {
-            'config_path': config_path, 'fullscreen': False, 'nogui': True,
-            'full_boot': True, 'config_file': '',
+            'fullscreen': False, 'nogui': True,
+            'full_boot': True,
         }
         self.runner.config = mock_config
-        expected = {'command': [self.runner.get_executable(), '--fullboot', '--nogui', cfgpath_arg, main_file]}
+        expected = {'command': [self.runner.get_executable(), '-slowboot', '-nogui', main_file]}
         self.assertEqual(self.runner.play(), expected)
