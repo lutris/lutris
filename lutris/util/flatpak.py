dbcedff78499aa886e25d52fc9a56eb224e84341
Daniel Johnson <danieljohnson2@fastmail.com>
2023-06-09 17:34:04 -0400

    Add a check to skip blank lines in 'flatpak list' output.
    I think this happens if you have zero flatpaks installed - it returns nothing at all, but that splits into one empty string.

--- lutris/util/flatpak.py
+++ lutris/util/flatpak.py
@@ -36,20 +36,21 @@ def get_installed_apps():
     package_list = read_process_output(command)
     packages = []
     for package in package_list.split("\n"):
-        try:
-            name, appid, version, branch, origin, installation = package.split("\t")
-        except ValueError:
-            # For older Flatpak versions
-            name, appid, version, branch, installation = package.split("\t")
-            origin = ""
-        packages.append({
-            "name": name,
-            "appid": appid,
-            "version": version,
-            "branch": branch,
-            "origin": origin,
-            "installation": installation
-        })
+        if package:
+            try:
+                name, appid, version, branch, origin, installation = package.split("\t")
+            except ValueError:
+                # For older Flatpak versions
+                name, appid, version, branch, installation = package.split("\t")
+                origin = ""
+            packages.append({
+                "name": name,
+                "appid": appid,
+                "version": version,
+                "branch": branch,
+                "origin": origin,
+                "installation": installation
+            })
     return packages
 
 
