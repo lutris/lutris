43c23d7cdce7c018202996b83a735783ed40d56d
Daniel Johnson <danieljohnson2@fastmail.com>
2023-11-04 06:49:36 -0400

    Detect when installer text is not valid markup and treat it as text.
    Installer scripts can contain whatever some dude pasted into them, and if we can detect that it is not markup, we can display it as plain text.

--- lutris/gui/installer/widgets.py
+++ lutris/gui/installer/widgets.py
@@ -1,4 +1,4 @@
-from gi.repository import Gtk, Pango
+from gi.repository import GLib, Gtk, Pango
 
 
 class InstallerLabel(Gtk.Label):
@@ -6,6 +6,9 @@ class InstallerLabel(Gtk.Label):
 
     def __init__(self, text, wrap=True, selectable=False):
         super().__init__()
+
+        is_valid_markup = InstallerLabel.is_valid_pango_markup(text)
+
         if wrap:
             self.set_line_wrap(True)
             self.set_line_wrap_mode(Pango.WrapMode.WORD_CHAR)
@@ -13,7 +16,30 @@ class InstallerLabel(Gtk.Label):
             self.set_property("ellipsize", Pango.EllipsizeMode.MIDDLE)
         self.set_alignment(0, 0.5)
         self.set_margin_right(12)
-        self.set_markup(text)
+
+        if is_valid_markup:
+            self.set_markup(text)
+        else:
+            self.set_text(text)
+
         self.props.can_focus = False
         self.set_tooltip_text(text)
         self.set_selectable(selectable)
+
+    @staticmethod
+    def is_valid_pango_markup(text):
+        def destroy_func(_user_data):
+            pass  # required by GLib, but we don't need this callback
+
+        if len(text) == 0:
+            return True  # Trivial case - empty strings are always valid
+
+        try:
+            parser = GLib.MarkupParser()
+            context = GLib.MarkupParseContext(parser, GLib.MarkupParseFlags.DEFAULT_FLAGS, None, destroy_func)
+
+            markup = f"<markup>{text}</markup>"
+            context.parse(markup, len(markup))
+            return True
+        except GLib.GError:
+            return False
