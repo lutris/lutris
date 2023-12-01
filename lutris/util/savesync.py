4dc19342edc96809115e00c5e0d54f17b7700c26
Mathieu Comandon <strycore@gmail.com>
2023-11-30 23:33:45 -0800

    Create SaveInfo class

--- lutris/util/savesync.py
+++ lutris/util/savesync.py
@@ -10,6 +10,7 @@ except ImportError:
     WEBDAV_AVAILABLE = False
 
 from lutris import settings
+from lutris.game import Game
 from lutris.util.log import logger
 from lutris.util.strings import human_size
 from lutris.util.wine.prefix import find_prefix
@@ -19,102 +20,110 @@ SAVE_TYPES = ["saves", "logs", "configs", "screenshots"]
 SYNC_TYPES = ["saves", "configs"]
 
 
-def get_dir_info(path):
-    path_files = {}
-    path = path.rstrip("/")
-    if os.path.isfile(path):
-        path_files[os.path.basename(path)] = os.stat(path)
+class SaveInfo:
+    save_types = ["saves", "logs", "configs", "screenshots"]
+    default_synced_types = ["saves", "configs"]
+
+    def __init__(self, game: Game):
+        self.game = game
+        self.save_config = self.game.config.game_level["game"].get("save_config")
+        if not self.save_config:
+            raise ValueError("%s has no save configuration" % self.game)
+        self.basedir = self.get_basedir()
+
+    def get_basedir(self) -> str:
+        save_config = self.game.config.game_level["game"]["save_config"]
+        basedir = save_config.get("basedir") or self.game.directory
+        if not basedir:
+            raise ValueError("No save directory provided")
+        prefix_path = os.path.dirname(self.game.config.game_config.get("exe"))
+
+        if self.game.runner_name == "wine":
+            prefix_path = self.game.config.game_config.get("prefix", "")
+            if prefix_path and not prefix_path.startswith("/"):
+                prefix_path = os.path.join(self.game.directory, prefix_path)
+            exe = self.game.config.game_config.get("exe", "")
+            if exe and not exe.startswith("/"):
+                exe = os.path.join(self.game.directory, exe)
+            if not prefix_path:
+                prefix_path = find_prefix(exe)
+            if "$GAMEDIR" in basedir:
+                basedir = basedir.replace("$GAMEDIR", self.game.directory or prefix_path)
+            if "$USERDIR" in basedir:
+                username = os.getenv("USER") or "steamuser"
+                basedir = basedir.replace("$USERDIR", os.path.join(prefix_path, "drive_c/users/%s" % username))
+        else:
+            if "$GAMEDIR" in basedir:
+                basedir = basedir.replace("$GAMEDIR", self.game.directory)
+            if "$USERDIR" in basedir:
+                basedir = os.path.expanduser(basedir.replace("$USERDIR", "~"))
+        return basedir
+
+    @staticmethod
+    def get_dir_info(path: str) -> dict:
+        path_files = {}
+        path = path.rstrip("/")
+        if os.path.isfile(path):
+            path_files[os.path.basename(path)] = os.stat(path)
+            return path_files
+        for root, _dirs, files in os.walk(path):
+            basedir = root[len(path) + 1:]
+            for path_file in files:
+                path_files[os.path.join(basedir, path_file)] = os.stat(os.path.join(root, path_file))
         return path_files
-    for root, _dirs, files in os.walk(path):
-        basedir = root[len(path) + 1:]
-        for path_file in files:
-            path_files[os.path.join(basedir, path_file)] = os.stat(os.path.join(root, path_file))
-    return path_files
 
+    def format_dir_info(self, path: str) -> list:
+        path_files = self.get_dir_info(path)
+        output = []
+        for path_file in sorted(path_files, key=lambda k: path_files[k].st_mtime, reverse=True):
+            fstats = path_files[path_file]
+            output.append({
+                "file": path_file,
+                "size": fstats.st_size,
+                "modified": fstats.st_mtime,
+            })
+        return output
 
-def format_dir_info(path):
-    path_files = get_dir_info(path)
-    output = []
-    for path_file in sorted(path_files, key=lambda k: path_files[k].st_mtime, reverse=True):
-        fstats = path_files[path_file]
-        output.append({
-            "file": path_file,
-            "size": fstats.st_size,
-            "modified": fstats.st_mtime,
-        })
-    return output
+    def get_save_files(self) -> dict:
+        """Return list of files with their details for each type along with metadata related to the save info"""
+        results = {"basedir": self.basedir, "config": self.save_config}
+        for section in self.save_types:
+            if section in self.save_config:
+                results[section] = {}
+                path = os.path.join(self.basedir, self.save_config[section])
+                results[section]["path"] = path
+                results[section]["files"] = self.format_dir_info(path)
+        return results
 
-
-def print_dir_details(title, path):
-    save_files = get_dir_info(path)
-    if not save_files:
-        return
-    print(title)
-    print("=" * len(title))
-    total_size = 0
-    for save_file in sorted(save_files, key=lambda k: save_files[k].st_mtime, reverse=True):
-        fstats = save_files[save_file]
-        total_size += fstats.st_size
-        print("%s (%s)\t\t%s" % (save_file, human_size(fstats.st_size),
-              datetime.fromtimestamp(fstats.st_mtime).strftime("%c")))
-    print("Total size: %s" % human_size(total_size))
-
-
-def get_basedir(game):
-    save_config = game.config.game_level["game"]["save_config"]
-    basedir = save_config.get("basedir") or game.directory
-    if not basedir:
-        logger.error("No save directory found")
-    prefix_path = os.path.dirname(game.config.game_config.get("exe"))
-    username = os.getenv("USER") or "steamuser"
-
-    if game.runner_name == "wine":
-        prefix_path = game.config.game_config.get("prefix", "")
-        if prefix_path and not prefix_path.startswith("/"):
-            prefix_path = os.path.join(game.directory, prefix_path)
-        exe = game.config.game_config.get("exe", "")
-        if exe and not exe.startswith("/"):
-            exe = os.path.join(game.directory, exe)
-        if not prefix_path:
-            prefix_path = find_prefix(exe)
-        if "$GAMEDIR" in basedir:
-            basedir = basedir.replace("$GAMEDIR", game.directory or prefix_path)
-        if "$USERDIR" in basedir:
-            basedir = basedir.replace("$USERDIR", os.path.join(prefix_path, "drive_c/users/%s" % username))
-    else:
-        if "$GAMEDIR" in basedir:
-            basedir = basedir.replace("$GAMEDIR", game.directory)
-        if "$USERDIR" in basedir:
-            basedir = os.path.expanduser(basedir.replace("$USERDIR", "~"))
-    return basedir
-
-
-def get_save_info(basedir, save_config):
-    results = {"basedir": basedir, "config": save_config}
-    for section in ["saves", "logs", "config", "screenshots"]:
-        if section in save_config:
-            results[section] = {}
-            path = os.path.join(basedir, save_config[section])
-            results[section]["path"] = path
-            results[section]["files"] = format_dir_info(path)
-    return results
+    def print_dir_details(self, title: str, path: str):
+        save_files = self.get_dir_info(path)
+        if not save_files:
+            return
+        print(title)
+        print("=" * len(title))
+        total_size = 0
+        for save_file in sorted(save_files, key=lambda k: save_files[k].st_mtime, reverse=True):
+            fstats = save_files[save_file]
+            total_size += fstats.st_size
+            print("%s (%s)\t\t%s" % (save_file, human_size(fstats.st_size),
+                                     datetime.fromtimestamp(fstats.st_mtime).strftime("%c")))
+        print("Total size: %s" % human_size(total_size))
 
 
 def show_save_stats(game, output_format="text"):
-    save_config = game.config.game_level["game"].get("save_config")
-    if not save_config:
-        logger.error("%s has no save configuration")
+    try:
+        save_info = SaveInfo(game)
+    except ValueError:
+        logger.error("Save information for %s unavailable", game)
         return
-    basedir = get_basedir(game)
+
     if output_format == "json":
-        print(json.dumps(get_save_info(basedir, save_config), indent=2))
+        print(json.dumps(save_info.get_save_files()), indent=2)
     else:
-        if "saves" in save_config:
-            print_dir_details("Saves", os.path.join(basedir, save_config["saves"]))
-        if "logs" in save_config:
-            print_dir_details("Logs", os.path.join(basedir, save_config["logs"]))
-        if "config" in save_config:
-            print_dir_details("Config", os.path.join(basedir, save_config["config"]))
+        for section in save_info.save_types:
+            if section in save_info.save_config:
+                save_info.print_dir_details(section.capitalize(), os.path.join(
+                    save_info.basedir, save_info.save_config[section]))
 
 
 def create_dirs(client, path):
@@ -162,11 +171,15 @@ def get_existing_saves(client, game_base_dir):
     return saves
 
 
-def upload_save(game, sections=SYNC_TYPES):
-    save_config = game.config.game_level["game"].get("save_config")
-    if not save_config:
-        logger.error("%s has no save configuration")
+def upload_save(game, sections=None):
+    if not sections:
+        sections = SYNC_TYPES
+    try:
+        save_info = SaveInfo(game)
+    except ValueError:
+        logger.error("Can't get save info for %s", game)
         return
+
     print("Uploading save for %s" % game)
     webdav_saves_path = settings.read_setting("webdav_saves_path")
     if not webdav_saves_path:
@@ -181,8 +194,7 @@ def upload_save(game, sections=SYNC_TYPES):
     # for save in existing_saves:
     #     print(save)
     #     os.remove(save)
-    basedir = get_basedir(game)
-    save_info = get_save_info(basedir, save_config)
+
     max_time = 0
     for section in sections:
         for save_file in save_info[section]["files"]:
@@ -193,18 +205,18 @@ def upload_save(game, sections=SYNC_TYPES):
     save_dest_dir = os.path.join(webdav_saves_path, game.slug, save_id)
     create_dirs(client, save_dest_dir)
 
-    save_info = get_save_info(basedir, save_config)
+    save_files = save_info.get_save_files()
     save_info_name = "saveinfo.json"
     save_info_path = os.path.join(settings.CACHE_DIR, save_info_name)
     with open(save_info_path, "w", encoding="utf-8") as save_info_file:
         json.dump(save_info, save_info_file, indent=2)
     client.upload_file(save_info_path, os.path.join(save_dest_dir, save_info_name))
-    basepath = save_info["saves"]["path"]
+    basepath = save_files["saves"]["path"]
     if os.path.isfile(basepath):
         basepath = os.path.dirname(basepath)
-    relpath = basepath[len(save_info["basedir"]) + 1:]
+    relpath = basepath[len(save_files["basedir"]) + 1:]
     create_dirs(client, os.path.join(save_dest_dir, relpath))
-    for save_file in save_info["saves"]["files"]:
+    for save_file in save_files["saves"]["files"]:
         upload_file_source = os.path.join(basepath, save_file["file"])
         upload_file_dest = os.path.join(save_dest_dir, relpath, save_file["file"])
         print(upload_file_source, ">", upload_file_dest)
@@ -217,17 +229,20 @@ def load_save_info(save_info_path):
     return save_info
 
 
-def parse_save_info(save_info_path):
+def parse_save_info(save_info_path: str):
+    """Parse the filename of a save info file and extract its information"""
     save_info_name, _ext = os.path.splitext(os.path.basename(save_info_path))
     hostname, ts = save_info_name.rsplit("-", maxsplit=1)
     return {"hostname": hostname, "datetime": datetime.fromtimestamp(int(ts))}
 
 
 def save_check(game):
-    save_config = game.config.game_level["game"].get("save_config")
-    if not save_config:
-        logger.error("%s has no save configuration")
+    try:
+        save_info = SaveInfo(game)
+    except ValueError:
+        logger.error("%s has no save configuration", game)
         return
+
     print("Checking sync of save for %s" % game)
     webdav_saves_path = settings.read_setting("webdav_saves_path")
     if not webdav_saves_path:
@@ -236,10 +251,10 @@ def save_check(game):
     client = get_webdav_client()
     if not client:
         return
-    basedir = get_basedir(game)
+
     remote_basedir = os.path.join(webdav_saves_path, game.slug)
     existing_saves = get_existing_saves(client, remote_basedir)
-    current_save_info = get_save_info(basedir, save_config)
+    current_save_files = save_info.get_save_files()
     for save_path in existing_saves:
         save_info_meta = parse_save_info(save_path)
         host = save_info_meta["hostname"]
@@ -247,10 +262,10 @@ def save_check(game):
         save_info = load_save_info(save_path)
         unsynced = {}
         for section in SAVE_TYPES:
-            if section not in current_save_info:
+            if section not in current_save_files:
                 continue
             files = {file_info["file"]: file_info for file_info in save_info[section]["files"]}
-            local_files = {file_info["file"]: file_info for file_info in current_save_info[section]["files"]}
+            local_files = {file_info["file"]: file_info for file_info in current_save_files[section]["files"]}
             unsynced[section] = {"unsynced": [], "newer": [], "older": [], "missing": []}
 
             for filename, file_info in local_files.items():
