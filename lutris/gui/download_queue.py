272da8ba819469c8c29c1308e1fa934a08f693d1
Daniel Johnson <danieljohnson2@fastmail.com>
2023-11-30 19:51:21 -0500

    Commenting and simplification of DownloadQueue

--- lutris/gui/download_queue.py
+++ lutris/gui/download_queue.py
@@ -11,6 +11,8 @@ from lutris.util import datapath
 
 @GtkTemplate(ui=os.path.join(datapath.get(), "ui", "download-queue.ui"))
 class DownloadQueue(Gtk.ScrolledWindow):
+    """This class is a widget that displays a stack of progress boxes, which you can create
+    and destroy with its methods."""
     __gtype_name__ = "DownloadQueue"
 
     download_box: Gtk.Box = GtkTemplate.Child()
@@ -30,27 +32,37 @@ class DownloadQueue(Gtk.ScrolledWindow):
             pass
 
     def add_progress_box(self, progress_function: ProgressBox.ProgressFunction) -> ProgressBox:
-        def start_update():
-            def check_progress():
-                progress_info = progress_function()
+        """Adds a progress box to the queue; it will display the progress indicated by
+        the progress_function, which is called immediately to initialize the box and
+        then polled to update it. Returns the new progress box.
 
-                if progress_info.label_markup:
-                    progress_info.label_markup = "<span size='10000'>%s</span>" % progress_info.label_markup
+        If called with a progress_function that has a box already, this method returns
+        that box instead of creating one."""
 
-                box.show()
-                return progress_info
+        def check_progress():
+            progress_info = progress_function()
 
-            box = ProgressBox(check_progress, visible=False, margin=6)
-            box.update_progress()
-            return box
+            if progress_info.label_markup:
+                progress_info.label_markup = "<span size='10000'>%s</span>" % progress_info.label_markup
 
-        progress_box = start_update()
+            return progress_info
+
+        progress_box = self.progress_boxes.get(progress_function)
+        if progress_box:
+            progress_box.update_progress()
+            return progress_box
+
+        progress_box = ProgressBox(check_progress, visible=False, margin=6)
+        progress_box.update_progress()
+
         self.progress_boxes[progress_function] = progress_box
         self.download_box.pack_start(progress_box, False, False, 0)
         progress_box.show()
         return progress_box
 
     def remove_progress_box(self, progress_function: ProgressBox.ProgressFunction) -> None:
+        """Removes and destroys the progress box created for the progress_function given,
+        if any is present."""
         progress_box = self.progress_boxes.get(progress_function)
         if progress_box:
             del self.progress_boxes[progress_function]
