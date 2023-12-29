6d7a65ac9ce4d8150a5c894c7c10d5ac29608da0
Mathieu Comandon <strycore@gmail.com>
2023-12-28 21:20:40 -0800

    Ok, I sort of see why but we should really consider another solution

--- lutris/scanners/lutris.py
+++ lutris/scanners/lutris.py
@@ -185,7 +185,7 @@ def remove_from_path_cache(game):
     get_path_cache.cache_clear()
 
 
-@lru_cache()  # why
+@lru_cache()
 def get_path_cache():
     """Return the contents of the path cache file; this
     dict is cached, so do not modify it."""
