8508ff9fdb0f41093a870bd01a936f36854d0f9b
Daniel Johnson <danieljohnson2@fastmail.com>
2024-02-09 04:59:24 -0500

    Pass correct object to get_installer_runner_version()
    It's supposed to be the installer not the interpreter. Looks like a copy & paste error.
    
    Resolves #5300

--- lutris/installer/interpreter.py
+++ lutris/installer/interpreter.py
@@ -277,7 +277,7 @@ class ScriptInterpreter(GObject.Object, CommandsMixin):
         try:
             runner.install(
                 ui_delegate,
-                version=runner.get_installer_runner_version(self) if runner.has_runner_versions else None,
+                version=runner.get_installer_runner_version(self.installer) if runner.has_runner_versions else None,
                 callback=install_more_runners,
             )
         except (NonInstallableRunnerError, RunnerInstallationError) as ex:
