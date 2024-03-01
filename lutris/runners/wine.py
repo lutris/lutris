01765e1b6fb9d2b7269fca9e1f6603d78b6f2565
Mathieu Comandon <strycore@gmail.com>
2024-03-01 01:58:44 -0800

    Revert "Fix check for 'ulwgl-run' presence."
    This reverts commit fadc55eb2d359cb821add2186accc1c15e8881da.

--- lutris/runners/wine.py
+++ lutris/runners/wine.py
@@ -1172,12 +1172,9 @@ class wine(Runner):
 
     def get_command(self):
         exe = self.get_executable()
-        try:
-            ulwgl_path = system.find_executable("ulwgl-run")
-            if "Proton" in exe and "lutris" not in exe:
-                return [ulwgl_path]
-        except MissingExecutableError:
-            pass  # No ULWGL, fall back to normal Wine
+        ulwgl_path = system.find_executable("ulwgl-run")
+        if "Proton" in exe and "lutris" not in exe and ulwgl_path:
+            return [ulwgl_path]
         return super().get_command()
 
     def play(self):  # pylint: disable=too-many-return-statements # noqa: C901
