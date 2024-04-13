1186a730a58cbe87903aa3ec408776c91b745c58
Daniel Johnson <danieljohnson2@fastmail.com>
2024-04-13 07:29:49 -0400

    Provide access to the search components; lazy load the predicates.
    This should support editing searches later, even editing invalid ones.

--- lutris/search.py
+++ lutris/search.py
@@ -1,4 +1,4 @@
-from typing import Any, Callable, Dict, List, Optional
+from typing import Any, Callable, Dict, List, Optional, Tuple
 
 from lutris.database import games
 from lutris.database.categories import (
@@ -12,39 +12,60 @@ from lutris.util.strings import strip_accents
 
 class BaseSearch:
     flag_texts = {"true": True, "yes": True, "false": False, "no": False}
+    tags = []
 
     def __init__(self, text: str) -> None:
         self.text = text
-        self.predicates: List[Callable] = []
-
-        if text:
-            for part in text.split():
-                if ":" in part:
-                    pos = part.index(":", 1)
-                    name = part[:pos]
-                    value = part[(pos + 1) :]
-                    predicate = self.get_part_predicate(name, value)
-                    if predicate:
-                        self.add_predicate(predicate)
-                        continue
-
-                self.add_predicate(self.get_text_predicate(part))
+        self.predicates: Optional[List[Callable]] = None
 
     def __str__(self) -> str:
         return self.text
 
+    @property
+    def is_empty(self) -> bool:
+        return not self.text
+
+    def get_candidate_text(self, candidate: Any) -> str:
+        return str(candidate)
+
+    def get_components(self) -> List[Tuple[str, str, str]]:
+        components = []
+
+        for raw in self.text.split():
+            if ":" in raw:
+                pos = raw.index(":", 1)
+                name = raw[:pos].strip().casefold()
+                if name in self.tags:
+                    value = raw[(pos + 1) :].strip()
+                    components.append((name, value, raw))
+                    continue
+            components.append(("", raw, raw))
+
+        return components
+
+    def get_predicates(self) -> List[Callable]:
+        if self.predicates is None:
+            predicates = []
+            if self.text:
+                for name, value, raw in self.get_components():
+                    if name:
+                        predicate = self.get_part_predicate(name, value)
+                        if predicate:
+                            predicates.append(predicate)
+                            continue
+
+                    predicates.append(self.get_text_predicate(raw))
+            self.predicates = predicates
+        return self.predicates
+
     def add_predicate(self, predicate: Callable) -> None:
-        self.predicates.append(predicate)
+        self.get_predicates().append(predicate)
 
     def get_part_predicate(self, name: str, value: str) -> Optional[Callable]:
         return None
 
-    @property
-    def is_empty(self) -> bool:
-        return not self.predicates
-
     def matches(self, candidate: Any) -> bool:
-        for predicate in self.predicates:
+        for predicate in self.get_predicates():
             if not predicate(candidate):
                 return False
 
@@ -59,11 +80,10 @@ class BaseSearch:
 
         return match_text
 
-    def get_candidate_text(self, candidate: Any) -> str:
-        return str(candidate)
-
 
 class GameSearch(BaseSearch):
+    tags = ["installed", "categorized", "category", "runner", "platform"]
+
     def __init__(self, text: str, service) -> None:
         self.service = service
         super().__init__(text)
@@ -153,6 +173,8 @@ class GameSearch(BaseSearch):
 
 
 class RunnerSearch(BaseSearch):
+    tags = ["installed"]
+
     def get_candidate_text(self, candidate: Any) -> str:
         return f"{candidate.name}\n{candidate.description}"
 
