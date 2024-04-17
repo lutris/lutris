7654ac7173267e43f6352037efd3f8c5f2519829
Daniel Johnson <danieljohnson2@fastmail.com>
2024-04-17 19:30:54 -0400

    Rework playtime so >= and <= are operators, and so that "playtime:>=3 hours playtime:<=20 hours" will work with no explicit AND.

--- lutris/util/tokenization.py
+++ lutris/util/tokenization.py
@@ -13,7 +13,7 @@ def clean_token(to_clean: Optional[str]) -> str:
     return to_clean.strip()
 
 
-def tokenize_search(text: str, isolated_tokens: Set[str]) -> Iterable[str]:
+def tokenize_search(text: str, isolated_tokens: Iterable[str]) -> Iterable[str]:
     """Iterates through a text and breaks in into tokens. Every character of the text is present
     in exactly one token returned, all in order, so the original text can be reconstructed by concatenating the
     tokens.
@@ -21,7 +21,10 @@ def tokenize_search(text: str, isolated_tokens: Set[str]) -> Iterable[str]:
     Tokens are separated by whitespace, but also certain characters (isolated_characters) are kept as separate tokens.
     Double-quoted text are protected from further tokenization."""
 
-    def _tokenize():
+    isolating_chars = set(ch for tok in isolated_tokens for ch in tok)
+    isolated_tokens = sorted(isolated_tokens, key=lambda tok: -len(tok))
+
+    def basic_tokenize():
         buffer = ""
         it = iter(text)
         while True:
@@ -33,13 +36,7 @@ def tokenize_search(text: str, isolated_tokens: Set[str]) -> Iterable[str]:
                 yield buffer
                 buffer = ""
 
-            # TODO: Support longer tokens here
-            if ch in isolated_tokens:
-                yield buffer
-                yield ch
-                buffer = ""
-                continue
-            elif ch == '"':
+            if ch == '"':
                 yield buffer
 
                 buffer = ch
@@ -60,8 +57,25 @@ def tokenize_search(text: str, isolated_tokens: Set[str]) -> Iterable[str]:
             buffer += ch
         yield buffer
 
+    def split_isolated_tokens(tokens: Iterable[str]) -> Iterable[str]:
+        for token in tokens:
+            i = 0
+            while i < len(token):
+                if token[i] in isolating_chars:
+                    for candidate in isolated_tokens:
+                        if token[i:].startswith(candidate):
+                            yield token[:i]
+                            yield candidate
+                            token = token[(i + len(candidate)) :]
+                            i = -1  # start again with reduced token!
+                            break
+                i += 1
+            yield token
+
     # Since we blindly return empty buffers, we must now filter them out
-    return filter(lambda t: len(t) > 0, _tokenize())
+    basic = basic_tokenize()
+    isolated = split_isolated_tokens(basic)
+    return filter(lambda t: len(t) > 0, isolated)
 
 
 class TokenReader:
@@ -114,15 +128,16 @@ class TokenReader:
                     buffer = token
                 break
             buffer += token
-        return buffer if buffer else None
+        return clean_token(buffer) if buffer else None
 
     def peek_token(self) -> Optional[str]:
         """Returns the next token, or None if the end of tokens has been reached. However,
         will not advance - repeated calls return the same token."""
-        if self.index >= len(self.tokens):
-            return None
 
-        return self.tokens[self.index]
+        saved_index = self.index
+        token = self.get_token()
+        self.index = saved_index
+        return token
 
     def consume(self, candidate: str) -> bool:
         """If the next token is 'candidate', advances over it and returns True;
