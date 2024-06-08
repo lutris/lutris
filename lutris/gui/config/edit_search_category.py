5ff89ccbaeadc3ba0025d5a6db3efb8278fe90d6
Daniel Johnson <danieljohnson2@fastmail.com>
2024-06-08 09:39:45 -0400

    Add the search itself to the search edit dialog, and hook this up so you can change the search.

--- lutris/gui/config/edit_search_category.py
+++ lutris/gui/config/edit_search_category.py
@@ -6,6 +6,7 @@ from gi.repository import Gtk
 
 from lutris.database import categories as categories_db
 from lutris.gui.dialogs import QuestionDialog, SavableModelessDialog
+from lutris.search import GameSearch
 
 
 class EditSearchCategoryDialog(SavableModelessDialog):
@@ -22,14 +23,10 @@ class EditSearchCategoryDialog(SavableModelessDialog):
 
         self.vbox.set_homogeneous(False)
         self.vbox.set_spacing(10)
-        name_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=6)
-        name_label = Gtk.Label(_("Name"))
-        self.name_entry = Gtk.Entry()
-        self.name_entry.set_text(self.category)
-        name_box.pack_start(name_label, False, False, 0)
-        name_box.pack_start(self.name_entry, True, True, 0)
 
-        self.vbox.pack_start(name_box, False, False, 0)
+        self.name_entry = self._add_entry_box(_("Name"), self.category)
+        self.search_entry = self._add_entry_box(_("Search"), self.search)
+
         # self.vbox.pack_start(self._create_games_checkboxes(), True, True, 0)
 
         delete_button = self.add_styled_button(Gtk.STOCK_DELETE, Gtk.ResponseType.NONE, css_class="destructive-action")
@@ -37,6 +34,16 @@ class EditSearchCategoryDialog(SavableModelessDialog):
         delete_button.set_sensitive(bool(self.category_id))
 
         self.show_all()
+
+    def _add_entry_box(self, label: str, text: str) -> Gtk.Entry:
+        hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=6)
+        entry_label = Gtk.Label(label)
+        entry = Gtk.Entry()
+        entry.set_text(text)
+        hbox.pack_start(entry_label, False, False, 0)
+        hbox.pack_start(entry, True, True, 0)
+        self.vbox.pack_start(hbox, False, False, 0)
+        return entry
 
     def on_delete_clicked(self, _button):
         dlg = QuestionDialog(
@@ -56,21 +63,24 @@ class EditSearchCategoryDialog(SavableModelessDialog):
         """Save game info and destroy widget."""
         old_name: str = self.category
         new_name: str = categories_db.strip_category_name(self.name_entry.get_text())
+        old_search: str = self.search
+        new_search: str = str(GameSearch(self.search_entry.get_text()))
+
+        if not new_name:
+            new_name = old_name
 
         if categories_db.is_reserved_category(new_name):
             raise RuntimeError(_("'%s' is a reserved category name.") % new_name)
 
-        if not self.category_id:
-            # Creating new category!
-            categories_db.add_category(category_name=new_name, search=self.search)
-        elif new_name and old_name != new_name:
-            # Rename existing category
-            if categories_db.is_reserved_category(new_name):
-                raise RuntimeError(_("'%s' is a reserved category name.") % new_name)
-
+        if old_name != new_name:
             if new_name in (c["name"] for c in categories_db.get_categories()):
                 raise RuntimeError(_("'%s' is already a category, and search-based categories can't be merged."))
-            else:
-                categories_db.rename_category(self.category_id, new_name)
+
+        if not self.category_id:
+            # Creating new category!
+            categories_db.add_category(category_name=new_name, search=new_search)
+        elif old_name != new_name or old_search != new_search:
+            # Changing an existing category.
+            categories_db.redefine_category(self.category_id, new_name, new_search)
 
         self.destroy()
