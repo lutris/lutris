886ed7c6cb45de23bbf3447669cf842635994f10
Tarulia <mihawk.90+git@googlemail.com>
2024-10-05 17:37:54 +0200

    Add `TryExec` key to desktop entries
    Generated desktop entries are non-functional when Lutris is not
    installed (e.g. after user uninstalls Lutris or reinstalls their OS).
    We can use `TryExec` to hide such entries. Upon reinstallation of Lutris
    they will automatically be shown again with no further action by users.
    
    > `TryExec`[1]
    >
    > Path to an executable file on disk used to determine if the program is
    > actually installed. If the path is not an absolute path, the file is
    > looked up in the $PATH environment variable. If the file is not
    > present or if it is not executable, the entry may be ignored (not be
    > used in menus, for example).
    
    Note this only takes a single executable as a value. For that reason it
    is not possible to use this key for Flatpaks. The presence of `flatpak`
    does not guarantee the Lutris Flatpak actually being installed.
    The use of `flatpak run net.lutris.Lutris` validates in
    `desktop-file-validate`, however KDE at least does not show the entry.
    
    An empty value also validates and doesn't hide the entry, so leaving it
    for Flatpaks appears fine.
    
    [1]: https://specifications.freedesktop.org/desktop-entry-spec/latest/recognized-keys.html

--- lutris/util/xdgshortcuts.py
+++ lutris/util/xdgshortcuts.py
@@ -65,6 +65,8 @@ def create_launcher(game_slug, game_id, game_name, launch_config_name=None, desk
     # field code in the Exec key.
     command = f"{lutris_executable} {shlex.quote(url)}".replace("%", "%%")
 
+    try_exec = "" if LINUX_SYSTEM.is_flatpak() else "lutris"
+
     launcher_content = dedent(
         """
         [Desktop Entry]
@@ -73,7 +75,8 @@ def create_launcher(game_slug, game_id, game_name, launch_config_name=None, desk
         Icon={}
         Exec=env LUTRIS_SKIP_INIT=1 {}
         Categories=Game
-        """.format(game_name, f"lutris_{game_slug}", command)
+        TryExec={}
+        """.format(game_name, f"lutris_{game_slug}", command, try_exec)
     )
 
     launcher_filename = get_xdg_basename(game_slug, game_id)
