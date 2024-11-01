b18410e928f04b96b9c080cfe99a0dcecd9eddf2
GloriousEggroll <gloriouseggroll@gmail.com>
2024-11-01 10:39:53 -0600

    don't nest proton folder inside wine folder, we don't want it being picked up as a wine build by mistake

--- lutris/util/wine/proton.py
+++ lutris/util/wine/proton.py
@@ -132,10 +132,9 @@ def get_proton_versions() -> Dict[str, str]:
                     versions[version] = wine_path
     return versions
 
-
 def _iter_proton_locations() -> Generator[str, None, None]:
     """Iterate through all potential Proton locations"""
-    yield os.path.join(settings.RUNNER_DIR, "wine")
+    yield os.path.join(settings.RUNNER_DIR, "proton")
 
     try:
         steamapp_dirs = get_steamapps_dirs()
