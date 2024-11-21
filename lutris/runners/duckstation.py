78000013521826e817a600fe076441bc0b6141d6
Mathieu Comandon <strycore@gmail.com>
2024-11-21 00:13:55 -0800

    Format

--- lutris/runners/duckstation.py
+++ lutris/runners/duckstation.py
@@ -1,11 +1,11 @@
 from gettext import gettext as _
+from os import path
 
 from lutris import settings
 from lutris.exceptions import MissingGameExecutableError
 from lutris.runners.runner import Runner
 from lutris.util import system
 
-from os import path
 
 class duckstation(Runner):
     human_name = _("DuckStation")
@@ -16,7 +16,7 @@ class duckstation(Runner):
     runner_executable = runner_directory + "/DuckStation-x64.AppImage"
     settings_directory = path.expanduser("~/.local/share/" + runner_directory)
     download_url = "https://github.com/stenzek/duckstation/releases/download/latest/DuckStation-x64.AppImage"
-    
+
     game_options = [
         {
             "option": "main_file",
@@ -26,7 +26,7 @@ class duckstation(Runner):
             "default_path": "game_path",
         }
     ]
-    
+
     runner_options = [
         {
             "option": "bigpicture",
@@ -57,14 +57,14 @@ class duckstation(Runner):
             "default": False,
         },
     ]
-    
+
     system_options_override = [
         {
             "option": "disable_runtime",
             "default": True,
         },
     ]
-    
+
     def uninstall(self, uninstall_callback=None):
         if path.isdir(self.settings_directory):
             system.delete_folder(self.settings_directory)
@@ -74,7 +74,7 @@ class duckstation(Runner):
 
     def run(self, ui_delegate):
         initial_arguments = self.get_command()
-        
+
         # Big Picture
         if self.runner_config.get("bigpicture"):
             initial_arguments.append("-bigpicture")
@@ -82,7 +82,7 @@ class duckstation(Runner):
 
     def play(self):
         arguments = self.get_command()
-        
+
         # Big Picture
         if self.runner_config.get("bigpicture"):
             arguments.append("-bigpicture")
