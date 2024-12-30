6d95fd325c50f728830911693a815712302509e1
Daniel Johnson <danieljohnson2@fastmail.com>
2024-12-30 07:03:45 -0500

    Save YAML files via a temporary file for safety.
    If Lutris gets killed while saving, we want to leave the old YAML file in place, so we'll do the save-to-temp-and-rename trick.
    
    I'm thinking of the migrate_ge_proton migration, which does a lot of this and might be killed by an impatient user. But this is generally a good practice.

--- lutris/util/yaml.py
+++ lutris/util/yaml.py
@@ -1,5 +1,7 @@
 """Utility functions for YAML handling"""
 
+import os
+
 # pylint: disable=no-member
 import yaml
 
@@ -7,7 +9,7 @@ from lutris.util.log import logger
 from lutris.util.system import path_exists
 
 
-def read_yaml_from_file(filename: str):
+def read_yaml_from_file(filename: str) -> dict:
     """Read filename and return parsed yaml"""
     if not path_exists(filename):
         return {}
@@ -20,7 +22,14 @@ def read_yaml_from_file(filename: str):
     return yaml_content
 
 
-def write_yaml_to_file(config: dict, filepath: str):
+def write_yaml_to_file(config: dict, filepath: str) -> None:
     yaml_config = yaml.safe_dump(config, default_flow_style=False)
-    with open(filepath, "w", encoding="utf-8") as filehandler:
-        filehandler.write(yaml_config)
+
+    temp_path = filepath + ".tmp"
+    try:
+        with open(temp_path, "w", encoding="utf-8") as filehandler:
+            filehandler.write(yaml_config)
+        os.rename(temp_path, filepath)
+    finally:
+        if os.path.isfile(temp_path):
+            os.unlink(temp_path)
