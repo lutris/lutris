1ae630a8b98eb1638b0d5eb8d753b820e9284b03
Daniel Johnson <danieljohnson2@fastmail.com>
2025-02-05 16:43:54 -0500

    Replace logger.error with exception
    Lutris can't really continue after failing to execute a cursor; we need to abort the current operation, or else we'll just crash later when we try to use the query.
    
    See #5910.

--- lutris/database/sql.py
+++ lutris/database/sql.py
@@ -1,8 +1,6 @@
 import sqlite3
 import threading
 
-from lutris.util.log import logger
-
 # Prevent multiple access to the database (SQLite limitation)
 DB_LOCK = threading.RLock()
 
@@ -27,8 +25,7 @@ def cursor_execute(cursor, query, params=None):
     params = params or ()
     lock = DB_LOCK.acquire(timeout=1)  # pylint: disable=consider-using-with
     if not lock:
-        logger.error("Database is busy. Not executing %s", query)
-        return
+        raise RuntimeError(f"Database is busy. Not executing {query}")
 
     try:
         return cursor.execute(query, params)
