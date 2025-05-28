7e9d1b140bc1fd626d8fef109e860031304a381b
Zebra2711 <zebra2711t@gmail.com>
2025-05-28 22:46:02 +0700

    winekill: Force-kill processes if waiting too long

--- lutris/runners/commands/wine.py
+++ lutris/runners/commands/wine.py
@@ -217,9 +217,19 @@ def winekill(prefix, arch=WINE_DEFAULT_ARCH, wine_path=None, env=None, initial_p
 
         if not running_processes:
             break
+
         if num_cycles > 20:
             logger.warning("Some wine processes are still running: %s", running_processes)
             break
+
+        if num_cycles > 35:
+            logger.warning("Wine processes running too long — force killing: %s", running_processes)
+            command = [os.path.join(wine_root, "wineboot"), "-k"]
+            logger.debug(command)
+            logger.debug(" ".join(command))
+            system.execute(command, env=env, quiet=True)
+            break
+
         time.sleep(0.1)
     logger.debug("Done waiting.")
 
