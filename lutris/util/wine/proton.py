690814938c9bf410ee896d194a3ca25ad806fa0a
Mathieu Comandon <strycore@gmail.com>
2025-06-03 06:17:18 -0700

    PROTON_DIR is not a thing lol

--- lutris/util/wine/proton.py
+++ lutris/util/wine/proton.py
@@ -13,7 +13,6 @@ from lutris.util.steam.config import get_steamapps_dirs
 from lutris.util.strings import get_natural_sort_key
 
 DEFAULT_GAMEID = "umu-default"
-PROTON_DIR: str = os.path.join(settings.RUNNER_DIR, "proton")
 
 
 def is_proton_version(version: Optional[str]) -> bool:
@@ -138,8 +137,6 @@ def get_proton_versions() -> Dict[str, str]:
 
 def _iter_proton_locations() -> Generator[str, None, None]:
     """Iterate through all potential Proton locations"""
-    yield PROTON_DIR
-
     try:
         steamapp_dirs = get_steamapps_dirs()
     except:
