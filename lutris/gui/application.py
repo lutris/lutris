262e93e2253e07059e24ba5f181ad96c2476edb3
andrew-ld <43882924+andrew-ld@users.noreply.github.com>
2025-06-26 10:42:51 +0200

    rename signal handler function name

--- lutris/gui/application.py
+++ lutris/gui/application.py
@@ -765,12 +765,12 @@ class Application(Gtk.Application):
 
             if self.quit_on_game_exit:
 
-                def on_sigterm_signal(signum, _frame):
+                def game_stop_signal_handler(signum, _frame):
                     logger.debug("signal handler called with signal: %d", signum)
                     game.stop()
 
-                signal.signal(signal.SIGTERM, on_sigterm_signal)
-                signal.signal(signal.SIGINT, on_sigterm_signal)
+                signal.signal(signal.SIGTERM, game_stop_signal_handler)
+                signal.signal(signal.SIGINT, game_stop_signal_handler)
         elif self.window:
             # If we're showing the window, it will handle the delegated UI
             # from here on out, no matter what command line we got.
