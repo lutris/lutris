9da18f389987c02cb0320f97c18630a3b7a4b647
Eikeno <eikenault@gmail.com>
2025-08-20 17:05:01 +0200

    bump version to 2123.1

--- lutris/runners/azahar.py
+++ lutris/runners/azahar.py
@@ -4,7 +4,7 @@ from lutris.exceptions import MissingGameExecutableError
 from lutris.runners.runner import Runner
 from lutris.util import system
 
-VER_STR = "2123"
+VER_STR = "2123.1"
 
 
 class azahar(Runner):
