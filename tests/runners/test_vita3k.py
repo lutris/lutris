1f6dae37256263b1ee02634ff2632eaae318fc4c
ItsAllAboutTheCode <52703+ItsAllAboutTheCode@users.noreply.github.com>
2025-09-23 05:31:22 -0500

    Fix Lutris GUI reporting Vita installed titles as missing
    The Vita3K emulator launches Vita titles using their Title ID and not a file path.
    Therefore the Vita3K runner is setup to accept the title ID field.
    However that title ID field was set as the 'main_file' option in the config yaml that gets generated when a Vita entry is added.
    The missing installed title check in Lutris invokes the game.py [get_path_from_config](https://github.com/lutris/lutris/blob/4620e1ba2fafb90ac1eef99dad4e5f65017d8554/lutris/game.py#L643) which treats the `main_file` key as a file path.
    
    As the Vita title ID is not a valid file path, the Missing Games check would flag all Vita installed titles as missing.
    
    The fix here is to change the key from 'main_file' to 'title_id' so that the field is not treated as a file path.

--- tests/runners/test_vita3k.py
+++ tests/runners/test_vita3k.py
@@ -21,8 +21,8 @@ class TestVita3kRunner(unittest.TestCase):
         with self.assertRaises(MissingVitaTitleIDError):
             self.runner.play()
 
-    @patch("lutris.util.system.path_exists")
     @patch("os.path.isfile")
+    @patch("lutris.util.system.path_exists")
     def test_play_fullscreen(self, mock_path_exists, mock_isfile):
         main_file = "PCSG00042"
         mock_path_exists.return_value = True
@@ -34,10 +34,23 @@ class TestVita3kRunner(unittest.TestCase):
         expected = {"command": [self.runner.get_executable(), "-F", "-r", main_file]}
         self.assertEqual(self.runner.play(), expected)
 
-    @patch("lutris.util.system.path_exists")
     @patch("os.path.isfile")
+    @patch("lutris.util.system.path_exists")
     def test_play(self, mock_path_exists, mock_isfile):
-        main_file = "/valid/path/to/iso"
+        title_id = "PCSG00009"
+        mock_path_exists.return_value = True
+        mock_isfile.return_value = True
+        mock_config = MagicMock()
+        mock_config.game_config = {"title_id": title_id}
+        mock_config.runner_config = {"fullscreen": False}
+        self.runner.config = mock_config
+        expected = {"command": [self.runner.get_executable(), "-r", title_id]}
+        self.assertEqual(self.runner.play(), expected)
+
+    @patch("os.path.isfile")
+    @patch("lutris.util.system.path_exists")
+    def test_legacy_main_file_play(self, mock_path_exists, mock_isfile):
+        main_file = "PCSG00009"
         mock_path_exists.return_value = True
         mock_isfile.return_value = True
         mock_config = MagicMock()
@@ -45,4 +58,18 @@ class TestVita3kRunner(unittest.TestCase):
         mock_config.runner_config = {"fullscreen": False}
         self.runner.config = mock_config
         expected = {"command": [self.runner.get_executable(), "-r", main_file]}
+        self.assertEqual(self.runner.play(), expected)
+
+    @patch("os.path.isfile")
+    @patch("lutris.util.system.path_exists")
+    def test_play_with_legacy_main_file(self, mock_path_exists, mock_isfile):
+        main_file = "PCSG00009"
+        title_id = "PCSG00027"
+        mock_path_exists.return_value = True
+        mock_isfile.return_value = True
+        mock_config = MagicMock()
+        mock_config.game_config = {"title_id": title_id, "main_file": main_file}
+        mock_config.runner_config = {"fullscreen": False}
+        self.runner.config = mock_config
+        expected = {"command": [self.runner.get_executable(), "-r", title_id]}
         self.assertEqual(self.runner.play(), expected)
