c0b5a08f192eaebc15641de2e75edce5d5baf1f8
Eikeno <eikenault@gmail.com>
2026-01-18 14:38:45 +0100

    add Azahar runner (#6043)
    * add Azahar runner
    
    fixes: #5854
    
    * better handling of version strings
    
    * remove unneeded json file
    
    * updated to release 2120.3
    
    * add flatpak_id to offer running from flatpak if installed
    
    * bump to v2121 and simplify runner to adjust upstream changes in providing appImage file (ural and not zipped anymore).
    
    * Bump appimage version to 2121.2
    
    * bump azahar to version 2122
    
    * bump version to 2122.1
    
    * bump version to 2123
    
    * bump version to 2123.1
    
    * bump version to 2123.2

---
+++ lutris/runners/azahar.py
@@ -0,0 +1,56 @@
+from gettext import gettext as _
+
+from lutris.exceptions import MissingGameExecutableError
+from lutris.runners.runner import Runner
+from lutris.util import system
+
+VER_STR = "2123.2"
+
+
+class azahar(Runner):
+    human_name = _("Azahar")
+    platforms = [_("Nintendo 3DS")]
+    description = _("Nintendo 3DS Emulator")
+    runnable_alone = True
+    runner_executable = "azahar/azahar.AppImage"
+    flatpak_id = "org.azahar_emu.Azahar"
+    download_url = f"https://github.com/azahar-emu/azahar/releases/download/{VER_STR}/azahar.AppImage"
+
+    # Azahar uses an AppImage, runtime causes QT  platform plugins issues.
+    system_options_override = [{"option": "disable_runtime", "default": True}]
+    game_options = [
+        {
+            "option": "main_file",
+            "label": _("The game data, commonly called a ROM image."),
+            "type": "file",
+            "default_path": "game_path",
+        },
+    ]
+    runner_options = [
+        {
+            "option": "fullscreen",
+            "label": _("Fullscreen"),
+            "type": "bool",
+            "default": True,
+        },
+    ]
+
+    def play(self):
+        """Run the game."""
+        arguments = self.get_command()
+        rom = self.game_config.get("main_file") or ""
+
+        if not system.path_exists(rom):
+            raise MissingGameExecutableError(filename=rom)
+
+        fullscreen = self.runner_config.get("fullscreen") or ""
+
+        if fullscreen:
+            arguments.append("-f")
+        # Don't pass '-w' here if `fullscreen' is not set to avoid interfering
+        # with (possible) present of future emulator caching of last used mode.
+        # Like that we avoid complications of having both `fullscreen' and
+        # `windowed' options to handle.
+
+        arguments.append(rom)
+        return {"command": arguments}
