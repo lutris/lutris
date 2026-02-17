82d7e87c71131db3c07486b8850230c9ac1a9941
Daniel Johnson <danieljohnson2@fastmail.com>
2026-02-17 16:34:43 -0500

    Add Python Version to the system information

--- lutris/util/linux.py
+++ lutris/util/linux.py
@@ -497,6 +497,7 @@ def gather_system_info_dict():
     system_dict["Arch"] = system_info["arch"]
     system_dict["Kernel"] = system_info["kernel"]
     system_dict["Lutris Version"] = settings.VERSION
+    system_dict["Python Version"] = sys.version
     system_dict["Desktop"] = system_info["env"].get("XDG_CURRENT_DESKTOP", "Not found")
     system_dict["Display Server"] = system_info["env"].get("XDG_SESSION_TYPE", "Not found")
     system_info_readable["System"] = system_dict
