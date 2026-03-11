d1089890874baebd97ab064407ec7ba7c272fef5
Daniel Johnson <danieljohnson2@fastmail.com>
2026-03-10 20:13:08 -0400

    A few performance improvements- make more use of the dicts we have to avoid linear scanning.

--- lutris/gui/views/list.py
+++ lutris/gui/views/list.py
@@ -136,17 +136,9 @@ class GameListView(Gtk.TreeView, GameView):  # type:ignore[misc]
         return self.get_model().get_value(iterator, COL_ID)
 
     def get_path_for_game_id(self, game_id):
-        path_found = None
-
-        def check_path(model, path, iterator):
-            nonlocal path_found
-            row_id = model.get_value(iterator, COL_ID)
-            if game_id == row_id:
-                path_found = path
-                return True  # stop iteration
-
-        self.get_model().foreach(check_path)
-        return path_found
+        if self.game_store:
+            return self.game_store.get_path_by_id(game_id)
+        return None
 
     def set_selected_game(self, game_id):
         row = self.game_store.get_row_by_id(game_id, filtered=True)
