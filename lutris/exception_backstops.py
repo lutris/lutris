60684b1e9845efd0c0f2dbbe7567c8101c8e7e4b
Daniel Johnson <danieljohnson2@fastmail.com>
2026-03-21 09:09:39 -0400

    Remove '# type: ignore[misc]' that is no longer required.
    The defect in the PyGObject-Stubs has been fixed in version 2.17 of the stubs. This is why we started getting errors in our GI over this.

--- lutris/exception_backstops.py
+++ lutris/exception_backstops.py
@@ -65,10 +65,7 @@ def _get_error_parent(error_objects: Iterable[Optional[GObject.Object]]) -> Gtk.
                 toplevel: Gtk.Window = error_object.get_toplevel()
                 if toplevel:
                     return toplevel
-        # GLib.GError is RuntimeError at runtime, but pygobject-stubs <= 2.16.0 omits the base
-        # class. This is fixed in pygobject-stubs main (post-2.16.0); remove the ignore once
-        # a fixed release is available and we've updated. -- @danieljohnson2
-        except GLib.GError:  # type: ignore[misc]
+        except GLib.GError:
             pass  # hasattr() is always true for (some) GObjects, but the method fails when used
 
     return get_required_main_window()
