a419dc2b09dd3509db104b8764b41d4d1c49353d
ItsAllAboutTheCode <52703+ItsAllAboutTheCode@users.noreply.github.com>
2026-03-23 20:42:58 -0500

    Added explicit require version calls for gobject packages
    This fixes issues when trying to import Gtk, Gdk, Pango, GnomeDesktop, PangoCairo and WebKit2 inside of unit test when on system with Gtk 4.0 installed.
    
    fixes #6569

--- lutris/gui/__init__.py
+++ lutris/gui/__init__.py
@@ -1,1 +1,10 @@
 """Lutris GUI package"""
+
+import gi
+
+gi.require_version("Gtk", "3.0")
+gi.require_version("Gdk", "3.0")
+gi.require_version("GnomeDesktop", "3.0")
+gi.require_version("Pango", "1.0")
+gi.require_version("PangoCairo", "1.0")
+gi.require_version("WebKit2", "4.1")
