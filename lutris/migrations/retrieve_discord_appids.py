72d5ac3e8e908bcfa73966c63a2529e1e2213056
Daniel Johnson <danieljohnson2@fastmail.com>
2026-03-29 06:08:39 -0400

    Fix sloppy edit- we need a dedupped list, not a set. I shouda trusted Claude!
    Resolves #6601

--- lutris/migrations/retrieve_discord_appids.py
+++ lutris/migrations/retrieve_discord_appids.py
@@ -10,7 +10,8 @@ def migrate():
     Update Games that do not have a Discord ID
     """
     logger.info("Updating Games Discord APP ID's")
-    slugs_to_update = {game["slug"] for game in get_games_where(discord_id__isnull=True)}
+    # Make a *de-duplicated* list of slugs; we need a list for the API.
+    slugs_to_update = list({game["slug"] for game in get_games_where(discord_id__isnull=True)})
     if not slugs_to_update:
         return
     games = get_api_games(slugs_to_update)
