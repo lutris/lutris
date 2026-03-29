d9d7a243df257882a0e8a600c636d45ffa83efb5
ItsAllAboutTheCode <52703+ItsAllAboutTheCode@users.noreply.github.com>
2026-03-29 03:53:46 -0500

    Additional GTK require_version fixes
    Moved the `gi.require_version` call to `lutris/__init__.py` for Gtk and Gdk

--- lutris/__init__.py
+++ lutris/__init__.py
@@ -1,3 +1,8 @@
 """Main Lutris package"""
 
 __version__ = "0.5.23"
+
+import gi
+
+gi.require_version("Gtk", "3.0")
+gi.require_version("Gdk", "3.0")
--- lutris/gui/__init__.py
+++ lutris/gui/__init__.py
@@ -2,6 +2,4 @@
 
 import gi
 
-gi.require_version("Gtk", "3.0")
-gi.require_version("Gdk", "3.0")
 gi.require_version("PangoCairo", "1.0")
