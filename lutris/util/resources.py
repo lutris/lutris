bdac79c4a27bde6f4d2967d4d92470f2eff56c3e
tuanaiseo <tuanaiseo@gmail.com>
2026-04-05 06:10:05 +0700

    fix(security): path traversal via unsanitized slug in filesystem
    Path builders directly concatenate `game_slug` into filesystem paths. If `game_slug` can contain `../` or absolute path segments from untrusted metadata, callers may read/write outside intended media directories.
    
    Affected files: resources.py
    
    Signed-off-by: tuanaiseo <221258316+tuanaiseo@users.noreply.github.com>

--- lutris/util/resources.py
+++ lutris/util/resources.py
@@ -1,20 +1,41 @@
 """Utility module to handle media resources"""
 
 import os
+import re
 
 from lutris import settings
 
 
+_SLUG_RE = re.compile(r"^[a-z0-9-]+$")
+
+
+def _sanitize_game_slug(game_slug: str) -> str:
+    if not _SLUG_RE.fullmatch(game_slug):
+        raise ValueError("Invalid game slug")
+    return game_slug
+
+
+def _safe_join(base_path: str, filename: str) -> str:
+    base_real = os.path.realpath(base_path)
+    candidate_path = os.path.realpath(os.path.join(base_real, filename))
+    if os.path.commonpath([base_real, candidate_path]) != base_real:
+        raise ValueError("Invalid resource path")
+    return candidate_path
+
+
 def get_icon_path(game_slug: str) -> str:
     """Return the absolute path for a game_slug icon"""
-    return os.path.join(settings.ICON_PATH, "lutris_%s.png" % game_slug)
+    slug = _sanitize_game_slug(game_slug)
+    return _safe_join(settings.ICON_PATH, "lutris_%s.png" % slug)
 
 
 def get_banner_path(game_slug: str) -> str:
     """Return the absolute path for a game_slug banner"""
-    return os.path.join(settings.BANNER_PATH, "{}.jpg".format(game_slug))
+    slug = _sanitize_game_slug(game_slug)
+    return _safe_join(settings.BANNER_PATH, "{}.jpg".format(slug))
 
 
 def get_cover_path(game_slug: str) -> str:
     """Return the absolute path for a game_slug coverart"""
-    return os.path.join(settings.COVERART_PATH, "{}.jpg".format(game_slug))
+    slug = _sanitize_game_slug(game_slug)
+    return _safe_join(settings.COVERART_PATH, "{}.jpg".format(slug))
