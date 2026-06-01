6c8d891ac617dbf7681707e02b7de3eeb460c8df
KTGX710 <kgallegos010@outlook.com>
2026-05-31 18:05:57 -0700

    Cleaned up gog.py.get_game_dlcs().

--- lutris/services/gog.py
+++ lutris/services/gog.py
@@ -84,6 +84,8 @@ class GOGService(OnlineService):
     medias = {"banner_small": GogSmallBanner, "banner": GogMediumBanner, "banner_large": GogLargeBanner}
     default_format = "banner"
 
+    MAX_PRODUCT_IDS_PER_REQUEST = 50
+
     embed_url = "https://embed.gog.com"
     api_url = "https://api.gog.com"
 
@@ -285,47 +287,46 @@ class GOGService(OnlineService):
         unified_json = []
         if not game_details["dlcs"]:
             return []
-
-        ### FIXME: URL doesn't work with more than 50 product id's
-        all_products_url = game_details["dlcs"]["expanded_all_products_url"]
-
+        ### GOG Api limits requests to 50 comma-separated ids: https://gogapidocs.readthedocs.io/en/latest/galaxy.html#get--products
         # If ids greater than 50 split request
-        if game_details["dlcs"]["products"].size() > 50:
-            # Get url prefix and suffix, excluding product ids list
-            all_products_url_prefix = all_products_url[: (all_products_url.find("ids=") + 4)]
-            all_products_url_suffix = all_products_url[(all_products_url.find("\u0026")): -1]
+        if game_details["dlcs"]["products"].size() > self.MAX_PRODUCT_IDS_PER_REQUEST:
             # Get product ids only form json
             product_ids = []
             for product in game_details["dlcs"]["products"]:
                 product_ids.append(product["id"])
+            # Get number of requests by batches of 50
+            if len(product_ids) % self.MAX_PRODUCT_IDS_PER_REQUEST <= 0:
+                num_requests = len(product_ids) / self.MAX_PRODUCT_IDS_PER_REQUEST
+            else:
+                num_requests = (len(product_ids) / self.MAX_PRODUCT_IDS_PER_REQUEST) + 1
             # Call make_api_request for each array
-            num_requests = len(product_ids) / 50
-            for i in range(int(num_requests + 1)):
-                try:
-                    start_index = 50 * i
-                    # Check if start index is out of bounds
-                    if start_index >= len(product_ids):
-                        break
-                    # Check if end index is out of bounds
-                    if start_index + 49 >= len(product_ids):
-                        end_index = -1
-                    else:
-                        end_index = start_index + 49
-                    # Build string of product ids
-                    product_ids_string = ",".join(product_ids[start_index: end_index])
-                    # Get JSONs from make_api_request
-                    request_json = self.make_api_request(all_products_url_prefix + product_ids_string + all_products_url_suffix)
-                    # Append all JSON objects into one
-                    for object in request_json:
-                        unified_json.append(object)
-                except:
-                    logger.info("Game details request index out of range.")
+            for i in range(int(num_requests)):
+                start_index = self.MAX_PRODUCT_IDS_PER_REQUEST * i
+                # Check if start index is out of bounds
+                if start_index >= len(product_ids):
                     break
+                # Check if end index exceeds array length
+                if (start_index + self.MAX_PRODUCT_IDS_PER_REQUEST - 1) >= len(product_ids):
+                    # Set end index to -1 to close loop
+                    end_index = -1
+                else:
+                    # Create index for array of 50 items [i.e. 0 to 49]
+                    end_index = start_index + self.MAX_PRODUCT_IDS_PER_REQUEST - 1
+                # Build string of product ids
+                product_ids_string = ",".join(product_ids[start_index:end_index])
+                # Send batched request
+                request_json = self.make_api_request(
+                    "{}/products/?ids={}&expand=downloads".format(self.api_url, product_ids_string)
+                )
+                # Append products from response to unified JSON
+                for product in request_json:
+                    unified_json.append(product)
         else:
             # Default logic
+            all_products_url = game_details["dlcs"]["expanded_all_products_url"]
             unified_json = self.make_api_request(all_products_url)
 
-        # Return uinified JSON object
+        # Return unified JSON object
         return unified_json
 
     def get_game_details(self, product_id: str) -> dict:
@@ -752,7 +753,7 @@ class GOGService(OnlineService):
         for dlc in dlcs:
             dlc_id = "gogdlc-%s" % dlc["slug"]
 
-            # remove mac installers for now
+            # remove Mac installers for now
             installfiles = [
                 installer for installer in dlc["downloads"].get("installers", []) if installer["os"] != "mac"
             ]
